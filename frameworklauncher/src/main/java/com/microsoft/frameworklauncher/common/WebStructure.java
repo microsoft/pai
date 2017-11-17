2bc61dbc1c6f5cdda6988a191f65fdfb772ab5e4
Yuqi Wang <yqwang@microsoft.com>
2017-11-17 18:04:50 +0800

    [Launcher]: Add VERSION_PATH in WebServer

--- frameworklauncher/src/main/java/com/microsoft/frameworklauncher/common/WebStructure.java
+++ frameworklauncher/src/main/java/com/microsoft/frameworklauncher/common/WebStructure.java
@@ -31,11 +31,12 @@ public class WebStructure {
   private static final String TASK_ROLE_NAME_PATH_PARAM_PLACEMENT = "{" + TASK_ROLE_NAME_PATH_PARAM + "}";
   private static final String CONTAINER_ID_PATH_PARAM_PLACEMENT = "{" + CONTAINER_ID_PATH_PARAM + "}";
 
-  public static final String ROOT_PATH = "/v1";
-  public static final String LAUNCHER_STATUS_PATH = ROOT_PATH + PATH_SEPARATOR + "LauncherStatus";
-  public static final String LAUNCHER_REQUEST_PATH = ROOT_PATH + PATH_SEPARATOR + "LauncherRequest";
+  public static final String ROOT_PATH = "/";
+  public static final String VERSION_PATH = ROOT_PATH + "v1";
+  public static final String LAUNCHER_STATUS_PATH = VERSION_PATH + PATH_SEPARATOR + "LauncherStatus";
+  public static final String LAUNCHER_REQUEST_PATH = VERSION_PATH + PATH_SEPARATOR + "LauncherRequest";
   public static final String DATA_DEPLOYMENT_VERSION_PATH = LAUNCHER_REQUEST_PATH + PATH_SEPARATOR + "DataDeploymentVersion";
-  public static final String FRAMEWORK_ROOT_PATH = ROOT_PATH + PATH_SEPARATOR + "Frameworks";
+  public static final String FRAMEWORK_ROOT_PATH = VERSION_PATH + PATH_SEPARATOR + "Frameworks";
   public static final String FRAMEWORK_PATH = FRAMEWORK_ROOT_PATH + PATH_SEPARATOR + FRAMEWORK_NAME_PATH_PARAM_PLACEMENT;
   public static final String AGGREGATED_FRAMEWORK_STATUS_PATH = FRAMEWORK_PATH + PATH_SEPARATOR + "AggregatedFrameworkStatus";
   public static final String FRAMEWORK_STATUS_PATH = FRAMEWORK_PATH + PATH_SEPARATOR + "FrameworkStatus";
