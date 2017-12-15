210a7bdb95944a9885311045aba460e27645ffc9
YitongFeng <imfengyitong@gmail.com>
2017-12-15 21:33:52 +0800

    grafana docker src

---
+++ service-deployment/src/grafana/start_server.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+chown -R grafana:grafana /var/lib/grafana /var/log/grafana
+
+if [ ! -z ${GF_AWS_PROFILES+x} ]; then
+    for profile in ${GF_AWS_PROFILES}; do
+        access_key_varname="GF_AWS_${profile}_ACCESS_KEY_ID"
+        secret_key_varname="GF_AWS_${profile}_SECRET_ACCESS_KEY"
+        region_varname="GF_AWS_${profile}_REGION"
+
+        if [ ! -z "${!access_key_varname}" -a ! -z "${!secret_key_varname}" ]; then
+            echo "[${profile}]" >> ~grafana/.aws/credentials
+            echo "aws_access_key_id = ${!access_key_varname}" >> ~grafana/.aws/credentials
+            echo "aws_secret_access_key = ${!secret_key_varname}" >> ~grafana/.aws/credentials
+            if [ ! -z "${!region_varname}" ]; then
+                echo "region = ${!region_varname}" >> ~grafana/.aws/credentials
+            fi
+        fi
+    done
+
+    chown grafana:grafana -R ~grafana/.aws
+    chmod 600 ~grafana/.aws/credentials
+fi
+
+# upgrade all installed plugins
+if [ "$UPGRADEALL" = true ] ; then
+    grafana-cli --pluginsDir "${GF_PLUGIN_DIR}" plugins upgrade-all || true
+fi
+
+exec gosu grafana /usr/sbin/grafana-server   \
+  --homepath=/usr/share/grafana              \
+  --config=/etc/grafana/grafana.ini          \
+  cfg:default.paths.data=${GF_PATHS_DATA}    \
+  cfg:default.paths.logs=${GF_PATHS_LOGS}    \
+  cfg:default.paths.plugins=${GF_PLUGIN_DIR} \
+  "$@"
+
