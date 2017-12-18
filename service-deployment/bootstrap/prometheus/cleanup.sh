d493a89fedc0315cc841b6fd7e8fc4df197f1305
YanjieGao <yanjga@microsoft.com>
2017-12-18 02:31:24 +0000

    decoupled the prometheus with grafana, only prometheus

--- service-deployment/bootstrap/prometheus/cleanup.sh
+++ service-deployment/bootstrap/prometheus/cleanup.sh
@@ -25,9 +25,6 @@ KUBECTL="kubectl  --namespace=\"${NAMESPACE}\""
 INSTANCES="daemonset/gpu-exporter
 daemonset/node-exporter
 deployment/prometheus-deployment
-deployment/grafana
-deployment/grafana-cmd
-service/grafana
 configmap/prometheus-configmap
 configmap/grafana-import-dashboards"
 
@@ -35,12 +32,12 @@ for instance in ${INSTANCES}; do
   eval "${KUBECTL} delete --ignore-not-found --now \"${instance}\""
 done
 
-PODS=$(eval "${KUBECTL} get pods -o name" | awk '/^pod\/(prometheus-deployment|node-exporter|gpu-exporter|grafana)-/ {print $1}' | tr '\n' ' ')
+PODS=$(eval "${KUBECTL} get pods -o name" | awk '/^pod\/(prometheus-deployment|node-exporter|gpu-exporter)-/ {print $1}' | tr '\n' ' ')
 while [ ! "${PODS}" = "" ]; do
   echo "Waiting 1 second for ${PODS}pods to shutdown..."
   sleep 1
   eval "${KUBECTL} delete --now ${PODS}"
-  PODS=$(eval "${KUBECTL} get pods -o name" | awk '/^pod\/(prometheus-deployment|node-exporter|gpu-exporter|grafana)-/ {print $1}' | tr '\n' ' ')
+  PODS=$(eval "${KUBECTL} get pods -o name" | awk '/^pod\/(prometheus-deployment|node-exporter|gpu-exporter)-/ {print $1}' | tr '\n' ' ')
 done
 
 eval "${KUBECTL} get pods"
--- service-deployment/bootstrap/prometheus/start.sh
+++ service-deployment/bootstrap/prometheus/start.sh
@@ -31,14 +31,11 @@ eval "kubectl create namespace \"${NAMESPACE}\""
 
 # (1) create configmap
 eval "${KUBECTL} create -f prometheus-configmap.yaml"
-eval "${KUBECTL} create configmap grafana-import-dashboards --from-file=dashboards -o json --dry-run" | eval "${KUBECTL} apply -f -"
 
 # (2) create the pods
 eval "${KUBECTL} create -f gpu-exporter-ds.yaml"
 eval "${KUBECTL} create -f node-exporter-ds.yaml"
 eval "${KUBECTL} create -f prometheus-deployment.yaml"
-eval "${KUBECTL} create -f grafana.yaml"
-eval "${KUBECTL} create -f grafana-cmd.yaml"
 eval "${KUBECTL} get pods"
 
 popd > /dev/null
