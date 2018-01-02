8790750d58f8fa1f2316ca43b4372083f6c31c33
yuye@microsoft.com <yuye@microsoft.com>
2018-01-02 11:51:21 +0800

    Add function to COPY and GENERATE for single image.

--- service-deployment/docker_build.py
+++ service-deployment/docker_build.py
@@ -86,6 +86,18 @@ def login_docker_registry(docker_registry, docker_username, docker_password):
 
 
 
+def generate_template_file_service(cluster_config, service_config, image_name):
+
+    if service_config['imagelist'][image_name]['templatelist'][0] == 'None':
+        return
+
+    for template_file in service_config['imagelist'][image_name]['templatelist']:
+        template_data = read_template("src/{0}/{1}.template".format(image_name, template_file))
+        generate_data = generate_from_template(template_data, cluster_config)
+        write_generated_file("src/{0}/{1}".format(image_name, template_file), generate_data)
+
+
+
 def generate_template_file(cluster_config, service_config):
 
     image_list = service_config['imagelist']
@@ -99,6 +111,21 @@ def generate_template_file(cluster_config, service_config):
             template_data = read_template("src/{0}/{1}.template".format(image, template_file))
             generate_data = generate_from_template(template_data, cluster_config)
             write_generated_file("src/{0}/{1}".format(image, template_file), generate_data)
+
+
+
+def delete_generated_template_file_service(service_config, image_name):
+
+    if service_config['imagelist'][image_name]['templatelist'][0] == 'None':
+        return
+
+    for template_file in service_config['imagelist'][image_name]['templatelist']:
+        file_path = "src/{0}/{1}".format(image_name, template_file)
+
+        if os.path.exists(file_path):
+            shell_cmd = "rm -rf {0}".format(file_path)
+            error_msg = "failed to rm {0}".format(file_path)
+            execute_shell(shell_cmd, error_msg)
 
 
 
@@ -118,6 +145,25 @@ def delete_generated_template_file(service_config):
                 shell_cmd = "rm -rf {0}".format(file_path)
                 error_msg = "failed to rm {0}".format(file_path)
                 execute_shell(shell_cmd, error_msg)
+
+
+
+def copy_arrangement_service(service_config, image_name):
+
+    if 'copy' not in service_config['imagelist'][image_name]:
+        return
+
+    dst = "src/{0}/copied_file".format(image_name)
+
+    if os.path.exists(dst) == False:
+        shell_cmd = "mkdir -p {0}".format(dst)
+        error_msg = "failed to mkdir -p {0}".format(dst)
+        execute_shell(shell_cmd, error_msg)
+
+    for copy_item_path in service_config['imagelist'][image_name]['copy']:
+        shell_cmd = "cp -r {0} {1}".format(copy_item_path, dst)
+        error_msg = "failed to copy {0}".format(copy_item_path)
+        execute_shell(shell_cmd, error_msg)
 
 
 
@@ -141,6 +187,23 @@ def copy_arrangement(service_config):
             shell_cmd = "cp -r {0} {1}".format(copy_item_path, dst)
             error_msg = "failed to copy {0}".format(copy_item_path)
             execute_shell(shell_cmd, error_msg)
+
+
+
+def copy_cleanup_service(service_config, image_name):
+
+    if 'copy' not in service_config['imagelist'][image_name]:
+        return
+
+    dst = "src/{0}/copied_file".format(image_name)
+
+    if os.path.exists(dst) == False:
+        return
+
+    for copy_item_path in service_config['imagelist'][image_name]['copy']:
+        shell_cmd = "rm -rf {0}".format(dst)
+        error_msg = "failed to rm {0}".format(copy_item_path)
+        execute_shell(shell_cmd, error_msg)
 
 
 
@@ -283,6 +346,7 @@ def hadoop_binary_prepare(custom_hadoop_path, hadoop_version):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', required=True, help="path to cluster configuration file")
+    parser.add_argument('-n', '--name', default='all', help="Build and push an image to the registry")
 
     args = parser.parse_args()
 
@@ -311,9 +375,9 @@ def main():
     build_docker_images(cluster_config, service_config)
     copy_cleanup(service_config)
 
-    hadoop_binary_remove(hadoop_version)
     delete_generated_template_file(service_config)
 
+    hadoop_binary_remove(hadoop_version)
 
     push_docker_images(cluster_config, service_config)
 
