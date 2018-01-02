1a363c170e8c9a30e464c88e1de4a37570458cb6
yuye@microsoft.com <yuye@microsoft.com>
2018-01-02 15:35:10 +0800

    Remove unused function.

--- service-deployment/docker_build.py
+++ service-deployment/docker_build.py
@@ -98,22 +98,6 @@ def generate_template_file_service(cluster_config, service_config, image_name):
 
 
 
-def generate_template_file(cluster_config, service_config):
-
-    image_list = service_config['imagelist']
-
-    for image in image_list:
-
-        if image_list[image]['templatelist'][0] == 'None':
-            continue
-
-        for template_file in image_list[image]['templatelist']:
-            template_data = read_template("src/{0}/{1}.template".format(image, template_file))
-            generate_data = generate_from_template(template_data, cluster_config)
-            write_generated_file("src/{0}/{1}".format(image, template_file), generate_data)
-
-
-
 def delete_generated_template_file_service(service_config, image_name):
 
     if service_config['imagelist'][image_name]['templatelist'][0] == 'None':
@@ -126,25 +110,6 @@ def delete_generated_template_file_service(service_config, image_name):
             shell_cmd = "rm -rf {0}".format(file_path)
             error_msg = "failed to rm {0}".format(file_path)
             execute_shell(shell_cmd, error_msg)
-
-
-
-def delete_generated_template_file(service_config):
-
-    image_list = service_config['imagelist']
-
-    for image in image_list:
-
-        if image_list[image]['templatelist'][0] == 'None':
-            continue
-
-        for template_file in image_list[image]['templatelist']:
-            file_path = "src/{0}/{1}".format(image, template_file)
-
-            if os.path.exists(file_path):
-                shell_cmd = "rm -rf {0}".format(file_path)
-                error_msg = "failed to rm {0}".format(file_path)
-                execute_shell(shell_cmd, error_msg)
 
 
 
@@ -167,29 +132,6 @@ def copy_arrangement_service(service_config, image_name):
 
 
 
-def copy_arrangement(service_config):
-
-    image_list = service_config['imagelist']
-
-    for image in image_list:
-
-        if 'copy' not in image_list[image]:
-            continue
-
-        dst = "src/{0}/copied_file".format(image)
-
-        if os.path.exists(dst) == False :
-            shell_cmd = "mkdir -p {0}".format(dst)
-            error_msg = "failed to mkdir -p {0}".format(dst)
-            execute_shell(shell_cmd, error_msg)
-
-        for copy_item_path in image_list[image]['copy']:
-            shell_cmd = "cp -r {0} {1}".format(copy_item_path, dst)
-            error_msg = "failed to copy {0}".format(copy_item_path)
-            execute_shell(shell_cmd, error_msg)
-
-
-
 def copy_cleanup_service(service_config, image_name):
 
     if 'copy' not in service_config['imagelist'][image_name]:
@@ -204,27 +146,6 @@ def copy_cleanup_service(service_config, image_name):
         shell_cmd = "rm -rf {0}".format(dst)
         error_msg = "failed to rm {0}".format(copy_item_path)
         execute_shell(shell_cmd, error_msg)
-
-
-
-def copy_cleanup(service_config):
-
-    image_list = service_config['imagelist']
-
-    for image in image_list:
-
-        if 'copy' not in image_list[image]:
-            continue
-
-        dst = "src/{0}/copied_file".format(image)
-
-        if os.path.exists(dst) == False:
-            continue
-
-        for copy_item_path in image_list[image]['copy']:
-            shell_cmd = "rm -rf {0}".format(dst)
-            error_msg = "failed to rm {0}".format(copy_item_path)
-            execute_shell(shell_cmd, error_msg)
 
 
 
@@ -414,15 +335,7 @@ def main():
 
     login_docker_registry(docker_registry, docker_username, docker_password)
 
-    #generate_template_file(cluster_config, service_config)
-
-    #copy_arrangement(service_config)
-
     build_docker_images(cluster_config, service_config, args.name)
-
-    #copy_cleanup(service_config)
-
-    #delete_generated_template_file(service_config)
 
     hadoop_binary_remove(hadoop_version)
 
