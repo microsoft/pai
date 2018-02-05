7220c3a006b4c239f53373177247ea8cb07315e6
abuccts <abuccts@gmail.com>
2018-02-05 13:45:34 +0800

    Add change password
    Add change password for user.

--- webportal/config/webpack.common.js
+++ webportal/config/webpack.common.js
@@ -44,6 +44,7 @@ const config = {
     layout: './src/app/layout/layout.component.js',
     register: './src/app/user/user-register/user-register.component.js',
     login: './src/app/user/user-login/user-login.component.js',
+    resetPassword: './src/app/user/reset-password/reset-password.component.js',
     dashboard: './src/app/dashboard/dashboard.component.js',
     submit: './src/app/job/job-submit/job-submit.component.js',
     view: './src/app/job/job-view/job-view.component.js',
@@ -190,6 +191,14 @@ const config = {
       minify: htmlMinifierOptions,
       cache: true,
       chunks: ['layout', 'login']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Platform for AI',
+      filename: 'reset-password.html',
+      template: './src/app/layout/layout.component.ejs',
+      minify: htmlMinifierOptions,
+      cache: true,
+      chunks: ['layout', 'resetPassword']
     }),
     new HtmlWebpackPlugin({
       title: 'Platform for AI',
