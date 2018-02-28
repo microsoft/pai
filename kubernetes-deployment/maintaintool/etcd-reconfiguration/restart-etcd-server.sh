948c6d32d6498850cdbb34b630789d907012f937
yuye@microsoft.com <yuye@microsoft.com>
2018-02-28 12:09:04 +0800

    Update script.

--- kubernetes-deployment/maintaintool/etcd-reconfiguration/restart-etcd-server.sh
+++ kubernetes-deployment/maintaintool/etcd-reconfiguration/restart-etcd-server.sh
@@ -17,4 +17,4 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-cp etcd-reconfiguration/etcd.yaml /etc/kubernetes/manifests/
+cp etcd-reconfiguration-restart/etcd.yaml /etc/kubernetes/manifests/
