2108fb63362f162f3c8cb0625b8c328b71ea9190
YanjieGao <yanjga@microsoft.com>
2018-03-22 02:57:50 +0000

    revert

--- service-deployment/bootstrap/prometheus/start.sh
+++ service-deployment/bootstrap/prometheus/start.sh
@@ -26,7 +26,7 @@ chmod u+x node-label.sh
 ./node-label.sh
 
 kubectl create -f prometheus-configmap.yaml
-#kubectl create -f node-exporter-ds.yaml
+kubectl create -f node-exporter-ds.yaml
 kubectl create -f prometheus-deployment.yaml
 
 popd > /dev/null
