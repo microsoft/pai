4375840f4c73cdd38e754bcf58efffe2a5754bbb
YanjieGao <yanjga@microsoft.com>
2018-04-09 09:23:29 +0000

    refine the gpu exporter

--- prometheus/exporter/gpu_exporter.py
+++ prometheus/exporter/gpu_exporter.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python
 import subprocess
 import sys
+from xml.dom import minidom
 
-def parse_smi(smi, logDir):
-    print("nvidia-smi results")
-    print(smi)
-    data = [line.split(',') for line in smi.splitlines()]
-    # pop the headers
-    data.pop(0)
-    gpu_count = len(data)
+def parse_xml(smi, logDir):
+    xmldoc = minidom.parse('test.xml')
+    gpuList = xmldoc.getElementsByTagName('gpu')
+    print(len(gpuList))
+    gpu_count = len(gpuList)
     print("gpu numbers" + str(gpu_count))
     nvidiasmi_attached_gpus = "nvidiasmi_attached_gpus" + "{} " + str(gpu_count) 
     outputFile = open(logDir + "gpu_exporter.prom", "w")
     outputFile.write(nvidiasmi_attached_gpus + "\n")
-    index = 0
-
-    for gpu_object in data:
-        nvidiasmi_utilization_gpus = "nvidiasmi_utilization_gpus" + "{minor_number=" + str(index) + "}" + " " + str(gpu_object[1])
-        nvidiasmi_utilization_memory = "nvidiasmi_utilization_memory" + "{minor_number=" + str(index) + "}" + " " + str(gpu_object[2]) 
-        index += 1
+    for gpu in gpuList:
+        minorNumber = gpu.getElementsByTagName('minor_number')[0].childNodes[0].data
+        gpuUtil = gpu.getElementsByTagName('utilization')[0].getElementsByTagName('gpu_util')[0].childNodes[0].data.replace("%", "")
+        gpuMemUtil = gpu.getElementsByTagName('utilization')[0].getElementsByTagName('memory_util')[0].childNodes[0].data.replace("%", "")
+        nvidiasmi_utilization_gpus = "nvidiasmi_utilization_gpus" + "{minor_number=" + str(minorNumber) + "}" + " " + str(gpuUtil)
+        nvidiasmi_utilization_memory = "nvidiasmi_utilization_memory" + "{minor_number=" + str(minorNumber) + "}" + " " + str(gpuMemUtil) 
         outputFile.write(nvidiasmi_utilization_memory + "\n")
         outputFile.write(nvidiasmi_utilization_gpus + "\n")
-    
-    outputFile.close()
 
 def main(argv):
     print(argv) 
     logDir = argv[0]
+    test = argv[1]
 
     try:
-        nvidia_smi_path = "nvidia-smi "
-        nvidia_smi_query = "--query-gpu=uuid,utilization.gpu,utilization.memory "
-        nvidia_smi_format = "--format=csv,nounits "
-        smi_output = subprocess.check_output([nvidia_smi_path, nvidia_smi_query, nvidia_smi_format])
-        parse_smi(smi_output, logDir)
+        nvidia_smi_path = "nvidia-smi -q -x"
+        nvidia_smi_query = "-q -x"
+        smi_output = subprocess.check_output([nvidia_smi_path, nvidia_smi_query])
+        parse_xml(smi_output, logDir)
     except subprocess.CalledProcessError as e:
         raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
-# execute test cmd example: python .\gpu_exporter.py ./ 
+# execute test cmd example: python .\gpu_exporter.py ./ True
 if __name__ == "__main__":
     main(sys.argv[1:])
