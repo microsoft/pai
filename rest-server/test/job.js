83d8e9449b8ff0585eae0fe40d196138a2857cf4
Hao Wu <wuh@microsoft.com>
2018-04-24 17:29:40 +0800

    Refine code style.

--- rest-server/test/job.js
+++ rest-server/test/job.js
@@ -66,40 +66,40 @@ describe('Jobs API /api/v1/jobs', () => {
   // GET /api/v1/jobs
   it('[P-01] Get job list', (done) => {
     nock(launcherWebserviceUri)
-    .get('/v1/Frameworks')
-    .reply(
-      200,
-      allFrameworks
-    );
+      .get('/v1/Frameworks')
+      .reply(
+        200,
+        allFrameworks
+      );
     chai.request(server)
-    .get('/api/v1/jobs')
-    .end((err, res) => {
-      expect(res, 'status code').to.have.status(200);
-      expect(res, 'json response').be.json;
-      expect(res.body.length, 'job list length').to.equal(2);
-      done();
-    });
+      .get('/api/v1/jobs')
+      .end((err, res) => {
+        expect(res, 'status code').to.have.status(200);
+        expect(res, 'json response').be.json;
+        expect(res.body.length, 'job list length').to.equal(2);
+        done();
+      });
   });
 
   it('[P-02] Get job list of user \'test1\'', (done) => {
     nock(launcherWebserviceUri)
-    .get('/v1/Frameworks')
-    .query({UserName: 'test1'})
-    .reply(
-      200,
-      {
-        'summarizedFrameworkInfos': [
-          allFrameworks.summarizedFrameworkInfos[0]
-        ]
-      }
-    );
+      .get('/v1/Frameworks')
+      .query({UserName: 'test1'})
+      .reply(
+        200,
+        {
+          'summarizedFrameworkInfos': [
+            allFrameworks.summarizedFrameworkInfos[0],
+          ],
+        }
+      );
     chai.request(server)
-    .get('/api/v1/jobs?username=test1')
-    .end((err, res) => {
-      expect(res, 'status code').to.have.status(200);
-      expect(res, 'json response').be.json;
-      expect(res.body.length, 'test1 job list length').to.equal(1);
-      done();
-    });
+      .get('/api/v1/jobs?username=test1')
+      .end((err, res) => {
+        expect(res, 'status code').to.have.status(200);
+        expect(res, 'json response').be.json;
+        expect(res.body.length, 'test1 job list length').to.equal(1);
+        done();
+      });
   });
 });
--- rest-server/test/jobSubmission.js
+++ rest-server/test/jobSubmission.js
@@ -46,25 +46,25 @@ describe('Submit job: POST /api/v1/jobs', () => {
 
   const prepareNockForCaseP01 = (jobName) => {
     global.nock(global.launcherWebserviceUri)
-    .get(`/v1/Frameworks/${jobName}`)
-    .reply(
-      404,
-      {}
-    );
+      .get(`/v1/Frameworks/${jobName}`)
+      .reply(
+        404,
+        {}
+      );
     global.nock(global.webhdfsUri)
-    .put(/op=MKDIR/)
-    .times(4)
-    .reply(
-      200,
-      {}
-    );
+      .put(/op=MKDIR/)
+      .times(4)
+      .reply(
+        200,
+        {}
+      );
     global.nock(global.webhdfsUri)
-    .put(/op=CREATE/)
-    .times(4)
-    .reply(
-      201,
-      {}
-    );
+      .put(/op=CREATE/)
+      .times(4)
+      .reply(
+        201,
+        {}
+      );
   };
 
   const prepareNockForCaseP02 = prepareNockForCaseP01;
@@ -73,18 +73,18 @@ describe('Submit job: POST /api/v1/jobs', () => {
 
   const prepareNockForCaseN03 = () => {
     global.nock(global.launcherWebserviceUri)
-    .get('/v1/Frameworks/job1')
-    .reply(
-      200,
-      global.mustache.render(
-        global.frameworkDetailTemplate,
-        {
-          'frameworkName': 'job1',
-          'userName': 'test',
-          'applicationId': 'app1',
-        }
-      )
-    );
+      .get('/v1/Frameworks/job1')
+      .reply(
+        200,
+        global.mustache.render(
+          global.frameworkDetailTemplate,
+          {
+            'frameworkName': 'job1',
+            'userName': 'test',
+            'applicationId': 'app1',
+          }
+        )
+      );
   };
 
   //
@@ -94,43 +94,43 @@ describe('Submit job: POST /api/v1/jobs', () => {
   it('[P-01] Submit a job to the default vc', (done) => {
     prepareNockForCaseP01('new_job');
     global.chai.request(global.server)
-    .post('/api/v1/jobs')
-    .set('Authorization', 'Bearer ' + validToken)
-    .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job' })))
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(202);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(res.body.message, 'response message').equal('update job new_job successfully');
-      done();
-    });
+      .post('/api/v1/jobs')
+      .set('Authorization', 'Bearer ' + validToken)
+      .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job' })))
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(202);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(res.body.message, 'response message').equal('update job new_job successfully');
+        done();
+      });
   });
 
   it('[P-02] Submit a job to vc1', (done) => {
     prepareNockForCaseP02('new_job_queue_vc1');
     global.chai.request(global.server)
-    .post('/api/v1/jobs')
-    .set('Authorization', 'Bearer ' + validToken)
-    .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job_queue_vc1', 'virtualCluster': 'vc1' })))
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(202);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(res.body.message, 'response message').equal('update job new_job_queue_vc1 successfully');
-      done();
-    });
+      .post('/api/v1/jobs')
+      .set('Authorization', 'Bearer ' + validToken)
+      .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job_queue_vc1', 'virtualCluster': 'vc1' })))
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(202);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(res.body.message, 'response message').equal('update job new_job_queue_vc1 successfully');
+        done();
+      });
   });
 
   it('[P-03] Submit a job using PUT method', (done) => {
     prepareNockForCaseP03('new_job');
     global.chai.request(global.server)
-    .put('/api/v1/jobs/new_job')
-    .set('Authorization', 'Bearer ' + validToken)
-    .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job' })))
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(202);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(res.body.message, 'response message').equal('update job new_job successfully');
-      done();
-    });
+      .put('/api/v1/jobs/new_job')
+      .set('Authorization', 'Bearer ' + validToken)
+      .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, { 'jobName': 'new_job' })))
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(202);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(res.body.message, 'response message').equal('update job new_job successfully');
+        done();
+      });
   });
 
   //
@@ -139,15 +139,15 @@ describe('Submit job: POST /api/v1/jobs', () => {
 
   it('[N-01] Invalid token', (done) => {
     global.chai.request(global.server)
-    .post('/api/v1/jobs')
-    .set('Authorization', 'Bearer ' + invalidToken)
-    .send({})
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(401);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(res.body.message, 'response message').equal('No authorization token was found');
-      done();
-    });
+      .post('/api/v1/jobs')
+      .set('Authorization', 'Bearer ' + invalidToken)
+      .send({})
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(401);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(res.body.message, 'response message').equal('No authorization token was found');
+        done();
+      });
   });
 
   it('[N-02] Schema checking failed', (done) => {
@@ -166,27 +166,27 @@ describe('Submit job: POST /api/v1/jobs', () => {
   it('[N-03] Duplicated job name', (done) => {
     prepareNockForCaseN03();
     global.chai.request(global.server)
-    .post('/api/v1/jobs')
-    .set('Authorization', 'Bearer ' + validToken)
-    .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, {'jobName': 'job1'})))
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(400);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(JSON.stringify(res.body), 'response body content').include('DuplicateJobSubmission');
-      done();
-    });
+      .post('/api/v1/jobs')
+      .set('Authorization', 'Bearer ' + validToken)
+      .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, {'jobName': 'job1'})))
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(400);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(JSON.stringify(res.body), 'response body content').include('DuplicateJobSubmission');
+        done();
+      });
   });
 
   it('[N-04] Cannot connect to Launcher', (done) => {
     global.chai.request(global.server)
-    .post('/api/v1/jobs')
-    .set('Authorization', 'Bearer ' + validToken)
-    .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, {'jobName': 'another_new_job'})))
-    .end((err, res) => {
-      global.chai.expect(res, 'status code').to.have.status(500);
-      global.chai.expect(res, 'response format').be.json;
-      global.chai.expect(JSON.stringify(res.body), 'response body content').include('InternalServerError');
-      done();
-    });
+      .post('/api/v1/jobs')
+      .set('Authorization', 'Bearer ' + validToken)
+      .send(JSON.parse(global.mustache.render(global.jobConfigTemplate, {'jobName': 'another_new_job'})))
+      .end((err, res) => {
+        global.chai.expect(res, 'status code').to.have.status(500);
+        global.chai.expect(res, 'response format').be.json;
+        global.chai.expect(JSON.stringify(res.body), 'response body content').include('InternalServerError');
+        done();
+      });
   });
 });
