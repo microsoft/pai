f77798ae100dc090d5c87a1559b604503e0f2cca
yuye@microsoft.com <yuye@microsoft.com>
2018-04-26 21:58:51 +0800

    Checkin docker library

--- pai-management/paiLibrary/common/docker_handler.py
+++ pai-management/paiLibrary/common/docker_handler.py
@@ -17,4 +17,77 @@
 
 
 import docker
+import file_handler
+import linux_shell
+import logging
+import logging.config
+
+
+# TODO: Change the command with linux_shell.execute_shell to docker lib.
+
+
+
+
+
+
+class docker_handler:
+
+
+    def __init__(self, docker_registry, docker_username, docker_password):
+
+        self.logger = logging.getLogger(__name__)
+
+        if docker_registry == "public":
+            docker_registry = ""
+
+        self.docker_registry = docker_registry
+        self.docker_username = docker_username
+        self.docker_password = docker_password
+
+        self.docker_login()
+        self.docker_client = self.client_initialization()
+
+
+
+    def docker_login(self):
+
+        shell_cmd = "docker login -u {0} -p {1} {2}".format(self.docker_username, self.docker_password, self.docker_registry)
+        error_msg = "docker registry login error"
+        linux_shell.execute_shell(shell_cmd, error_msg)
+        self.logger.info("docker registry login successfully")
+
+
+
+    def client_initialization(self):
+
+        client = docker.from_env()
+        return client
+
+
+
+    def image_build(self, image_name, path_to_dockerfile, image_tag = "latest"):
+
+        target_tag = "{0}:{1}".format(image_name, image_tag)
+        image_obj, build_log = self.docker_client.images.build(path=path_to_dockerfile, tag=target_tag, rm=True, pull=True)
+        for line in build_log:
+            self.logger.info(line)
+
+
+
+    def image_retag(self, origin_image_name, target_image_name, image_tag):
+
+        origin_tag = "{0}:{1}".format(origin_image_name, image_tag)
+
+        target_image = self.docker_client.images.get(origin_tag)
+        target_image.tag(target_image_name, image_tag)
+
+
+
+    def image_push(self, image_name, image_tag):
+
+        target_tag = "{0}:{1}".format(image_name, image_tag)
+        target_image = self.docker_client.images.push(image_name, image_tag)
+
+
+
 
