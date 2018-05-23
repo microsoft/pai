5b4708c849b486e3d8b84b3d0f1bc0a0ba280dcd
yuye@microsoft.com <yuye@microsoft.com>
2018-05-23 13:23:28 +0800

    Remove triggle logic in refrash and start, when the target service is a single service.

--- pai-management/paiLibrary/paiService/service_management_refrash.py
+++ pai-management/paiLibrary/paiService/service_management_refrash.py
@@ -69,6 +69,8 @@ class service_management_refrash:
         dependency_list = service_refrasher.get_dependency()
         if dependency_list != None:
             for fat_serv in dependency_list:
+                if fat_serv not in self.service_list:
+                    continue
                 if fat_serv in self.done_dict and self.done_dict[fat_serv] == True:
                     continue
                 self.start(fat_serv)
