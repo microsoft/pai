e94a2b4dea02a75dcd86c598f74a0e3b3f267e94
yuye@microsoft.com <yuye@microsoft.com>
2018-06-01 20:24:49 +0800

    Issue Fix.

--- pai-management/doc/example/add-service/boostrap/hbase/hbase-configuration/regionserver-hbase-start.sh
+++ pai-management/doc/example/add-service/boostrap/hbase/hbase-configuration/regionserver-hbase-start.sh
@@ -19,6 +19,6 @@
 
 
 # After the configuration and environment in regionserver-hbase-generate.sh is done, start the regionserver of hbase.
-$HBASE_BIN_DIR/hbase-daemon.sh foreground_start regionserver
+$HBASE_BIN_DIR/hbase-daemon.sh start regionserver
 
 while true; do sleep 1000; done
