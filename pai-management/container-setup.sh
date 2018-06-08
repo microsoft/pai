f527f35f96930823f655d512615a4dd937d3cfcc
YundongYe <YundongYe@outlook.com>
2018-06-08 12:32:14 +0800

    Solve Dev-Box Environment Setup Issue.  (#681)
    * Solve issue. /usr/local/lib/python2.7/dist-packages/requests/__init__.py:80: RequestsDependencyWarning: urllib3 (1.13.1) or chardet (3.0.4) doesn't match a supported version!
    
    * Solve issue. /usr/local/lib/python2.7/dist-packages/requests/__init__.py:80: RequestsDependencyWarning: urllib3 (1.13.1) or chardet (3.0.4) doesn't match a supported version!
    
    * Solve issue. /usr/local/lib/python2.7/dist-packages/requests/__init__.py:80: RequestsDependencyWarning: urllib3 (1.13.1) or chardet (3.0.4) doesn't match a supported version!

--- pai-management/container-setup.sh
+++ pai-management/container-setup.sh
@@ -17,6 +17,12 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
+echo y | pip uninstall requests
+echo y | pip install requests
+echo y | pip uninstall docopt
+echo y | pip install docopt
+
 cp -r /docker/* /usr/bin/
 
 docker run hello-world  || exit $?
