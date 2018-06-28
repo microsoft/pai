1868c47e84aedd70c59276dc2b1e673b132014e5
qyyy <qyyy@users.noreply.github.com>
2018-06-28 13:16:32 +0800

    Update Consumer.py

--- examples/kafka/python-kafka-test/Consumer.py
+++ examples/kafka/python-kafka-test/Consumer.py
@@ -1,12 +1,6 @@
 from kafka import KafkaConsumer
-from hdfs.client import Client
 import time
 
-path = outdir.split('/')
-hdfs_host = 'http://' + path[2].split(':')[0] + ':50070'
-file_path = '/' + '/'.join(path[3:])
-client = Client(hdfs_host)
-print(hdfs_host)
 consumer = KafkaConsumer('test', bootstrap_servers=host, group_id='test_group', auto_offset_reset='earliest')
 print 'Consumer start!'
 now = time.time()
@@ -18,4 +12,3 @@ with open('output.log','a') as p:
         count += 1
         if count == 50 or time.time()-now > 10:
             break
-client.upload(file_path,'output.log')
