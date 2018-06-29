a114f149a01fcdc31fbf3fc069eb12179d4ce33a
qyyy <qyyy@users.noreply.github.com>
2018-06-29 09:52:04 +0800

    Update Producer.py

--- examples/kafka/python-kafka-test/Producer.py
+++ examples/kafka/python-kafka-test/Producer.py
@@ -1,7 +1,11 @@
 from kafka import KafkaProducer
+import argparse
 import json
 
-producer = KafkaProducer(bootstrap_servers=host, value_serializer=lambda v: json.dumps(v).encode('utf-8'), acks='all')
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', type=str, default=None)
+args = parser.parse_args()
+producer = KafkaProducer(bootstrap_servers=args.host, value_serializer=lambda v: json.dumps(v).encode('utf-8'), acks='all')
 for i in range(100):
     producer.send('test', {i:'The new ' + str(i) + 'th number'})
 producer.flush()
