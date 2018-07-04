f89144eb10b46dbcc7d2b0d6347f9bceecf106b4
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2018-07-04 13:17:04 +0800

    REST Server: lint

--- rest-server/src/config/express.js
+++ rest-server/src/config/express.js
@@ -46,7 +46,7 @@ app.use('/api/v1', router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError('Not Found', 'ERR_NO_API', `API ${req.url} not found.`))
+  next(createError('Not Found', 'ERR_NO_API', `API ${req.url} not found.`));
 });
 
 // error handler
