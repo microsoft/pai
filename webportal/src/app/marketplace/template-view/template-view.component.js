77cecef8f78d295dd47f8113ecdaa8a9d5bb3edd
shaochen shi <shishaochen_ha@sina.com>
2018-07-21 13:00:42 +0800

    When search empty string, refresh the page.

--- webportal/src/app/marketplace/template-view/template-view.component.js
+++ webportal/src/app/marketplace/template-view/template-view.component.js
@@ -31,12 +31,6 @@ const templateModalComponent = require('./template-modal.component');
 const loading = require('../../job/loading/loading.component');
 const webportalConfig = require('../../config/webportal.config.json');
 
-let tables = {'#job-table': null,
-              '#data-table': null,
-              '#script-table': null,
-              '#dockerimage-table': null,
-            };
-
 $('#content-wrapper').html(templateViewComponent({
   breadcrumb: breadcrumbComponent,
   loading: loadingComponent,
@@ -47,70 +41,6 @@ const generateQueryString = function(data) {
   return '?type=' + encodeURIComponent(data.type) + '&name=' + encodeURIComponent(data.name) + '&version='
     + encodeURIComponent(data.version);
 };
-
-/*
-const loadTemplates = function(name) {
-  const tablename = '#' + name + '-table';
-  const $table = $(tablename)
-    .on('preXhr.dt', loading.showLoading)
-    .on('xhr.dt', loading.hideLoading);
-
-  tables[tablename] = $table.dataTable({
-    'ajax': {
-      url: `${webportalConfig.restServerUri}/api/v1/template/${name}`,
-      type: 'GET',
-      dataSrc: (data) => {
-        if (data.code) {
-          alert(data.message);
-        } else {
-          return data;
-        }
-      },
-    },
-    'columns': [
-      {
-        title: 'Template',
-        data: 'name',
-      },
-      {
-        title: 'Used',
-        data: 'count',
-      },
-      {
-        title: 'Description',
-        data: 'description',
-        orderable: false,
-      },
-      {
-        title: 'Operation',
-        data: generateQueryString,
-        orderable: false,
-        searchable: false,
-        render: function(qs, type) {
-          return '<button class="btn btn-default btn-sm" onclick="window.location.href=\'/import.html'
-            + qs + '\'">Use</button>';
-        },
-      },
-    ],
-    'order': [
-      [1, 'desc'],
-    ],
-    //'scrollY': (($(window).height() - 265)) + 'px',
-    'lengthMenu': [[20, 50, 100, -1], [20, 50, 100, 'All']],
-    'deferRender': true,
-    'autoWidth': false,
-  }).api();
-};
-
-$(window).resize(function(event) {
-  $('#content-wrapper').css({'height': (($(window).height() - 200)) + 'px'});
-  Object.keys(tables).forEach((name) => {
-    if (tables[name] != null) {
-      tables[name].columns.adjust().draw();
-    }
-  });
-});
-*/
 
 const generateUI = function(type, data) {
   let htmlstr = '';
@@ -155,10 +85,8 @@ const generateUI = function(type, data) {
 
 const loadTemplates = function(name) {
   const tablename = '#' + name + '-table';
-  const $table = $(tablename)
-    .on('preXhr.dt', loading.showLoading)
+  $(tablename).on('preXhr.dt', loading.showLoading)
     .on('xhr.dt', loading.hideLoading);
-
   $.ajax({
     url: `${webportalConfig.restServerUri}/api/v1/template/${name}`,
     type: 'GET',
@@ -169,6 +97,34 @@ const loadTemplates = function(name) {
   });
 };
 
+const search = function(event) {
+  if (!event.keyCode || event.keyCode == 13) {
+    var query = $('#search').val();
+    if (query) {
+      $.ajax({
+        url: `${webportalConfig.restServerUri}/api/v1/template?query=` + encodeURIComponent(query),
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+          let categories = {'data': [], 'dockerimage': [], 'script': [], 'job': []};
+          data.forEach((item) => {
+              categories[item.type].push(item);
+          });
+          Object.keys(categories).forEach((type) => {
+            $('#' + type + '-table').html(generateUI(type, categories[type]));
+          }); 
+        }
+      });
+    } else {
+      // No query, list popular templates
+      window.location.reload(false);
+    }
+  }
+};
+
+$('#btn-search').click(search);
+$('#search').on('keyup', search);
+
 $(window).resize(function(event) {
   $('#content-wrapper').css({'height': (($(window).height() - 200)) + 'px'});
 });
@@ -177,42 +133,6 @@ $('#btn-share').click(function(event) {
   $('#modalPlaceHolder').html(templateModalComponent.generateHtml());
   templateModalComponent.initializeComponent();
   $('#shareModal').modal('show');
-});
-
-$('#btn-search').click(function(event) {
-  $.ajax({
-    url: `${webportalConfig.restServerUri}/api/v1/template?query=` + $('#search').val(),
-    type: 'GET',
-    dataType: 'json',
-    success: function (data) {
-      let categories = {'data': [], 'dockerimage': [], 'script': [], 'job': []};
-      data.forEach((item) => {
-          categories[item.type].push(item);
-      });
-      Object.keys(categories).forEach((type) => {
-        $('#' + type + '-table').html(generateUI(type, categories[type]));
-      }); 
-    }
-  });
-});
-
-$('#search').on('keyup', function (e) {
-  if (e.keyCode == 13) {
-    $.ajax({
-      url: `${webportalConfig.restServerUri}/api/v1/template?query=` + $('#search').val(),
-      type: 'GET',
-      dataType: 'json',
-      success: function (data) {
-        let categories = {'data': [], 'dockerimage': [], 'script': [], 'job': []};
-        data.forEach((item) => {
-            categories[item.type].push(item);
-        });
-        Object.keys(categories).forEach((type) => {
-          $('#' + type + '-table').html(generateUI(type, categories[type]));
-        }); 
-      }
-    });
-  }
 });
 
 $(document).ready(() => {
