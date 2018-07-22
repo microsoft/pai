434b2e7ad9a2404af1456399c2c4d05684a094dd
Yifan Xiong <v-yifxio@microsoft.com>
2018-07-22 18:22:11 +0800

    Split template detail page into components
    Split Overview, Q & A, Review & Rating, Experiment tabs into components.

--- webportal/src/app/marketplace/template-view/template-detail.component.js
+++ webportal/src/app/marketplace/template-view/template-detail.component.js
@@ -16,52 +16,54 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-require('./template-detail.component.scss');
-
+// module dependencies
 const breadcrumbComponent = require('../../job/breadcrumb/breadcrumb.component.ejs');
 const webportalConfig = require('../../config/webportal.config.json');
-const detailComponent = require('./template-detail.component.ejs');
+const templateDetailComponent = require('./template-detail.component.ejs');
+const detailOverviewComponent = require('./detail-overview.component.ejs');
+const detailQaComponent = require('./detail-qa.component.ejs');
+const detailReviewComponent = require('./detail-review.component.ejs');
+const detailExperimentComponent = require('./detail-experiment.component.ejs');
 
-const loadSummary = function() {
+require('./template-detail.component.scss');
+
+
+const loadSummary = () => {
   const searchParams = new URLSearchParams(window.location.search);
-  let type = searchParams.get('type');
-  let name = searchParams.get('name');
-  let version = searchParams.get('version');
+  const type = searchParams.get('type');
+  const name = searchParams.get('name');
+  const version = searchParams.get('version');
   if (!type || !name || !version) {
     window.location.href = "/template.html"
   } else {
-    $('#txt-name').text(name);
     $.ajax({
       url: `${webportalConfig.restServerUri}/api/v1/template/${type}/${name}/${version}`,
       type: 'GET',
       dataType: 'json',
-      success: function (data) {
-        var template = data.job ? data.job : data;
-        $('#txt-contributor').text(template.contributor);
-        $('#txt-description').text(template.description);
+      success: function(data) {
+        const template = data.job ? data.job : data;
+        const templateDetailHtml = templateDetailComponent({
+          breadcrumb: breadcrumbComponent,
+          overview: detailOverviewComponent,
+          qa: detailQaComponent,
+          review: detailReviewComponent,
+          experiment: detailExperimentComponent,
+          detail: {
+            type,
+            name,
+            version,
+            description: template.description,
+            contributor: template.contributor,
+          },
+        });
+        $('#content-wrapper').html(templateDetailHtml);
       }
     });
-    $('#btn-use').click(function(event) {
-      window.location.href = "/import.html" + window.location.search
+    $('#btn-use').click((event) => {
+      window.location.href = "/import.html" + window.location.search;
     });
   }
-}
-
-$('#content-wrapper').html(detailComponent({
-  breadcrumbHtml: breadcrumbComponent({
-    breadcrumbTitle: 'Template',
-    pages: [
-      {
-        title: 'Marketplace',
-        path: '/template.html'
-      },
-      {
-        title: 'Template',
-        path: '#'
-      },
-    ]
-  }),
-}));
+};
 
 // detail page start
 $(document).ready(() => {
