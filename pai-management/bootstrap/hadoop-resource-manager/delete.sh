3745d47a0a77aa756f6e94d8e4a94dde117188f7
ydye <yuye@microsoft.com>
2018-08-08 11:45:32 +0800

    issue fix

--- pai-management/bootstrap/hadoop-resource-manager/delete.sh
+++ pai-management/bootstrap/hadoop-resource-manager/delete.sh
@@ -26,7 +26,7 @@ echo "Create hadoop-delete configmap for deleting data on the host"
 kubectl create configmap hadoop-resource-manager-delete --from-file=hadoop-resource-manager-delete/ --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
 
 echo "Create cleaner daemon"
-kubectl apply --overwrite=true create -f delete.yaml || exit $?
+kubectl apply --overwrite=true -f delete.yaml || exit $?
 sleep 5
 
 PYTHONPATH="../.." python -m  k8sPaiLibrary.monitorTool.check_pod_ready_status -w -k app -v delete-batch-job-hadoop-resource-manager || exit $?
