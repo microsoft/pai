4570d07113529409bb9fcdd24fd14b2e780c7e25
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2018-08-31 20:13:08 +0800

    implement request rendering of marketplace homepage

--- webportal/src/app/marketplace/marketplace.component.js
+++ webportal/src/app/marketplace/marketplace.component.js
@@ -16,6 +16,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 const template = require('./marketplace.component.ejs');
+const slideTemplate = require('./marketplace.slide.component.ejs');
+const webportalConfig = require('../config/webportal.config.js');
+const url = require('url');
 require('./marketplace.component.css');
 require('slick-carousel');
 require('slick-carousel/slick/slick.css');
@@ -23,38 +26,155 @@ const userAuth = require('../user/user-auth/user-auth.component');
 const webportalConfig = require('../config/webportal.config.js');
 // require('slick-carousel/slick/slick-theme.css');
 
+const slideContext = {
+    parseType: function parseType(rawType) {
+        return {
+            'job': 'job',
+            'dockerimage': 'docker',
+            'script': 'script',
+            'data': 'data',
+        }[rawType] || 'job';
+    },
+};
+
+function prepareCarousel($carousel) {
+    const $prev = $('<a class="btn btn-link"><span class="glyphicon glyphicon-menu-left"></span></a>')
+        .appendTo($carousel);
+    const $slick = $('<div></div>')
+        .appendTo($carousel);
+    const $next = $('<a class="btn btn-link"><span class="glyphicon glyphicon-menu-right"></span></a>')
+        .appendTo($carousel);
+    $carousel.empty().append($prev, $slick, $next);
+
+    setTimeout(function() {
+        $slick.slick({
+            slidesToShow: 5,
+            swipe: false,
+            infinite: false,
+            prevArrow: $prev,
+            nextArrow: $next,
+        });
+    });
+
+    return $slick;
+}
+
+function loadCarousel($slick, type, page) {
+    const requestId = Date.now();
+    $slick.data('request-id', requestId);
+
+    const url = `${webportalConfig.restServerUri}/api/v2/template/${type}`;
+
+    setTimeout(apply, 0, 1);
+
+    return;
+
+    function apply(page) {
+        if ($slick.data('request-id') !== requestId) return;
+        $.getJSON(url, {pageno: page})
+            .then(function(data) {
+                if ($slick.data('request-id') !== requestId) return;
+
+                $(slideTemplate.call(slideContext, data)).each(function() {
+                    $slick.slick('slickAdd', this);
+                });
+
+                if (data.pageNo * data.pageSize < Math.min(data.totalCount, 30)) {
+                    setTimeout(apply, 100, page + 1);
+                }
+            });
+    }
+}
+
+function search(query) {
+    function clear($slick) {
+        const slideCount = $slick.find('.thumbnail').length;
+        for (let i = 0; i < slideCount; i += 1) {
+            $slick.slick('slickRemove');
+        }
+        return $slick;
+    }
+
+    const requestId = Date.now();
+
+    const $jobsSlick = clear($('#marketplace-jobs .slick-slider')).data('request-id', requestId);
+    const $dockersSlick = clear($('#marketplace-dockers .slick-slider')).data('request-id', requestId);
+    const $scriptsSlick = clear($('#marketplace-scripts .slick-slider')).data('request-id', requestId);
+    const $dataSlick = clear($('#marketplace-datas .slick-slider')).data('request-id', requestId);
+
+    const url = `${webportalConfig.restServerUri}/api/v2/template`;
+
+    setTimeout(append, 0, 1);
+
+    function append(page) {
+        if ($jobsSlick.data('request-id') !== requestId) return;
+
+        $.getJSON(url, {query: query, pageno: page})
+            .then(function(data) {
+                if ($jobsSlick.data('request-id') !== requestId) return;
+
+                const items = data.items;
+
+                $(slideTemplate.call(slideContext, {
+                    items: items.filter(function(item) {
+                        return item.type === 'job';
+                    }),
+                })).each(function() {
+                    $jobsSlick.slick('slickAdd', this);
+                });
+                $(slideTemplate.call(slideContext, {
+                    items: items.filter(function(item) {
+                        return item.type === 'dockerimage';
+                    }),
+                })).each(function() {
+                    $dockersSlick.slick('slickAdd', this);
+                });
+                $(slideTemplate.call(slideContext, {
+                    items: items.filter(function(item) {
+                        return item.type === 'script';
+                    }),
+                })).each(function() {
+                    $scriptsSlick.slick('slickAdd', this);
+                });
+                $(slideTemplate.call(slideContext, {
+                    items: items.filter(function(item) {
+                        return item.type === 'docker';
+                    }),
+                })).each(function() {
+                    $dataSlick.slick('slickAdd', this);
+                });
+
+                if (data.pageNo * data.pageSize < Math.min(data.totalCount, 150)) {
+                    setTimeout(append, 100, page + 1);
+                }
+            });
+    }
+}
+
+$('#sidebar-menu--template-view').addClass('active');
+
 $(function() {
-    $('#content-wrapper').html(template());
+    const query = url.parse(window.location.href, true).query;
+    $('#content-wrapper').html(template(query));
+    $('#search').submit(function(event) {
+        event.preventDefault();
+        search(this.find('input').val());
+    });
+    prepareCarousel($('#marketplace-jobs'));
+    prepareCarousel($('#marketplace-dockers'));
+    prepareCarousel($('#marketplace-scripts'));
+    prepareCarousel($('#marketplace-datas'));
 
     setTimeout(() => {
-        $('#marketplace-jobs').slick({
-            infinite: true,
-            slidesToShow: 5,
-            swipe: false,
-            prevArrow: $('#marketplace-jobs-prev'),
-            nextArrow: $('#marketplace-jobs-next'),
-        });
-        $('#marketplace-dockers').slick({
-            infinite: true,
-            slidesToShow: 5,
-            swipe: false,
-            prevArrow: $('#marketplace-dockers-prev'),
-            nextArrow: $('#marketplace-dockers-next'),
-        });
-        $('#marketplace-scripts').slick({
-            infinite: true,
-            slidesToShow: 5,
-            swipe: false,
-            prevArrow: $('#marketplace-scripts-prev'),
-            nextArrow: $('#marketplace-scripts-next'),
-        });
-        $('#marketplace-datas').slick({
-            infinite: true,
-            slidesToShow: 5,
-            swipe: false,
-            prevArrow: $('#marketplace-datas-prev'),
-            nextArrow: $('#marketplace-datas-next'),
-        });
+        if (query.query) {
+            $('#search input').val(query.query);
+            search(query.query)
+        } else {
+            loadCarousel($('#marketplace-jobs .slick-slider'), 'job');
+            loadCarousel($('#marketplace-dockers .slick-slider'), 'dockerimage');
+            loadCarousel($('#marketplace-scripts .slick-slider'), 'script');
+            loadCarousel($('#marketplace-datas .slick-slider'), 'data');
+        }
 
         $('#upload-button').click(() => {
             $('#upload-body-select').removeClass('hidden');
