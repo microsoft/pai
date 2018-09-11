45fb9385db65538d16b5feb2929e67773bc0eeb8
YundongYe <YundongYe@outlook.com>
2018-09-11 09:48:17 +0800

    Add support for random docker root dir for kubelet (#1230)

--- pai-management/k8sPaiLibrary/maintaintool/docker-info-root-dir.py
+++ pai-management/k8sPaiLibrary/maintaintool/docker-info-root-dir.py
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Copyright (c) Microsoft Corporation
 # All rights reserved.
 #
@@ -17,30 +15,17 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-set -e
+from __future__ import print_function
 
-scriptPath=$1
-
-
-#stop all swap on the machine
-
-swapoff -a
-
-# check etc/ exist or not.
-staticpod="$scriptPath/etc"
-if [ -d "$staticpod" ]; then
-
-    cp -r $scriptPath/etc /
-
-fi
-
-manifestpath="/etc/kubernetes/manifests"
-if [ ! -d "$manifestpath" ]; then
-
-    mkdir -p $manifestpath
-
-fi
+import docker
+import sys
 
 
-chmod u+x $scriptPath/kubelet.sh
-./$scriptPath/kubelet.sh $scriptPath
+client = docker.from_env()
+docker_info_dict = client.info()
+
+if 'DockerRootDir' in docker_info_dict:
+    print(docker_info_dict['DockerRootDir'])
+else:
+    print("Failed to get DockerRootDir through docker api.")
+    sys.exit(1)
