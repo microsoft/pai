7100d32816ee30a5c1a5c607dfad5fd985ea0312
Di Xu <xudifsd@gmail.com>
2018-09-19 12:54:10 +0800

    collect network metrics for containers

--- src/job-exporter/src/job_exporter.py
+++ src/job-exporter/src/job_exporter.py
@@ -24,6 +24,7 @@ import logging
 import docker_stats
 import docker_inspect
 import gpu_exporter
+import network
 import utils
 from utils import Metric
 
@@ -50,7 +51,7 @@ pai_services = map(lambda s: "k8s_" + s, [
     "hadoop-data-node",
     "zookeeper",
     "node-exporter",
-    "gpu-exporter",
+    "job-exporter",
     "yarn-exporter",
     "nvidia-drivers"
 ])
@@ -71,7 +72,7 @@ def parse_from_labels(labels):
     return gpuIds, otherLabels
 
 
-def collect_job_metrics(gpuInfos):
+def collect_job_metrics(gpu_infos, all_conns):
     stats = docker_stats.stats()
     if stats is None:
         logger.warning("docker stats returns None")
@@ -87,28 +88,41 @@ def collect_job_metrics(gpuInfos):
                 pai_service_name = service_name[4:] # remove "k8s_" prefix
                 break
 
-        if pai_service_name is None:
-            inspectInfo = docker_inspect.inspect(container_id)
-            if inspectInfo is None or not inspectInfo["labels"]:
-                continue
+        inspect_info = docker_inspect.inspect(container_id)
+        pid = inspect_info["pid"] if inspect_info is not None else None
+        inspect_labels = utils.walk_json_field_safe(inspect_info, "labels")
 
-            gpuIds, otherLabels = parse_from_labels(inspectInfo["labels"])
-            otherLabels.update(inspectInfo["env"])
+        if not inspect_labels and pai_service_name is None:
+            continue # other container, maybe kubelet or api-server
+
+        # get network consumption, since all our services/jobs running in host network,
+        # network statistic from docker is not specific to that container. We have to
+        # get network statistic by ourselves.
+        lsof_result = network.lsof(pid)
+        net_in, net_out = network.get_container_network_metrics(all_conns, lsof_result)
+        if logger.isEnabledFor(logging.DEBUG):
+            debug_info = utils.check_output("ps -o cmd fp {0} | tail -n 1".format(pid), shell=True)
+
+            logger.debug("pid %s with cmd `%s` has lsof result %s, in %d, out %d",
+                    pid, debug_info, lsof_result, net_in, net_out)
+
+        if pai_service_name is None:
+            gpuIds, otherLabels = parse_from_labels(inspect_info["labels"])
+            otherLabels.update(inspect_info["env"])
 
             for id in gpuIds:
-                if gpuInfos:
-                    logger.info(gpuInfos)
+                if gpu_infos:
                     labels = copy.deepcopy(otherLabels)
                     labels["minor_number"] = id
 
-                    result.append(Metric("container_GPUPerc", labels, gpuInfos[id]["gpuUtil"]))
-                    result.append(Metric("container_GPUMemPerc", labels, gpuInfos[id]["gpuMemUtil"]))
+                    result.append(Metric("container_GPUPerc", labels, gpu_infos[id]["gpuUtil"]))
+                    result.append(Metric("container_GPUMemPerc", labels, gpu_infos[id]["gpuMemUtil"]))
 
             result.append(Metric("container_CPUPerc", otherLabels, stats["CPUPerc"]))
             result.append(Metric("container_MemUsage", otherLabels, stats["MemUsage_Limit"]["usage"]))
             result.append(Metric("container_MemLimit", otherLabels, stats["MemUsage_Limit"]["limit"]))
-            result.append(Metric("container_NetIn", otherLabels, stats["NetIO"]["in"]))
-            result.append(Metric("container_NetOut", otherLabels, stats["NetIO"]["out"]))
+            result.append(Metric("container_NetIn", otherLabels, net_in))
+            result.append(Metric("container_NetOut", otherLabels, net_out))
             result.append(Metric("container_BlockIn", otherLabels, stats["BlockIO"]["in"]))
             result.append(Metric("container_BlockOut", otherLabels, stats["BlockIO"]["out"]))
             result.append(Metric("container_MemPerc", otherLabels, stats["MemPerc"]))
@@ -118,8 +132,8 @@ def collect_job_metrics(gpuInfos):
             result.append(Metric("service_mem_usage_byte", labels, stats["MemUsage_Limit"]["usage"]))
             result.append(Metric("service_mem_limit_byte", labels, stats["MemUsage_Limit"]["limit"]))
             result.append(Metric("service_mem_usage_percent", labels, stats["MemPerc"]))
-            result.append(Metric("service_net_in_byte", labels, stats["NetIO"]["in"]))
-            result.append(Metric("service_net_out_byte", labels, stats["NetIO"]["out"]))
+            result.append(Metric("service_net_in_byte", labels, net_in))
+            result.append(Metric("service_net_out_byte", labels, net_out))
             result.append(Metric("service_block_in_byte", labels, stats["BlockIO"]["in"]))
             result.append(Metric("service_block_out_byte", labels, stats["BlockIO"]["out"]))
 
@@ -144,8 +158,11 @@ def main(argv):
             gpu_metrics = gpu_exporter.convert_gpu_info_to_metrics(gpu_infos)
             utils.export_metrics_to_file(gpu_metrics_path, gpu_metrics)
 
+            all_conns = network.iftop()
+            logger.debug("iftop result is %s", all_conns)
+
             # join with docker stats metrics and docker inspect labels
-            job_metrics = collect_job_metrics(gpu_infos)
+            job_metrics = collect_job_metrics(gpu_infos, all_conns)
             utils.export_metrics_to_file(job_metrics_path, job_metrics)
         except Exception as e:
             logger.exception("exception in job exporter loop")
