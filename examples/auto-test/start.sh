103b79e96660de0b394e985d75e4a1ced8d1d981
yuqian <t-yuqia@microsoft.com>
2018-09-19 11:25:45 +0900

    some fix

--- examples/auto-test/start.sh
+++ examples/auto-test/start.sh
@@ -1,5 +1,5 @@
 echo "Run with parameter ci/release/normal!"
-if [ ! -d "/pai" ]; then
+if [ ! -d "pai/" ]; then
     git clone -b yuqian/examples_fix https://github.com/Microsoft/pai.git
     mv pai/examples/ . && rm -rf pai/
     if [ $1 == "ci" ]; then
@@ -9,7 +9,8 @@ if [ ! -d "/pai" ]; then
     else
         threshold=30
     fi
-    python3 start_all_test.py --path ./examples --threshold $threshold --rest_server_socket $2 --hdfs_socket $3 --webhdfs_socket $4 --PAI_username $5 --PAI_password $6
+    abspath=`pwd`/pai_tmp/examples/auto-test
+    python3 $abspath/start_all_test.py --path ./examples --threshold $threshold --rest_server_socket $2 --hdfs_socket $3 --webhdfs_socket $4 --PAI_username $5 --PAI_password $6
     rm -rf ./examples/
 else
     echo "Pai folder already exist! Please remove it or run this project in another folder!"
