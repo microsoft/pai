3ee6c140503f3483e5ae11be61f99f1ee31b6169
ydye <yuye@microsoft.com>
2018-09-30 10:48:34 +0800

    Init commit

--- deployment/paiLibrary/common/kubernetes_test.py
+++ deployment/paiLibrary/common/kubernetes_test.py
@@ -6,3 +6,5 @@ kube_config_path = "/root/.kube/config"
 node_list = kubernetes_handler.list_all_nodes(kube_config_path)
 print node_list[0].metadata.name
 print node_list[0].status.addresses
+print node_list[0].status.addresses[0].type
+print node_list[0].status.addresses[0].address
