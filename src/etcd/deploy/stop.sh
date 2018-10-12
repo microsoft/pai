7559bd506ccf38f8ca7a455fe944768ce4ef94dd
YanjieGao <yanjga@microsoft.com>
2018-10-12 10:02:52 +0000

    add the stop.sh

--- src/etcd/deploy/stop.sh
+++ src/etcd/deploy/stop.sh
@@ -19,4 +19,8 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+kubectl delete -f etcd-operator.yaml 
+kubectl delete rs --selector=name=etcd-operator
+kubectl delete pods --selector=name=etcd-operator
 kubectl delete -f etcd.yaml
+kubectl  delete pods -l app=etcd
