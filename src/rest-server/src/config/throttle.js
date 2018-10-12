b15b51fd78b5d5f6ec6d902f3033398aa9d493b1
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2018-10-12 19:46:25 +0800

    Add throttle middleware

--- src/rest-server/src/config/throttle.js
+++ src/rest-server/src/config/throttle.js
@@ -4,7 +4,7 @@
 // MIT License
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-// documentation files (the 'Software'), to deal in the Software without restriction, including without limitation
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
 // to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -15,39 +15,19 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import { HttpError } from "http-errors";
+const Bottleneck = require('bottleneck').default;
 
-declare type Status =
-    number |
-    'Bad Request' |
-    'Conflict' |
-    'Forbidden' |
-    'Internal Server Error' |
-    'Not Found' |
-    'Too Many Requests' |
-    'Unauthorized';
-declare type Code =
-    'BadConfigurationError' |
-    'ConflictJobError' |
-    'ConflictUserError' |
-    'ForbiddenUserError' |
-    'IncorrectPasswordError' |
-    'InvalidParametersError' |
-    'NoApiError' |
-    'NoJobError' |
-    'NoJobConfigError' |
-    'NoJobSshInfoError' |
-    'NoUserError' |
-    'NoVirtualClusterError' |
-    'ReadOnlyJobError' |
-    'RemoveAdminError' |
-    'TooManyRequestsError' |
-    'UnauthorizedUserError' |
-    'UnknownError';
+// Per user configuration
+exports.user = {
+    maxConcurrent: Number(process.env.USER_MAX_CONCURRENT) || 2,
+    highWater: Number(process.env.USER_HIGH_WATER) || 5,
+    // When adding a new job to a limiter, if the queue length reaches highWater, do not add the new job.
+    strategy: Bottleneck.strategy.OVERFLOW,
+};
 
-declare function createError(status: Status, code: Code, message: string): HttpError;
-declare namespace createError {
-    declare function unknown(cause: Error | string): HttpError;
-}
-
-export = createError;
+exports.anonymous = {
+    maxConcurrent: Number(process.env.ANOMYMOUS_MAX_CONCURRENT) || 20,
+    highWater: Number(process.env.ANOMYMOUS_HIGH_WATER) || 50,
+    // When adding a new job to a limiter, if the queue length reaches highWater, do not add the new job.
+    strategy: Bottleneck.strategy.OVERFLOW,
+};
