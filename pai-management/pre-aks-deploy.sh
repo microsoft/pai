fdbf757467ccb8b4215ca526e3f01493a0d60d6d
YanjieGao <yanjga@microsoft.com>
2018-11-07 09:51:24 +0000

    add preaksdeploy

--- pai-management/pre-aks-deploy.sh
+++ pai-management/pre-aks-deploy.sh
@@ -1,23 +1,33 @@
-SUBSCRIPTION=""
-RESOURCE_GROUP=""
-RESOURCE_NAME=""
-RESOURCE_GROUP_DETAIL=""
-RESOURCE_NAME=""
-VM_USER_NAME=""
-VM_PASSWORD=""
-echo "pls login azure cli:"
+subscriptionName=""
+resourceGroup=""
+name=""
+nodeResourceGroup=""
+vmUsername=""
+vmPassword=""
+inAKS=false
+
 az login
-echo $SUBSCRIPTION
-az account set --subscription "$SUBSCRIPTION"
-echo $RESOURCE_GROUP
-az aks get-credentials --resource-group "$RESOURCE_GROUP" --name "$RESOURCE_NAME"
-az vm list-ip-addresses --resource-group "$RESOURCE_GROUP_DETAIL" -o table
-for i in `az vm list-ip-addresses --resource-group $RESOURCE_GROUP_DETAIL -o table`;
-   do
-   echo $i is line;
-   if [[ $i =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-   else
-      az vm user update -u "azureuser" -p "Azureuser001" -n $i -g $RESOURCE_GROUP --name $RESOURCE_NAME
-      echo "change password for node $i" 
-   fi
-   done
+az account set --subscription "$subscriptionName"
+az aks get-credentials --resource-group "$resourceGroup" --name "$name"
+az vm list-ip-addresses --resource-group "$nodeResourceGroup" -o table
+if $inAKS; then
+echo "enter in AKS config"
+    for i in `az vm list-ip-addresses --resource-group $nodeResourceGroup -o table`;
+    do
+    if [[ $i =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+       az vm user update -u "$vmUsername" -p "$vmPassword" -n $i -g $resourceGroup --name $name
+       echo "change password for node $i"
+    fi
+    done
+else
+echo "enter not in AKS config"
+    for i in `az vm list-ip-addresses --resource-group $nodeResourceGroup -o table`;
+    do
+    if [[ $i =~ aks-agentpool* ]]; then
+       echo "label and deploy dev-box for node $i"
+       kubectl label nodes $i dev-box=true
+       kubectl apply -f dev-box-k8s-deploy.yaml
+       break;
+    fi
+    done
+fi
