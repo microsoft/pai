abca48643bfd88176f5d571d318ae7ec11730459
yuqian <t-yuqia@microsoft.com>
2018-11-08 14:24:02 +0900

    add username to auto-test for HDFS path

--- examples/cntk/prepare.sh
+++ examples/cntk/prepare.sh
@@ -20,19 +20,19 @@ function prepare_data(){
     
     #upload data to HDFS
 	echo "Uploading cntk data, waiting..."
-    hdfs dfs -put cmudict-0.7b hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.mapping hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.test hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.test.ctf hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.test.txt hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-1-21 hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-1-21.ctf hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-1-21.txt hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-20-21 hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-20-21.ctf hdfs://$1/examples/cntk/data
-    hdfs dfs -put cmudict-0.7b.train-dev-20-21.txt hdfs://$1/examples/cntk/data
-    hdfs dfs -put tiny.ctf hdfs://$1/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.mapping hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.test hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.test.ctf hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.test.txt hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-1-21 hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-1-21.ctf hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-1-21.txt hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-20-21 hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-20-21.ctf hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put cmudict-0.7b.train-dev-20-21.txt hdfs://$1/$2/examples/cntk/data
+    hdfs dfs -put tiny.ctf hdfs://$1/$2/examples/cntk/data
 }
 
 function prepare_code(){
@@ -46,8 +46,8 @@ function prepare_code(){
 
     #upload code to HDFS
 	echo "Uploading cntk code, waiting..."
-    hdfs dfs -put cntk-g2p.sh hdfs://$1/examples/cntk/code
-    hdfs dfs -put G2P.cntk hdfs://$1/examples/cntk/code
+    hdfs dfs -put cntk-g2p.sh hdfs://$1/$2/examples/cntk/code
+    hdfs dfs -put G2P.cntk hdfs://$1/$2/examples/cntk/code
 }
 
 if [ $# != 1 ]; then
@@ -57,23 +57,23 @@ fi
 
 #make directory on HDFS
 echo "Make cntk directory, waiting..."
-hdfs dfs -mkdir -p hdfs://$1/examples/cntk/code
-hdfs dfs -mkdir -p hdfs://$1/examples/cntk/data
-hdfs dfs -mkdir -p hdfs://$1/examples/cntk/output
+hdfs dfs -mkdir -p hdfs://$1/$2/examples/cntk/code
+hdfs dfs -mkdir -p hdfs://$1/$2/examples/cntk/data
+hdfs dfs -mkdir -p hdfs://$1/$2/examples/cntk/output
 
-hdfs dfs -test -e hdfs://$1/examples/cntk/code/*
+hdfs dfs -test -e hdfs://$1/$2/examples/cntk/code/*
 if [ $? -eq 0 ] ;then
     echo "Code exists on HDFS!"
 else
-    prepare_code $1
+    prepare_code $1 $2
     echo "Have prepared code!"
 fi
 
-hdfs dfs -test -e hdfs://$1/examples/cntk/data/*
+hdfs dfs -test -e hdfs://$1/$2/examples/cntk/data/*
 if [ $? -eq 0 ] ;then
     echo "Data exists on HDFS!"
 else
-    prepare_data $1
+    prepare_data $1 $2
     echo "Have prepared data"
 fi
 
