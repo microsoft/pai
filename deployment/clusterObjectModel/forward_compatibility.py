23e0c54bd6503e4afd1dd6ad4cdb7c452dc82aa4
yuye@microsoft.com <yuye@microsoft.com>
2018-11-28 16:37:06 +0800

    Add validation-post

--- deployment/clusterObjectModel/forward_compatibility.py
+++ deployment/clusterObjectModel/forward_compatibility.py
@@ -20,6 +20,39 @@
 def service_configuration_convert(service_configuration):
     if "rest-server" not in service_configuration and "restserver" in service_configuration:
         service_configuration["rest-server"] = service_configuration["restserver"]
+
+    if "yarn-frameworklauncher" not in service_configuration and "frameworklauncher" in service_configuration:
+        service_configuration["yarn-frameworklauncher"] = service_configuration["frameworklauncher"]
+
+
+    if "cluster" in service_configuration:
+        if "common" not in service_configuration["cluster"]:
+            if "clusterid" in service_configuration["cluster"]:
+                service_configuration["cluster"]["common"]["clusterid"] = service_configuration["cluster"]["clusterid"]
+            if "data-path" in service_configuration["cluster"]:
+                service_configuration["cluster"]["common"]["data-path"] = service_configuration["cluster"]["data-path"]
+        if "docker-registry" not in service_configuration["cluster"] and "docker-registry-info" in service_configuration["cluster"]:
+            if "docker-namespace" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["namespace"] = \
+                service_configuration["cluster"]["docker-registry-info"]["docker-namespace"]
+            if "docker-registry-domain" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["domain"] = \
+                service_configuration["cluster"]["docker-registry-info"]["docker-registry-domain"]
+            if "docker-username" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["username"] = \
+                service_configuration["cluster"]["docker-registry-info"]["docker-username"]
+            if "docker-password" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["password"] = \
+                service_configuration["cluster"]["docker-registry-info"]["docker-password"]
+            if "docker-tag" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["tag"] = \
+                service_configuration["cluster"]["docker-registry-info"]["docker-tag"]
+            if "secret-name" in service_configuration["cluster"]["docker-registry-info"]:
+                service_configuration["cluster"]["docker-registry"]["secret-name"] = \
+                service_configuration["cluster"]["docker-registry-info"]["secret-name"]
+
+
+
     return service_configuration
 
 
