b291dc214373dde5fe6234a7070e1ed2fde2aac6
yuye@microsoft.com <yuye@microsoft.com>
2018-11-30 15:19:13 +0800

    Issue FIx

--- src/pylon/config/pylon.py
+++ src/pylon/config/pylon.py
@@ -15,7 +15,7 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class Webportal:
+class Pylon:
 
     def __init__(self, cluster_configuration, service_configuration, default_service_configuraiton):
         self.cluster_configuration = cluster_configuration
