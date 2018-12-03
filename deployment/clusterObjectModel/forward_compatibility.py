00f152fe9cd1f56f4cbb4c2785775c246ce82c68
yuye@microsoft.com <yuye@microsoft.com>
2018-12-03 16:06:01 +0800

    Issue FIx

--- deployment/clusterObjectModel/forward_compatibility.py
+++ deployment/clusterObjectModel/forward_compatibility.py
@@ -22,8 +22,8 @@ def transform(old_model, old_key, new_model, new_key):
     new_key_list = new_key.split('.')
     old_dict = old_model
     new_dict = new_model
-    print old_key_list
-    print old_dict
+    if old_dict == None:
+        return
     for key in old_key_list:
         if key not in old_dict:
             return
