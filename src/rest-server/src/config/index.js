545e9299a29a7be00f294d2e0a0a0e5f7d610d85
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2018-12-11 15:00:45 +0800

    Fix control characters escaping

--- src/rest-server/src/config/index.js
+++ src/rest-server/src/config/index.js
@@ -36,14 +36,14 @@ mustache.escape = (string) => {
     .replace(/[\x20-\x24\x26-\x2A\x2C\x3B\x3C\x3E\x3F\x5B-\x5E\x60\x7B-\x7E]/g, '\\$&')
     .replace(/[\x00-\x1F\x7F]/g, (c) => ({
       /* eslint-enable no-control-regex */
-      0x07: '\\a',
-      0x08: '\\b',
-      0x09: '\\t',
-      0x0A: '\\n',
-      0x0B: '\\v',
-      0x0C: '\\f',
-      0x0D: '\\r',
-      0x1B: '\\E',
+      0x07: '\\\\a',
+      0x08: '\\\\b',
+      0x09: '\\\\t',
+      0x0A: '\\\\n',
+      0x0B: '\\\\v',
+      0x0C: '\\\\f',
+      0x0D: '\\\\r',
+      0x1B: '\\\\E',
     }[c.charCodeAt(0)] || ''));
 };
 
