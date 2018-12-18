5146cf175dfb04d2d59b04de51966cac7e1cc2fb
FAREAST\canwan <1542762413@qq.com>
2018-12-18 13:06:19 +0800

    change username to hex to have backward compability

--- src/rest-server/src/config/secret.js
+++ src/rest-server/src/config/secret.js
@@ -20,6 +20,7 @@
 const Joi = require('joi');
 const {readFileSync} = require('fs');
 const {Agent} = require('https');
+const {resolve} = require('path');
 
 let userSecretConfig = {
   apiServerUri: process.env.K8S_APISERVER_URI,
@@ -40,11 +41,13 @@ userSecretConfig.requestConfig = () => {
   };
 
   if ('K8S_APISERVER_CA_FILE' in process.env) {
+    process.env.K8S_APISERVER_CA_FILE = resolve(__dirname, 'ca.crt');
     const ca = readFileSync(process.env.K8S_APISERVER_CA_FILE);
     config.httpsAgent = new Agent({ca});
   }
 
   if ('K8S_APISERVCER_TOKEN_FILE' in process.env) {
+    process.env.K8S_APISERVER_TOKEN_FILE = resolve(__dirname, 'token');
     const token = readFileSync(process.env.K8S_APISERVER_TOKEN_FILE, 'ascii');
     config.headers['Authorization'] = `Bearer ${token}`;
   }
