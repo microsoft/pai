5465e4cf7605b80886e8ffaad753224bddf96806
yuye@microsoft.com <yuye@microsoft.com>
2018-12-26 17:10:59 +0800

    Remove unused line

--- src/hadoop-resource-manager/deploy/hadoop-resource-manager-configuration/resourcemanager-generate-script.sh
+++ src/hadoop-resource-manager/deploy/hadoop-resource-manager-configuration/resourcemanager-generate-script.sh
@@ -17,8 +17,6 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-cat /etc/hosts
-
 cp  /hadoop-configuration/core-site.xml $HADOOP_CONF_DIR/core-site.xml
 cp  /hadoop-configuration/mapred-site.xml $HADOOP_CONF_DIR/mapred-site.xml
 cp  /hadoop-configuration/yarn-site.xml $HADOOP_CONF_DIR/yarn-site.xml
