6b03499093f70236a09fc8c061f3c830cb3c9650
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-01-31 14:35:37 +0800

    Web portal: add "My jobs" filter button.

--- src/webportal/src/app/job/job-view/job-view.component.js
+++ src/webportal/src/app/job/job-view/job-view.component.js
@@ -256,7 +256,7 @@ const loadJobs = (specifiedVc) => {
           return `<a class="table-job-name" href="view.html?username=${namespace || username}&jobName=${name}"><div class="table-job-name">${name}</div></a>`;
         }
       }},
-      {title: 'User', data: 'username', responsivePriority: 2},
+      {title: 'User', name: 'user', data: 'username', responsivePriority: 2},
       {title: 'Virtual Cluster', data: 'virtualCluster', responsivePriority: 3, render(virtualCluster) {
         let vcName = virtualCluster || 'default';
         return '<a href="virtual-clusters.html?vcName=' + vcName + '">' + vcName + '</a>';
@@ -297,6 +297,44 @@ const loadJobs = (specifiedVc) => {
       '<\'row\'<\'col-sm-12\'tr>>' +
       '<\'row\'<\'col-sm-5\'i><\'col-sm-7\'p>>',
     'buttons': [
+      {
+        // Show my jobs
+        text: 'My jobs',
+        enabled: (function() {
+          let token;
+          userAuth.checkToken(function(t) {
+            token = t;
+          }, false);
+          return Boolean(token);
+        }()),
+        init(dt, node) {
+          if (userAuth.checkAdmin()) {
+            // Admins default browse all jobs
+            dt.button(node).active(false);
+            dt.column('user:name').search('');
+          } else {
+            // Users default browse their own jobs.
+            dt.button(node).active(true);
+            dt.column('user:name').search(cookies.get('user'));
+          }
+          setTimeout(function() {
+            dt.draw();
+          }, 0);
+        },
+        action(e, dt, node) {
+          const button = dt.button(node);
+          if (button.active()) {
+            button.active(false);
+            dt.column('user:name').search('');
+          } else {
+            button.active(true);
+            dt.column('user:name').search(cookies.get('user'));
+          }
+          setTimeout(function() {
+            dt.draw();
+          }, 0);
+        },
+      },
       {
         text: 'Select displayed jobs',
         action: function(e, dt) {
