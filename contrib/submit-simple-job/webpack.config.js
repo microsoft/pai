4fa71dd4a2a60113582f6898142b07978d138c1f
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-02-14 18:38:57 +0800

    Merge webpack configurations

--- contrib/submit-simple-job/webpack.config.js
+++ contrib/submit-simple-job/webpack.config.js
@@ -1,23 +1,35 @@
-const { resolve } = require('path')
+const {resolve} = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: './index.ts',
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    }],
-  },
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: 'plugin.js'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  watchOptions: {
-    ignored: /node_modules/,
-  },
+module.exports = (env) => {
+  /** @type { import('webpack').Configuration } */
+  const config = {
+    entry: './index.ts',
+    output: {},
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
+
+  if (env === 'development') {
+    config.mode = 'development';
+    config.output.path = resolve(__dirname, '..', '..',
+      'src', 'webportal', 'dist', 'scripts', 'plugins');
+    config.output.filename = 'submit-simple-job.js';
+
+    config.watch = true;
+    config.watchOptions = {ignored: /node_modules/};
+  } else {
+    config.mode = 'production';
+    config.output.path = resolve(__dirname, 'dist');
+    config.output.filename = 'plugin.js';
+  }
+
+  return config;
 };
