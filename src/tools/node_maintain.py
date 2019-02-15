63a5bee07a759f8c80230cbf46e26c83ed40ddc7
mzmssg <mzmssg@gmail.com>
2019-02-15 11:26:52 +0000

    fix bugs

--- src/tools/node_maintain.py
+++ src/tools/node_maintain.py
@@ -172,7 +172,7 @@ def refresh_yarn_nodes(args):
 
 def setup_parser():
     top_parser = argparse.ArgumentParser()
-    top_parser.add_argument("master-ip", help="master node ip")
+    top_parser.add_argument("master_ip", help="master node ip")
     top_parser.add_argument("--yarn-ip",
                             help="specify yarn resource manager ip separately, by default it's master node ip")
     top_parser.add_argument("--kubernetes-ip",
