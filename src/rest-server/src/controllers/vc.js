fd91b828ec1a1d38d3f527261d34495d3ccb98fd
zimiao <mzmssg.love@163.com>
2019-02-20 20:23:52 +0800

    fix typo

--- src/rest-server/src/controllers/vc.js
+++ src/rest-server/src/controllers/vc.js
@@ -23,8 +23,8 @@ const createError = require('../util/error');
  * Validation, not allow operation to "default" vc.
  */
 const validate = (req, res, next, vcName) => {
-  if (! /^[A-Za-z0-9._~]+$/.test(vcName)) {
-    return next(createError('Bad Request', 'InvalidParametersError', 'VC name should only container letters, numbers and ._~'));
+  if (! /^[A-Za-z0-9_,]+$/.test(vcName)) {
+    return next(createError('Bad Request', 'InvalidParametersError', 'VC name should only contain alpha-numeric and underscore characters'));
   }
   if (vcName === 'default' && req.method !== 'GET') {
     return next(createError('Forbidden', 'ForbiddenUserError', `Update operation to default vc isn't allowed`));
