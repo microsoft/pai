a9983a45e017deef93ec11b4ebf661ebd0b33656
yuye@microsoft.com <yuye@microsoft.com>
2019-03-04 14:00:52 +0800

    Add type convert

--- src/rest-server/src/config/paiConfig.js
+++ src/rest-server/src/config/paiConfig.js
@@ -32,7 +32,7 @@ try {
 
 let paiConfigData = {
     machineList: paiMachineList,
-    debuggingReservationSeconds: process.env.DEBUGGING_RESERVATION_SECONDS,
+    debuggingReservationSeconds: Number(process.env.DEBUGGING_RESERVATION_SECONDS)
 };
 
 
