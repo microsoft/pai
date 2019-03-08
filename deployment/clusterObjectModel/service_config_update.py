c653f2c59feeb7325275a155cd169cc9c24a59f1
Hao Yuan <hao1939@gmail.com>
2019-03-08 22:16:23 +0800

    upgrade docker image tag to v0.10.0

--- deployment/clusterObjectModel/service_config_update.py
+++ deployment/clusterObjectModel/service_config_update.py
@@ -31,8 +31,8 @@ class ServiceConfigUpdate:
 
 
     def run(self):
-        overwirte_service_configuration = file_handler.load_yaml_config( "{0}/services-configuration.yaml".format(self.config_path))
-        self.overwirte_service_configuration, updated = forward_compatibility.service_configuration_convert(overwirte_service_configuration)
+        service_configuration = file_handler.load_yaml_config("{0}/services-configuration.yaml".format(self.config_path))
+        self.overwirte_service_configuration, updated = forward_compatibility.service_configuration_convert(service_configuration)
 
         if updated is True:
             self.logger.warning("=======================================================================")
@@ -47,6 +47,7 @@ class ServiceConfigUpdate:
                 "mv {0}/services-configuration.yaml {0}/services-configuration.yaml.old".format(self.config_path),
                 "failed to mv the old services-configuration.yaml"
             )
+            self.overwirte_service_configuration["cluster"]["docker-registry"]["tag"] = "v0.10.0"
             file_handler.dump_yaml_data("{0}/services-configuration.yaml".format(self.config_path), self.overwirte_service_configuration)
 
             self.logger.warning("=======================================================================")
