f1b153549b588cf1de9e92d7b5d87c993a3ca63d
Qinzheng Sun <sunqinzheng@users.noreply.github.com>
2019-03-20 19:34:46 +0800

    [VS Code] view container (#2301)
    * add a dashboard in grafana to list all tasks in node (#2197)
    
    * Fix format in issue templates (#2233)
    
    Fix format in issue templates:
    - remove trailing spaces
    - change chinese colon into english
    
    * Fix auto retries when out of memory. (#1108)
    
    * Distinguish cgroup OOM from dmesg.
    
    * Remove cgroup OOM detection
    
    Make all OOM cause exiting by 5
    
    * Exit 55 when OOM
    
    * Refine homepage for new users (#2155)
    
    Updated first level bullets, to add more content for administrators and users, who is first time touch OpenPAI, or computing platform.
    
    * Fix yarn container failed when docker container exited quickly. (#2256)
    
    * REST server: remove expires in JWT payload of unit test (#2263)
    
    * Deploy: add explicit config field in webportal  plugin (#2251)
    
    * Deploy: add explicit config field in webportal  plugin
    
    * Fix json.dumps
    
    * t
    
    * fix
    
    * Update PLUGINS.md
    
    * Update webportal.md
    
    * alert on unhealthy gpu (#2209)
    
    * Pylon: fix double start query in yarn redirect (#2258)
    
    * Pylon: fix double start query in yarn redirect
    
    * Hide debug info in docker-compose.yaml
    
    * adapt user transfer script to new config (#2266)
    
    * Webportal: add pai-version attribute to <pai-plugin> (#2245)
    
    * Webportal: add pai-version attribute to <pai-plugin>
    
    * Use preprocess to apply window.PAI_VERSION
    
    * set version in layout.html
    
    * Fix ib drivers bug (#2269)
    
    * FIx ib installation script bug (#2271)
    
    * [BUG] Fix hadoop ai build path (#2262)
    
    * fix hadoop ai build bugs
    
    * refine
    
    * Web portal submit job: support init json from sessionStorage. (#2253)
    
    * YARN and HDFS log persistence  (#2244)
    
    * rm log persist
    
    * change log dir to host
    
    * persist nm log to host
    
    * resolve conflict
    
    * persist namenode log
    
    * persist data node log
    
    * add comments
    
    * move log path to common pai storage
    
    * use twisted in yarn-exporter (#2273)
    
    * [Job Debugging] Basic Implement Of Job Debugging. (#2272)
    
    * Refine document for new user to submit job (#2278)
    
    1. add new guidance to submit job for beginners.
    2. refine homepage to connect with new guidance.
    3. reorganize content of troubleshooting for next refactoring.
    4. fix links in md files.
    
    * [Drivers] Fix the issue when installing IB drivers.  (#2275)
    
    * fix can not report zombie process using gpu error (#2279)
    
    * fix external process error
    
    * add debug log
    
    * fix short ID and long ID do not match
    
    * use time based atomic ref to exchange info between threads
    
    * add test case for AtomicRef
    
    * fix bug in file remove (#2288)
    
    * fix hadoop build error (#2296)
    
    * export vc/node related metrics from yarn (#2289)
    
    * 720
    
    * open hdfs explorer in view container
    enable tslint rule "ordered-imports"
    
    * add tslint rule for indent
    
    * add home button to hdfs explorer's navigation;
    adjust octicon's color
    
    * fix lint error
    
    * [VS Code] Add job list (#2160)
    
    * add job list view to pai extension
    
    * [VS Code] joblist fix (#2185)
    
    * eager load recent jobs when job submitted
    
    * avoid eager getChildren, and let vscode treeview.reveal do it implicitly
    
    * fix lint error

--- contrib/pai_vscode/src/pai/configurationTreeDataProvider.ts
+++ contrib/pai_vscode/src/pai/configurationTreeDataProvider.ts
@@ -5,10 +5,9 @@
  */
 
 import { injectable } from 'inversify';
-import { isNil } from 'lodash';
 import {
-    commands, Event, EventEmitter, TreeDataProvider, TreeItem, TreeItemCollapsibleState, window,
-    workspace
+    commands, window,
+    Event, EventEmitter, TreeDataProvider, TreeItem, TreeItemCollapsibleState
 } from 'vscode';
 
 import {
@@ -22,7 +21,8 @@ import {
 import { __ } from '../common/i18n';
 import { getSingleton, Singleton } from '../common/singleton';
 import { Util } from '../common/util';
-import { ClusterManager, getClusterName } from './clusterManager';
+
+import { getClusterName, ClusterManager } from './clusterManager';
 import { IPAICluster } from './paiInterface';
 
 interface IChildNodeDefinition {
@@ -37,8 +37,11 @@ interface IChildNodeDefinition {
  * General tree node recording its parent
  */
 class TreeNode extends TreeItem {
-    constructor(title: string, public readonly parent?: TreeNode) {
+    public readonly parent: TreeNode | undefined;
+
+    constructor(title: string, parent?: TreeNode) {
         super(title, parent ? TreeItemCollapsibleState.None : TreeItemCollapsibleState.Expanded);
+        this.parent = parent;
     }
 }
 
@@ -102,16 +105,19 @@ const childNodeDefinitions: IChildNodeDefinition[] = [
  */
 export class ConfigurationNode extends TreeNode {
     public children: TreeNode[] = [];
+    public readonly index: number;
+    private _configuration: IPAICluster | undefined;
 
-    public constructor(private _configuration: IPAICluster, public readonly index: number) {
+    public constructor(configuration: IPAICluster, index: number) {
         super('...');
         this.iconPath = Util.resolvePath(ICON_PAI);
-        this.configuration = this._configuration;
+        this.index = index;
+        this.configuration = configuration;
         this.contextValue = CONTEXT_CONFIGURATION_ITEM;
     }
 
     public get configuration(): IPAICluster {
-        return this._configuration;
+        return this._configuration!;
     }
     public set configuration(to: IPAICluster) {
         this.label = getClusterName(to);
@@ -129,7 +135,7 @@ export class ConfigurationNode extends TreeNode {
             node.command = {
                 title: __(def.title),
                 command: COMMAND_TREEVIEW_DOUBLECLICK,
-                arguments: [this, def.command]
+                arguments: [def.command, this]
             };
             node.iconPath = Util.resolvePath(def.icon);
             this.children.push(node);
@@ -146,31 +152,10 @@ export class ConfigurationTreeDataProvider extends Singleton implements TreeData
     public onDidChangeTreeData: Event<TreeNode> = this.onDidChangeTreeDataEmitter.event; // tslint:disable-line
 
     private configurationNodes: ConfigurationNode[] = [];
-    private lastClick?: { command: string, time: number };
-    private readonly doubleClickInterval: number = 300;
-
     constructor() {
         super();
         this.context.subscriptions.push(
-            commands.registerCommand(COMMAND_REFRESH_CLUSTER, index => this.refresh(index)),
-            commands.registerCommand(COMMAND_TREEVIEW_DOUBLECLICK, (node: TreeNode, command: string) => {
-                const mode: string | undefined = workspace.getConfiguration('workbench.list').get('openMode');
-                if (mode === 'doubleClick') {
-                    void commands.executeCommand(command, node);
-                } else {
-                    // Single Click
-                    if (
-                        !isNil(this.lastClick) &&
-                        this.lastClick.command === command &&
-                        Date.now() - this.lastClick.time < this.doubleClickInterval
-                    ) {
-                        this.lastClick = undefined;
-                        void commands.executeCommand(command, node);
-                    } else {
-                        this.lastClick = { command, time: Date.now() };
-                    }
-                }
-            }),
+            commands.registerCommand(COMMAND_REFRESH_CLUSTER, () => this.refresh()),
             window.registerTreeDataProvider(VIEW_CONFIGURATION_TREE, this)
         );
     }
