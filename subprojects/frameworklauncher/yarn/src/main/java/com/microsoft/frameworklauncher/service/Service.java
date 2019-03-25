9520507e04274e2fb9b6413610286df70434cfda
Zhiltsov Max <zhiltsov.max35@gmail.com>
2019-03-25 17:11:39 +0300

    Graceful container shutdown support in Launcher

--- subprojects/frameworklauncher/yarn/src/main/java/com/microsoft/frameworklauncher/service/Service.java
+++ subprojects/frameworklauncher/yarn/src/main/java/com/microsoft/frameworklauncher/service/Service.java
@@ -1,19 +1,19 @@
 // Copyright (c) Microsoft Corporation
-// All rights reserved. 
+// All rights reserved.
 //
 // MIT License
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
 // to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package com.microsoft.frameworklauncher.service;
 
@@ -303,6 +303,7 @@ public class Service extends AbstractService {
 
     // SetupEntryPoint
     Vector<CharSequence> vargs = new Vector<>(30);
+    vargs.add("trap ' ' TERM;"); // prevents intermediate "bash -c" interpreter from immediate exit after receiving TERM signal
     vargs.add(ApplicationConstants.Environment.JAVA_HOME.$$() + "/bin/java");
     vargs.add("-D" + GlobalConstants.ENV_VAR_LAUNCHER_LOG_DIR + "=" + GlobalConstants.REF_ENV_VAR_LAUNCHER_LOG_DIR);
     vargs.add("-Xmx" + amResource.getMemory() + "m");
