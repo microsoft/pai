fc0b5cb0f5e8a94cdfd1bac4893a8f8a90dfd249
yxwithu <yxwithu@163.com>
2019-04-09 11:19:59 +0800

    add ie11 support
    fix IE11 job missing bug #2515
    https://github.com/Microsoft/pai/issues/2515

--- src/webportal/src/app/job/job-view/fabric/JobList/index.jsx
+++ src/webportal/src/app/job/job-view/fabric/JobList/index.jsx
@@ -34,6 +34,8 @@ import Paginator from './Paginator';
 import Table from './Table';
 import TopBar from './TopBar';
 
+import 'whatwg-fetch';
+
 import webportalConfig from '../../../../config/webportal.config';
 import userAuth from '../../../../user/user-auth/user-auth.component';
 
@@ -86,7 +88,7 @@ export default function JobList() {
     userAuth.checkToken((token) => {
       jobs.forEach((job) => {
         const {name, username} = job;
-        fetch(`${webportalConfig.restServerUri}/api/v1/user/${username}/jobs/${name}/executionType`, {
+        window.fetch(`${webportalConfig.restServerUri}/api/v1/user/${username}/jobs/${name}/executionType`, {
           method: 'PUT',
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -119,7 +121,7 @@ export default function JobList() {
       const {keyword, users, virtualClusters, statuses} = filter;
       setFilter(new Filter(keyword, users, virtualClusters.add(query['vcName']), statuses));
     }
-    fetch(`${webportalConfig.restServerUri}/api/v1/jobs`)
+    window.fetch(`${webportalConfig.restServerUri}/api/v1/jobs`)
       .then((response) => {
         if (!response.ok) {
           throw Error(response.message);
