76305ddf506d1a8ace823a77eee658aab61afeae
yuye@microsoft.com <yuye@microsoft.com>
2019-04-16 17:41:34 +0800

    Code Format

--- src/webportal/src/app/user/user-logout/user-logout.component.js
+++ src/webportal/src/app/user/user-logout/user-logout.component.js
@@ -26,7 +26,7 @@ const userLogout = () => {
   if (webportalConfig.authnMethod === 'basic') {
     window.location.replace('/index.html');
   } else {
-    location.href=webportalConfig.restServerUri+'/api/v1/auth/oidc/logout';
+    location.href = webportalConfig.restServerUri+'/api/v1/auth/oidc/logout';
   }
 };
 
