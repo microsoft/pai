94db70d22a38e2df2283898e60b8443c94f59ed4
yuye@microsoft.com <yuye@microsoft.com>
2019-04-25 17:01:57 +0800

    init push

--- src/rest-server/src/util/manager/user/crudK8sSecret.js
+++ src/rest-server/src/util/manager/user/crudK8sSecret.js
@@ -15,4 +15,46 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+const CrudK8sSecret = require('./crudBase');
+const axios = require('axios');
 
+class UserK8sSecret extends CrudK8sSecret {
+  constructor(options) {
+    super();
+    this.secretRootUri = `${options.paiUserNameSpace}/secrets`;
+    this.request = axios.create(options.requestConfig);
+    this.options = options;
+  }
+
+  async read(key, options) {
+    try {
+      const hexKey = key ? Buffer.from(key).toString('hex') : '';
+      const response = await this.request.get(`${this.secretRootUri}/${hexKey}`, {
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+      let allUserSecrets = [];
+      let userData = reponse['data']
+      if (userData.hasOwnProperty('items')) {
+        userData['items'].forEach((item) => {
+          allUserSecrets.push({
+            username: Buffer.from(item['data']['username'], 'base64').toString(),
+            password: Buffer.from(item['data']['password'], 'base64').toString(),
+            group
+          });
+        });
+      } else {
+
+      }
+    } catch (error) {
+      throw error.response;
+    }
+
+  }
+
+  async encrypt(username, password) {
+
+  }
+
+}
