8c393bde2f4eee3dc9fe074037a80a90fe0520b6
yuye@microsoft.com <yuye@microsoft.com>
2019-04-29 17:09:12 +0800

    Refactor

--- src/rest-server/src/util/manager/user/crudK8sSecret.js
+++ src/rest-server/src/util/manager/user/crudK8sSecret.js
@@ -15,7 +15,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const User = require('./schema');
+const User = require('./user');
 const axios = require('axios');
 
 function getSecretRootUri(options) {
