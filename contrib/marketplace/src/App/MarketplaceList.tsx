3f8b68b8dbad86c6279c2591ac50b329c4f8807d
Yifan Xiong <yifan.xiong@microsoft.com>
2019-04-30 17:53:02 +0800

    Upgrade marketplace plugin
    Close #2630.
    Support user to go through protocol jobs in marketplace without submitting job.

---
+++ contrib/marketplace/src/App/MarketplaceList.tsx
@@ -0,0 +1,410 @@
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import React from "react";
+import {
+  DocumentCard, DocumentCardActions, DocumentCardActivity, DocumentCardLogo, DocumentCardStatus, DocumentCardTitle,
+  DefaultPalette, Fabric, Icon, IconButton, Label, Persona, PersonaSize, Stack, Spinner, SpinnerSize, Text, TextField,
+  initializeIcons, mergeStyleSets,
+} from "office-ui-fabric-react";
+import classNames from "classnames/bind";
+import yaml from "yaml";
+
+const styles = mergeStyleSets({
+  title: {
+    marginTop: "15px",
+    fontWeight: "600",
+  },
+
+  subTitle: {
+    fontSize: "16px",
+    fontWeight: "300",
+    color: DefaultPalette.neutralSecondary,
+  },
+
+  layoutOption: {
+    width: "95%",
+    alignSelf: "center",
+    height: "40px",
+  },
+
+  sep: {
+    borderRightWidth: "1px",
+    borderRightStyle: "solid",
+    borderRightColor: DefaultPalette.neutralLight,
+  },
+
+  layout: {
+    width: "95%",
+    marginTop: "20px",
+    marginBottom: "30px",
+    alignSelf: "center",
+    boxSizing: "border-box",
+    borderStyle: "1px solid rgba(0, 0, 0, 0.2)",
+    backgroundColor: DefaultPalette.white,
+  },
+
+  gridCell: {
+    display: "inline-block",
+    width: 240,
+  },
+
+  gridTitle: {
+    height: 150,
+  },
+
+  listCell: {
+    display: "flex",
+    width: "100%",
+    minHeight: "54px",
+    paddingBottom: "10px",
+    boxSizing: "border-box",
+    borderBottomWidth: "1px",
+    borderBottomStyle: "solid",
+    borderBottomColor: DefaultPalette.neutralLight,
+    selectors: {
+      "&:hover": {
+        backgroundColor: DefaultPalette.neutralLighterAlt,
+      },
+    },
+  },
+
+  listCellIconItem: {
+    width: "4%",
+  },
+
+  listCellTextItem: {
+    width: "96%",
+  },
+
+  listCellIcon: {
+    fontWeight: "400",
+    color: DefaultPalette.themePrimary,
+  },
+
+  listCellTitle: {
+    fontWeight: "400",
+    fontSize: "17px",
+  },
+
+  listCellIconButtons: {
+    width: "75%",
+  },
+
+  listCellDescription: {
+    color: DefaultPalette.white,
+    selectors: {
+      "&:disabled": {
+        color: DefaultPalette.white,
+      },
+    },
+  },
+});
+const cx = classNames.bind(styles);
+
+initializeIcons();
+
+type MarketplaceUriType = ("GitHub" | null);
+type LayoutType = ("grid" | "list");
+
+interface IProtocolItem {
+  name: string;
+  uri: string;
+}
+
+interface IProtocol {
+  name: string;
+  contributor: string;
+  description: string;
+  prerequisitesNum: number;
+}
+
+interface IMarketplaceListProps {
+  api: string;
+  user: string;
+  token: string;
+  defaultURI: string;
+  defaultURIType: MarketplaceUriType;
+}
+
+interface IMarketplaceListState {
+  uri: string;
+  uriType: MarketplaceUriType;
+  protocols: Array<IProtocol | null>;
+  loading: boolean;
+  layout: LayoutType;
+}
+
+export default class MarketplaceList extends React.Component<IMarketplaceListProps, IMarketplaceListState> {
+  public static defaultProps: Partial<IMarketplaceListProps> = {
+    defaultURI: "https://api.github.com/repos/Microsoft/pai/contents/marketplace-v2",
+    defaultURIType: "GitHub",
+  };
+
+  public state = {
+    uri: this.props.defaultURI,
+    uriType: this.props.defaultURIType,
+    protocols: [],
+    loading: true,
+    layout: "list" as LayoutType,
+  };
+
+  public componentDidMount() {
+    this.getProtocols();
+  }
+
+  public render() {
+    return (
+      <Fabric>
+        <Stack>
+          <Stack horizontal={true} horizontalAlign="center" padding={15}>
+            <Text variant="xxLarge" nowrap={true} block={true} className={styles.title}>
+              Marketplace <span className={styles.subTitle}>Protocol Preview</span>
+            </Text>
+          </Stack>
+          {this.state.loading ? this.renderLoading() : this.renderContent()}
+        </Stack>
+      </Fabric>
+    );
+  }
+
+  private renderLoading = () => {
+    return (
+      <Stack>
+        <Spinner
+          label="Loading Marketplace ..."
+          ariaLive="assertive"
+          labelPosition="left"
+          size={SpinnerSize.large}
+        />
+      </Stack>
+    );
+  }
+
+  private renderContent = () => {
+    return (
+      <>
+        <Stack className={styles.layoutOption}>
+          <Stack horizontal={true} horizontalAlign="end">
+            <Label>Change View : </Label>
+            <IconButton
+              className={styles.sep}
+              iconProps={{ iconName: "GridViewSmall" }}
+              title="View in Grid"
+              ariaLabel="View in Grid"
+              onClick={this.changeGridView}
+            />
+            <IconButton
+              iconProps={{ iconName: "List" }}
+              title="View in List"
+              ariaLabel="View in List"
+              onClick={this.changeListView}
+            />
+          </Stack>
+        </Stack>
+        {this.renderLayout()}
+      </>
+    );
+  }
+
+  private renderLayout = () => {
+    const renderItem = (protocol: IProtocol, index: number) => {
+      if (this.state.layout === "list") {
+        return this.renderListItem(protocol, index);
+      } else {
+        return this.renderGridItem(protocol, index);
+      }
+    };
+
+    return (
+      <Stack padding={25} className={styles.layout}>
+        <Stack horizontal={true} horizontalAlign="center" wrap={true} tokens={{childrenGap: "20 20"}}>
+          {this.state.protocols.map((protocol: IProtocol, index: number) => renderItem(protocol, index))}
+        </Stack>
+      </Stack>
+    );
+  }
+
+  private renderListItem = (protocol: IProtocol, index: number) => {
+    return (
+      <Stack key={index} className={styles.listCell}>
+        <Stack horizontal={true} horizontalAlign="start" verticalAlign="start" padding={10}>
+          <Stack.Item grow={4} className={styles.listCellIconItem}>
+            <Text variant="xxLarge" nowrap={true} block={true} className={styles.listCellIcon}>
+              <Icon iconName="FileYML" />
+            </Text>
+          </Stack.Item>
+          <Stack.Item grow={96} className={styles.listCellTextItem}>
+            <Stack horizontal={true} horizontalAlign="space-between" verticalAlign="center">
+              <Text variant="xxLarge" nowrap={true} block={true} className={styles.listCellTitle}>
+                {protocol.name}
+              </Text>
+              <Stack
+                horizontal={true}
+                horizontalAlign="start"
+                verticalAlign="center"
+                className={styles.listCellIconButtons}
+              >
+                <IconButton
+                  iconProps={{ iconName: "View" }}
+                  title="View protocol job"
+                  ariaLabel="View protocol job"
+                />
+                <IconButton
+                  iconProps={{ iconName: "FavoriteStar" }}
+                  title="Star protocol job"
+                  ariaLabel="Star protocol job"
+                />
+                <IconButton
+                  iconProps={{ iconName: "Share" }}
+                  title="Share protocol job"
+                  ariaLabel="Share protocol job"
+                />
+              </Stack>
+              <Persona
+                size={PersonaSize.size32}
+                text={protocol.contributor}
+                secondaryText={`Shared on ${this.state.uriType}`}
+                imageUrl={this.getProfileImage(protocol.contributor)}
+              />
+            </Stack>
+            <TextField
+              className={styles.listCellDescription}
+              rows={5}
+              label="Description:"
+              multiline={true}
+              resizable={false}
+              readOnly={true}
+              defaultValue={protocol.description}
+            />
+          </Stack.Item>
+        </Stack>
+      </Stack>
+    );
+  }
+
+  private renderGridItem = (protocol: IProtocol, index: number) => {
+    const people = [{
+      name: protocol.contributor,
+      profileImageSrc: this.getProfileImage(protocol.contributor),
+      initials: this.getProfileInitials(protocol.contributor),
+    }];
+    const cardActions = [
+      {
+        iconProps: { iconName: "View"  },
+        ariaLabel: "View protocol job",
+      },
+      {
+        iconProps: { iconName: "FavoriteStar" },
+        ariaLabel: "Star protocol job",
+      },
+      {
+        iconProps: { iconName: "Share" },
+        ariaLabel: "Share protocol job",
+      },
+    ];
+
+    return (
+      <DocumentCard key={index} className={styles.gridCell}>
+        <DocumentCardLogo logoIcon="FileYML" />
+        <div className={styles.gridTitle}>
+          <DocumentCardTitle title={protocol.name} shouldTruncate={true} />
+          <DocumentCardTitle title={protocol.description} shouldTruncate={true} showAsSecondaryTitle={true} />
+          <DocumentCardStatus statusIcon="attach" status={` ${protocol.prerequisitesNum} Prerequisite(s)`} />
+        </div>
+        <DocumentCardActivity activity={`Shared on ${this.state.uriType}`} people={people} />
+        <DocumentCardActions actions={cardActions} />
+      </DocumentCard>
+    );
+  }
+
+  private changeListView = () => {
+    this.setState({layout: "list"});
+  }
+
+  private changeGridView = () => {
+    this.setState({layout: "grid"});
+  }
+
+  private getProfileImage = (contributor: string) => {
+    if (contributor === "OpenPAI") {
+      return "https://raw.githubusercontent.com/Microsoft/pai/master/pailogo.jpg";
+    } else {
+      return "";
+    }
+  }
+
+  private getProfileInitials = (contributor: string) => {
+    return contributor.split(" ").map((name: string) => name[0]).join(".").toUpperCase();
+  }
+
+  private getProtocols = async () => {
+    const protcolList = await this.getProtocolList(this.state.uri, this.state.uriType);
+    if (protcolList !== null) {
+      let protocols = await Promise.all(protcolList.map(async (item: IProtocolItem) => {
+        return await this.getProtocolItem(item.uri);
+      }));
+      protocols = await protocols.filter((x) => x != null);
+      this.setState({
+        protocols,
+        loading: false,
+      });
+    }
+  }
+
+  private getProtocolList = async (uri: string, uriType: MarketplaceUriType) => {
+    if (uriType === "GitHub") {
+      try {
+        const res = await fetch(uri);
+        const data = await res.json();
+        if (Array.isArray(data)) {
+          const protocolList: IProtocolItem[] = [];
+          for (const item of data) {
+            if (item.type === "file") {
+              protocolList.push({
+                name: item.name,
+                uri: item.download_url,
+              });
+            }
+          }
+          return protocolList;
+        } else {
+          alert(`Cannot get ${uri}`);
+        }
+      } catch (err) {
+        alert(err.message);
+      }
+    }
+    return null;
+  }
+
+  private getProtocolItem = async (uri: string) => {
+    try {
+      const res = await fetch(uri);
+      const data = await res.text();
+      const protocol = yaml.parse(data);
+      return {
+        name: protocol.name,
+        contributor: protocol.contributor,
+        description: protocol.description,
+        prerequisitesNum: protocol.prerequisites.length,
+      } as IProtocol;
+    } catch (err) {
+      return null;
+    }
+  }
+}
