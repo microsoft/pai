7d75001e4abca3826232921c00203d5a40158d1f
yuye@microsoft.com <yuye@microsoft.com>
2019-04-30 15:20:38 +0800

    refactor

--- src/rest-server/src/util/manager/group/crudK8sSecret.js
+++ src/rest-server/src/util/manager/group/crudK8sSecret.js
@@ -15,117 +15,136 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const GroupSchema = require('./schema');
-const CrudK8sSecret = require('./crudBase');
+const Group = require('./schema');
 const axios = require('axios');
+const readFileSync = require('fs');
+const Agent = require('https');
 
-class GroupK8sSecret extends CrudK8sSecret {
-  constructor(options) {
-    super();
-    this.secretRootUri = `${options.kubernetesAPIServerAddress}/${options.groupNamespace}/secrets`;
-    this.request = axios.create(options.requestConfig);
-    this.options = options;
+function initConfig(apiServerUri, namespace, option) {
+  const config = {
+    'apiServerUri': apiServerUri,
+    'namespace': namespace,
+    'reqeustConfig': {
+      'baseURL': `${apiServerUri}/api/v1/namespaces/`,
+      'maxRedirects': 0,
+    },
+  };
+  if ('k8sAPIServerCaFile' in option) {
+    const ca = readFileSync(option.k8sAPIServerCaFile);
+    config.reqeustConfig.httpsAgent = new Agent({ca});
   }
-
-  async read(key, options) {
-    try {
-      const hexKey = key ? Buffer.from(key).toString('hex') : '';
-      const response = await this.request.get(`${this.secretRootUri}/${hexKey}`, {
-        headers: {
-          'Accept': 'application/json',
-        },
-      });
-      let allGroupInstance = [];
-      let groupData = response['data'];
-      if (groupData.hasOwnProperty('items')) {
-        for (const item of groupData['items']) {
-          let groupInstance = GroupSchema.createGroup({
-            'groupname': Buffer.from(item['data']['groupname'], 'base64').toString(),
-            'description': Buffer.from(item['data']['description'], 'base64').toString(),
-            'GID': Buffer.from(item['data']['email'], 'base64').toString(),
-            'extension': JSON.parse(Buffer.from(item['data']['extension'], 'base64').toString()),
-          });
-          allGroupInstance.push(groupInstance);
-        }
-      } else {
-        let groupInstance = GroupSchema.createGroup({
-          'groupname': Buffer.from(groupData['data']['groupname'], 'base64').toString(),
-          'description': Buffer.from(groupData['data']['description'], 'base64').toString(),
-          'GID': Buffer.from(groupData['data']['email'], 'base64').toString(),
-          'extension': JSON.parse(Buffer.from(groupData['data']['extension'], 'base64').toString()),
-        });
-        allGroupInstance.push(groupInstance);
-      }
-      return allGroupInstance;
-    } catch (error) {
-      throw error.response;
-    }
-  }
-
-  async create(key, value, option) {
-    try {
-      const hexKey = key ? Buffer.from(key).toString('hex') : '';
-      let groupInstance = GroupSchema.createGroup({
-        'groupname': value['groupname'],
-        'description': value['description'],
-        'GID': value['description'],
-        'extension': value['extension'],
-      });
-      let groupData = {
-        'metadata': {'name': hexKey},
-        'data': {
-          'groupname': Buffer.from(groupInstance['groupname']).toString('base64'),
-          'description': Buffer.from(groupInstance['description']).toString('base64'),
-          'GID': Buffer.from(groupInstance['GID']).toString('base64'),
-          'extension': Buffer.from(JSON.stringify(groupInstance['extension'])).toString('base64'),
-        },
-      };
-      let response = await this.request.post(`${this.secretRootUri}`, groupData);
-      return response['data'];
-    } catch (error) {
-      throw error.response;
-    }
-  }
-
-  async update(key, value, option) {
-    try {
-      const hexKey = Buffer.from(key).toString('hex');
-      let groupInstance = GroupSchema.createGroup({
-        'groupname': value['groupname'],
-        'description': value['description'],
-        'GID': value['description'],
-        'extension': value['extension'],
-      });
-      let groupData = {
-        'metadata': {'name': hexKey},
-        'data': {
-          'groupname': Buffer.from(groupInstance['groupname']).toString('base64'),
-          'description': Buffer.from(groupInstance['description']).toString('base64'),
-          'GID': Buffer.from(groupInstance['GID']).toString('base64'),
-          'extension': Buffer.from(JSON.stringify(groupInstance['extension'])).toString('base64'),
-        },
-      };
-      let response = await this.request.put(`${this.secretRootUri}/${hexKey}`, groupData);
-      return response['data'];
-    } catch (error) {
-      throw error.response;
-    }
-  }
-
-  async delete(key, option) {
-    try {
-      const hexKey = Buffer.from(key).toString('hex');
-      let response = await this.request.delete(`${this.secretRootUri}/${hexKey}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-      });
-      return response;
-    } catch (error) {
-      throw error.response;
-    }
+  if ('k8sAPIServerTokenFile' in option) {
+    const token = readFileSync(option.k8sAPIServerTokenFile, 'ascii');
+    config.reqeustConfig.headers = {Authorization: `Bearer ${token}`};
   }
 }
 
-module.exports = GroupK8sSecret;
+function getSecretRootUri(config) {
+  return `${config.apiServerUri}/${config.namespace}/secrets`;
+}
+
+async function read(key, config) {
+  try {
+    const request = axios.create(config.requestConfig);
+    const hexKey = key ? Buffer.from(key).toString('hex') : '';
+    const response = await request.get(getSecretRootUri() + `/${hexKey}`, {
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+    let allGroupInstance = [];
+    let groupData = response['data'];
+    if (groupData.hasOwnProperty('items')) {
+      for (const item of groupData['items']) {
+        let groupInstance = Group.createGroup({
+          'groupname': Buffer.from(item['data']['groupname'], 'base64').toString(),
+          'description': Buffer.from(item['data']['description'], 'base64').toString(),
+          'GID': Buffer.from(item['data']['email'], 'base64').toString(),
+          'extension': JSON.parse(Buffer.from(item['data']['extension'], 'base64').toString()),
+        });
+        allGroupInstance.push(groupInstance);
+      }
+    } else {
+      let groupInstance = group.createGroup({
+        'groupname': Buffer.from(groupData['data']['groupname'], 'base64').toString(),
+        'description': Buffer.from(groupData['data']['description'], 'base64').toString(),
+        'GID': Buffer.from(groupData['data']['email'], 'base64').toString(),
+        'extension': JSON.parse(Buffer.from(groupData['data']['extension'], 'base64').toString()),
+      });
+      allGroupInstance.push(groupInstance);
+    }
+    return allGroupInstance;
+  } catch (error) {
+    throw error.response;
+  }
+}
+
+async function create(key, value, config) {
+  try {
+    const request = axios.create(config.requestConfig);
+    const hexKey = key ? Buffer.from(key).toString('hex') : '';
+    let groupInstance = Group.createGroup({
+      'groupname': value['groupname'],
+      'description': value['description'],
+      'GID': value['description'],
+      'extension': value['extension'],
+    });
+    let groupData = {
+      'metadata': {'name': hexKey},
+      'data': {
+        'groupname': Buffer.from(groupInstance['groupname']).toString('base64'),
+        'description': Buffer.from(groupInstance['description']).toString('base64'),
+        'GID': Buffer.from(groupInstance['GID']).toString('base64'),
+        'extension': Buffer.from(JSON.stringify(groupInstance['extension'])).toString('base64'),
+      },
+    };
+    let response = await request.post(getSecretRootUri(), groupData);
+    return response['data'];
+  } catch (error) {
+    throw error.response;
+  }
+}
+
+async function update(key, value, config) {
+  try {
+    const request = axios.create(config.requestConfig);
+    const hexKey = Buffer.from(key).toString('hex');
+    let groupInstance = Group.createGroup({
+      'groupname': value['groupname'],
+      'description': value['description'],
+      'GID': value['description'],
+      'extension': value['extension'],
+    });
+    let groupData = {
+      'metadata': {'name': hexKey},
+      'data': {
+        'groupname': Buffer.from(groupInstance['groupname']).toString('base64'),
+        'description': Buffer.from(groupInstance['description']).toString('base64'),
+        'GID': Buffer.from(groupInstance['GID']).toString('base64'),
+        'extension': Buffer.from(JSON.stringify(groupInstance['extension'])).toString('base64'),
+      },
+    };
+    let response = await request.put(getSecretRootUri() + `/${hexKey}`, groupData);
+    return response['data'];
+  } catch (error) {
+    throw error.response;
+  }
+}
+
+async function remove(key, config) {
+  try {
+    const request = axios.create(config.requestConfig);
+    const hexKey = Buffer.from(key).toString('hex');
+    let response = await request.delete(getSecretRootUri() + `/${hexKey}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    });
+    return response;
+  } catch (error) {
+    throw error.response;
+  }
+}
+
+module.exports = {initConfig, create, read, update, remove};
