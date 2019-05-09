e44a2ab349a2833c893f5c44f3c9cdd84c4dbc58
mslichao <mslichao@outlook.com>
2019-05-09 13:36:06 +0800

    remove unused import

--- src/webportal/src/app/user/fabric/userView/Ordering.jsx
+++ src/webportal/src/app/user/fabric/userView/Ordering.jsx
@@ -15,7 +15,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import {toBool, getVirtualCluster} from './utils';
+import {getVirtualCluster} from './utils';
 
 export default class Ordering {
   /**
