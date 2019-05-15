b318b927291bf706f06c76e3b678cac861b8a8a9
yuye@microsoft.com <yuye@microsoft.com>
2019-05-15 19:10:10 +0800

    part commit.

--- src/rest-server/src/util/hdfs.js
+++ src/rest-server/src/util/hdfs.js
@@ -31,6 +31,14 @@ class Hdfs {
     this._list(this._constructTargetUrl(path, options, 'LISTSTATUS'), next);
   }
 
+  async listAsync(path, options) {
+    try {
+      return await this._listAxios(this._constructTargetUrl(path, options, 'LISTSTATUS'));
+    } catch (error) {
+      throw error;
+    }
+  }
+
   createFolder(path, options, next) {
     this._createFolder(this._constructTargetUrl(path, options, 'MKDIRS'), next);
   }
@@ -49,7 +57,7 @@ class Hdfs {
 
   async createFileAsync(path, data, options) {
     try {
-      return await this._createFileAxios(this._constructTargetUrl(path, options, 'CREATE'), data )
+      return await this._createFileAxios(this._constructTargetUrl(path, options, 'CREATE'), data );
     } catch (error) {
       throw error;
     }
@@ -57,6 +65,14 @@ class Hdfs {
 
   readFile(path, options, next) {
     this._readFile(this._constructTargetUrl(path, options, 'OPEN'), next);
+  }
+
+  async readFileAsync(path, options) {
+    try {
+      return await this._readFileAxios(this._constructTargetUrl(path, options, 'OPEN'));
+    } catch (error) {
+      throw error;
+    }
   }
 
   //
@@ -78,6 +94,16 @@ class Hdfs {
       return new Error('[WebHDFS] Empty response.');
     } else {
       return new Error('[WebHDFS] ' + response.status + ' ' + JSON.stringify(response.body));
+    }
+  }
+
+  async _listAxios(targetUrl) {
+    // Ref: http://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/WebHDFS.html#List_a_Directory
+    try {
+      const response = await axios.get(targetUrl);
+      return {status: 'succeeded', content: response.body};
+    } catch (error) {
+      throw error;
     }
   }
 
@@ -126,14 +152,15 @@ class Hdfs {
       }
     } catch (error) {
       if (error.response.status === 307) {
-        return await this._createFileAxios(
+        await this._createFileAxios(
           error.response.headers['x-location'] // X-Location header is created in unit test only.
             ? error.response.headers['x-location']
             : error.response.headers['location'],
           data,
         );
+      } else {
+        throw error;
       }
-      throw error;
     }
   }
 
@@ -156,6 +183,27 @@ class Hdfs {
           next(this._constructErrorObject(response));
         }
       });
+  }
+
+  async _readFileAxios(targetUrl) {
+    // Ref: http://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/WebHDFS.html#Open_and_Read_a_File
+    try {
+      const response = await axios.get(targetUrl);
+      return {
+        status: 'scceeded',
+        content: response.body,
+      };
+    } catch (error) {
+      if (error.response.status === 307) {
+        await this._readFileAxios(
+          error.response.headers['x-location'] // X-Location header is created in unit test only.
+            ? error.response.headers['x-location']
+            : error.response.headers['location']
+        );
+      } else {
+        throw error;
+      }
+    }
   }
 
   _readFile(targetUrl, next) {
