243b393dd2f0150540783497018310989d295d1f
yuye@microsoft.com <yuye@microsoft.com>
2019-05-16 14:00:33 +0800

    for test

--- src/authentication/config/authentication.py
+++ src/authentication/config/authentication.py
@@ -36,7 +36,6 @@ class Authentication:
         return True, None
 
     def run(self):
-        print self.service_configuration
         return self.service_configuration
 
     def validation_post(self, cluster_object_model):
