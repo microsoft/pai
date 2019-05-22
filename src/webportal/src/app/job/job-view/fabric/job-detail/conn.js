b4746846677449b83784e9176c9fba954b73daf0
Huibin Bi (Wicresoft) <v-hub@microsoft.com>
2019-05-22 15:43:32 +0800

    Change date. getTime to date. now

--- src/webportal/src/app/job/job-view/fabric/job-detail/conn.js
+++ src/webportal/src/app/job/job-view/fabric/job-detail/conn.js
@@ -21,6 +21,7 @@ import qs from 'querystring';
 
 import {checkToken} from '../../../../user/user-auth/user-auth.component';
 import config from '../../../../config/webportal.config';
+import {getDurationString} from "./util";
 
 const params = new URLSearchParams(window.location.search);
 const namespace = params.get('username');
@@ -91,7 +92,7 @@ export function getJobMetricsUrl(getHumanizedJobStateString, jobInfo) {
   let to = '';
   const status = getHumanizedJobStateString(jobInfo);
   if (status === 'Running') {
-    to = new Date().getTime();
+    to = Date.now();
   } else {
     to = jobInfo.jobStatus.completedTime;
   }
