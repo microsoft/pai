160c37190e456f7f3701a6ae2522e4a0e1f5a8fa
Qinzheng Sun <sunqinzheng@users.noreply.github.com>
2019-05-29 14:51:10 +0800

    [Web Portal] fix retry count's margin-top #2845 (#2846)

--- src/webportal/src/app/job/job-view/fabric/job-detail/components/summary.jsx
+++ src/webportal/src/app/job/job-view/fabric/job-detail/components/summary.jsx
@@ -347,11 +347,12 @@ export default class Summary extends React.Component {
             <div className={t.ml4}>
               <div className={c(t.gray, FontClassNames.medium)}>Retries</div>
               <Link
-                className={c(t.mt3, FontClassNames.mediumPlus)}
                 onClick={() => openJobAttemptsPage(jobInfo.jobStatus.retries)}
                 disabled={isNil(jobInfo.jobStatus.retries)}
               >
-                {jobInfo.jobStatus.retries}
+                <div className={c(t.mt3, FontClassNames.mediumPlus)}>
+                  {jobInfo.jobStatus.retries}
+                </div>
               </Link>
             </div>
           </div>
