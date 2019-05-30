51e394e9b486d2cb4557ed056de7ab4e610706eb
yuye@microsoft.com <yuye@microsoft.com>
2019-05-30 10:20:29 +0800

    Issue fix

--- src/rest-server/test/jobSubmission.js
+++ src/rest-server/test/jobSubmission.js
@@ -647,6 +647,64 @@ describe('Submit job: POST /api/v1/jobs', () => {
         200,
         {}
       );
+    //
+    // Mock k8s secret return result
+    //
+    nock(global.apiServerRootUri)
+      .get('/api/v1/namespaces/pai-user-v2/secrets/7573657231')
+      .reply(200, {
+        'kind': 'Secret',
+        'apiVersion': 'v1',
+        'metadata': {
+          'name': '7573657231',
+        },
+        'data': {
+          'email': '',
+          'extension': 'e30=',
+          'grouplist': 'WyJkZWZhdWx0IiwidmMxIl0=',
+          'password': 'ZmE5NGU5MDE0ZWI1MmU4YTk3Mjg2ZjJmNjVhOWU1OTdlMjIyMTVjMmM1NmIzYjJhYmJhOWRmY2ZmZjJmZjM3MTgzM2ZkOTExYWFhZWM0YmI4N2VkYmI0YTc5NWQ3Nzk5OWNkMWI0MWY4MDg3ODQ4NmE3ZTIwYWJmOGM0YWQ1ODc=',
+          'username': 'dXNlcjE=',
+        },
+        'type': 'Opaque'
+      });
+
+    nock(yarnUri)
+      .get('/ws/v1/cluster/scheduler')
+      .reply(200, {
+        'scheduler': {
+          'schedulerInfo': {
+            'queues': {
+              'queue': [
+                {
+                  'queueName': 'default',
+                  'state': 'RUNNING',
+                  'type': 'capacitySchedulerLeafQueueInfo',
+                  "absoluteCapacity": 30.000002,
+                  "absoluteMaxCapacity": 100,
+                },
+                {
+                  'queueName': 'vc1',
+                  'state': 'RUNNING',
+                  'type': 'capacitySchedulerLeafQueueInfo',
+                  "capacity": 50.000002,
+                  "absoluteCapacity": 0,
+                  "absoluteMaxCapacity": 100,
+                },
+                {
+                  'queueName': 'vc2',
+                  'state': 'RUNNING',
+                  'type': 'capacitySchedulerLeafQueueInfo',
+                  "capacity": 19.999996,
+                  "absoluteCapacity": 0,
+                  "absoluteMaxCapacity": 100,
+                }
+              ]
+            },
+            'type': 'capacityScheduler',
+            'usedCapacity': 0.0
+          }
+        }
+      });
 
     // Add OS platform check
     // Since ssh-keygen package only works for Linux
