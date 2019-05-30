859b13854a9d3f846107cbbad531f0ffd60b1e80
yuye@microsoft.com <yuye@microsoft.com>
2019-05-30 13:24:09 +0800

    Issue fix

--- src/rest-server/src/util/manager/user/crudK8sSecret.js
+++ src/rest-server/src/util/manager/user/crudK8sSecret.js
@@ -78,6 +78,8 @@ async function read(key, config) {
   try {
     const request = axios.create(config.requestConfig);
     const hexKey = Buffer.from(key).toString('hex');
+    // eslint-disable-next-line no-console
+    console.log(`${config.namespace}/secrets/${hexKey}`.toString());
     const response = await request.get(`${config.namespace}/secrets/${hexKey}`.toString(), {
       headers: {
         'Accept': 'application/json',
@@ -93,6 +95,8 @@ async function read(key, config) {
     });
     return userInstance;
   } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('read failed');
     throw error.response;
   }
 }
