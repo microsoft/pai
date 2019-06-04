1c48ef088c572396fa5d8730ee36a753d530be01
yuye@microsoft.com <yuye@microsoft.com>
2019-06-04 10:30:24 +0800

    Modify api to update extension field of group & user

--- src/rest-server/src/controllers/v2/token.js
+++ src/rest-server/src/controllers/v2/token.js
@@ -52,6 +52,9 @@ const get = async (req, res, next) => {
       hasGitHubPAT: userInfo.extension.hasOwnProperty('githubPAT')&& Boolean(userInfo.extension['githubPAT']),
     });
   } catch (error) {
+    if (error.status && error.status === 404) {
+      return next(createError('Bad Request', 'NoUserError', `User ${req.params.username} is not found.`));
+    }
     return next(createError.unknown(error));
   }
 };
