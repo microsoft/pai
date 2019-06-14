01077f1d1afa35fd28f0d241a6495701fd5264e5
WangDian <wangdian82@gmail.com>
2019-06-14 16:35:05 +0800

    New team-wise manage cli Use namespace pai-storage to save config as secret

--- contrib/storage_plugin/utils/storage_util.py
+++ contrib/storage_plugin/utils/storage_util.py
@@ -28,6 +28,29 @@ from kubernetes.client.rest import ApiException
 
 logger = logging.getLogger(__name__)
 
+def confirm_namespace(namespace):
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+
+    try:
+        api_response = api_instance.read_namespace(namespace)
+
+    except ApiException as e:
+        if e.status == 404:
+            logger.info("Couldn't find namespace {0}. Create new namespace".format(namespace))
+            try:
+                meta_data = client.V1ObjectMeta(name=namespace)
+                body = client.V1ConfigMap(metadata=meta_data)
+                api_response = api_instance.create_namespace(body)
+                logger.info("Namesapce {0} is created".format(namespace))
+            except ApiException as ie:
+                logger.error("Exception when calling CoreV1Api->create_namespace: {0}".format(str(ie)))
+                sys.exit(1)
+        else:
+            logger.error("Exception when calling CoreV1Api->read_namespace: {0}".format(str(e)))
+            sys.exit(1)
+
+
 # List usernames from pai-user secrets
 def get_pai_users():
     users = []
@@ -50,19 +73,9 @@ def get_pai_users():
     return users
 
 
-def get_storage_config_files(storage_config_name):
-    # List storage users
-    data = get_storage_config(storage_config_name, "default")
-    if data != None:
-        files = []
-        for item in data.keys():
-            files.append(item)
-        return files
-    else:
-        return None
-
-
 def update_configmap(name, data_dict, namespace):
+    confirm_namespace(namespace)
+
     config.load_kube_config()
     api_instance = client.CoreV1Api()
 
@@ -83,7 +96,7 @@ def update_configmap(name, data_dict, namespace):
                 api_response = api_instance.create_namespaced_config_map(namespace, body)
                 logger.info("Configmap named {0} is created".format(name))
             except ApiException as ie:
-                logger.error("Exception when calling CoreV1Api->create_namespaced_config_map: {0}".format(str(e)))
+                logger.error("Exception when calling CoreV1Api->create_namespaced_config_map: {0}".format(str(ie)))
                 sys.exit(1)
         else:
             logger.error("Exception when calling CoreV1Api->patch_namespaced_config_map: {0}".format(str(e)))
@@ -91,6 +104,8 @@ def update_configmap(name, data_dict, namespace):
 
 
 def get_storage_config(storage_config_name, namespace):
+    confirm_namespace(namespace)
+
     config.load_kube_config()
     api_instance = client.CoreV1Api()
 
@@ -107,3 +122,73 @@ def get_storage_config(storage_config_name, namespace):
 
     return api_response.data
 
+
+def patch_secret(name, data_dict, namespace):
+    confirm_namespace(namespace)
+
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+
+    meta_data = client.V1ObjectMeta()
+    meta_data.namespace = namespace
+    meta_data.name = name
+    body = client.V1Secret(metadata = meta_data, data = data_dict)
+
+    try:
+        api_response = api_instance.patch_namespaced_secret(name, namespace, body)
+        logger.info("Secret named {0} is updated.".format(name))
+    except ApiException as e:
+        logger.info(e)
+        if e.status == 404:
+            try:
+                logger.info("Couldn't find secret named {0}. Create a new secret".format(name))
+                api_response = api_instance.create_namespaced_secret(namespace, body)
+                logger.info("Secret named {0} is created".format(name))
+            except ApiException as ie:
+                logger.error("Exception when calling CoreV1Api->create_namespaced_secret: {0}".format(str(ie)))
+                sys.exit(1)
+        else:
+            logger.error("Exception when calling CoreV1Api->patch_namespaced_secret: {0}".format(str(e)))
+            sys.exit(1)
+
+
+def get_secret(name, namespace):
+    confirm_namespace(namespace)
+
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+
+    try:
+        api_response = api_instance.read_namespaced_secret(name, namespace)
+    except ApiException as e:
+        if e.status == 404:
+            logger.info("Couldn't find secret named {0}.".format(name))
+            return None
+        else:
+            logger.error("Exception when calling CoreV1Api->read_namespaced_config_map: {0}".format(str(e)))
+            sys.exit(1)
+
+    return api_response.data
+
+
+def delete_secret_content(name, key, namespace):
+    confirm_namespace(namespace)
+    
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+    try:
+        api_response = api_instance.read_namespaced_secret(name, namespace)
+        if api_response is not None and type(api_response.data) is dict:
+            removed_content = api_response.data.pop(key, None)
+            if removed_content is not None:
+                meta_data = client.V1ObjectMeta()
+                meta_data.namespace = namespace
+                meta_data.name = name
+                body = client.V1Secret(metadata = meta_data, data = api_response.data)
+                api_instance.replace_namespaced_secret(name, namespace, body)
+    except ApiException as e:
+        if e.status == 404:
+            logger.info("Couldn't find secret named {0}.".format(name))
+        else:
+            logger.error("Exception when try to delete {0} from {1}: reason: {2}".format(key, name, str(e)))
+            sys.exit(1)
