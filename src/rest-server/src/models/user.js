434e4a3ae103f8b09e377df4ef9c16dfe15fba9c
Yifan Xiong <yifan.xiong@microsoft.com>
2019-06-18 12:39:01 +0800

    Remove deprecated api in rest server
    Remove deprecated api in rest server.

--- src/rest-server/src/models/user.js
+++ src/rest-server/src/models/user.js
@@ -210,42 +210,6 @@ const checkUserVc = (username, virtualCluster, next) => {
   }
 };
 
-const updateUserGithubPAT = (username, githubPAT, next) => {
-  const dbGet = util.callbackify(db.get.bind(db));
-  dbGet(username, null, (err, res) => {
-    if (err) {
-      if (err.status === 404) {
-        return next(createError('Not Found', 'NoUserError', `User ${username} not found.`));
-      } else {
-        return next(err);
-      }
-    }
-    const dbSet = util.callbackify(db.set.bind(db));
-    res[0]['githubPAT'] = githubPAT;
-    dbSet(username, res[0], {'update': true}, (err, res) => {
-      if (err) {
-        return next(err);
-      }
-      next(null, true);
-    });
-  });
-};
-
-const getUserGithubPAT = (username, next) => {
-  if (typeof username === 'undefined') {
-    next(createError('Unauthorized', 'UnauthorizedUserError', 'Guest is not allowed to do this operation.'));
-  } else {
-    const get = util.callbackify(db.get.bind(db));
-    get(username, null, (err, res) => {
-      if (err) {
-        return next(err);
-      }
-      let githubPAT = res[0]['githubPAT'];
-      next(null, githubPAT);
-    });
-  }
-};
-
 const getUserList = (next) => {
   const get = util.callbackify(db.get.bind(db));
   get('', null, (err, res) => {
@@ -306,4 +270,4 @@ if (config.env !== 'test') {
 }
 
 // module exports
-module.exports = {encrypt, db, update, remove, updateUserVc, checkUserVc, getUserList, updateUserGithubPAT, getUserGithubPAT};
+module.exports = {encrypt, db, update, remove, updateUserVc, checkUserVc, getUserList};
