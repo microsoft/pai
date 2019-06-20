185251215494c244d54ea2ea8e7a6f0622942739
fenghuajia <17610303377@163.com>
2019-06-20 15:23:08 +0800

    group manege plugin.2

--- src/webportal/src/app/job/job-view/fabric/JobList/TopBar.jsx
+++ src/webportal/src/app/job/job-view/fabric/JobList/TopBar.jsx
@@ -1,20 +1,3 @@
-// Copyright (c) Microsoft Corporation
-// All rights reserved.
-//
-// MIT License
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 import React, {useContext, useState, useEffect} from 'react';
 
 import {getTheme, ColorClassNames} from '@uifabric/styling';
@@ -27,8 +10,7 @@ import Context from './Context';
 import Filter from './Filter';
 
 import webportalConfig from '../../../../config/webportal.config';
-import {checkToken} from'../../../../user/user-auth/user-auth.component';
- 
+
 /* eslint-disable react/prop-types */
 function FilterButton({defaultRender: Button, ...props}) {
   const {subMenuProps: {items}} = props;
@@ -80,12 +62,7 @@ function TopBar() {
   const {refreshJobs, selectedJobs, stopJob, username, filter, setFilter} = useContext(Context);
 
   useEffect(() => {
-    const token = checkToken();
-    fetch(`${webportalConfig.restServerUri}/api/v2/user/get`, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    })
+    fetch(`${webportalConfig.restServerUri}/api/v1/user`)
       .then((response) => {
         return response.json();
       }).then((body) => {
