a2f7797c3f13ea56082d82f3f54aeeadabaf75a5
Yi Yi <yiyi@microsoft.com>
2019-06-25 06:10:47 +0800

    update for node 8

--- contrib/pai_vscode/installYamlExtension.js
+++ contrib/pai_vscode/installYamlExtension.js
@@ -1,16 +1,33 @@
-var path = require('path');
+var fs = require('fs');
 var os = require('os');
+var path = require('path');
+
+function mkDirByPathSync(targetDir) {
+    const sep = path.sep;
+    const initDir = path.isAbsolute(targetDir) ? sep : '';
+
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = path.resolve(baseDir, parentDir, childDir);
+        try {
+            fs.mkdirSync(curDir);
+        } catch (err) {
+            if (err.code === 'EEXIST') {
+                return curDir;
+            }
+        }
+        return curDir;
+    }, initDir);
+}
 
 async function downloadAndUnzipExtension(url, path, cb) {
     var unzipper = require('unzipper');
     var request = require('request');
-    var fs = require('fs');
     var getDirName = require('path').dirname;
 
     request(url).pipe(unzipper.Parse()).on('entry', function (entry) {
         if (entry.path.startsWith('extension/')) {
             var newPath = path + '/' + entry.path.slice(10);
-            fs.mkdirSync(getDirName(newPath), { recursive: true });
+            mkDirByPathSync(getDirName(newPath));
             entry.pipe(fs.createWriteStream(newPath));
         } else {
             entry.autodrain();
@@ -20,7 +37,7 @@ async function downloadAndUnzipExtension(url, path, cb) {
 
 function installVscodeYamlExtension() {
     const version = '0.4.0';
-    const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}-test`);
+    const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}`);
     console.log(`extensionPath: ${extensionPath}`);
     const url = `https://github.com/redhat-developer/vscode-yaml/releases/download/0.4.0/redhat.vscode-yaml-0.4.0.vsix`;
     downloadAndUnzipExtension(url, extensionPath, function() {
