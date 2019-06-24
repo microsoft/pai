8f880b0c7ec4500109742cb8b5753c63785b0d03
Yi Yi <yiyi@microsoft.com>
2019-06-25 05:59:31 +0800

    update

--- contrib/pai_vscode/installYamlExtension.js
+++ contrib/pai_vscode/installYamlExtension.js
@@ -1,35 +1,38 @@
-var glob = require('glob');
-var gulp = require('gulp');
-var os = require('os');
 var path = require('path');
-var decompress = require('gulp-decompress');
-var download = require('gulp-download');
+var os = require('os');
+
+async function downloadAndUnzipExtension(url, path, cb) {
+    var unzipper = require('unzipper');
+    var request = require('request');
+    var fs = require('fs');
+    var getDirName = require('path').dirname;
+
+    request(url).pipe(unzipper.Parse()).on('entry', function (entry) {
+        if (entry.path.startsWith('extension/')) {
+            var newPath = path + '/' + entry.path.slice(10);
+            fs.mkdirSync(getDirName(newPath), { recursive: true });
+            entry.pipe(fs.createWriteStream(newPath));
+        } else {
+            entry.autodrain();
+        }
+    }).on('finish', cb);
+}
 
 function installVscodeYamlExtension() {
     const version = '0.4.0';
-    const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}`);
-    const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
+    const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}-test`);
     console.log(`extensionPath: ${extensionPath}`);
-    if (existingExtensions.length === 0) {
-        download(`https://github.com/redhat-developer/vscode-yaml/releases/download/0.4.0/redhat.vscode-yaml-0.4.0.vsix`)
-            .pipe(decompress({
-                filter: (file) => file.path.startsWith('extension/'),
-                map: (file) => {
-                    file.path = file.path.slice(10);
-                    return file;
-                }
-            }))
-            .pipe(gulp.dest(extensionPath));
-    }
-
-    var exec = require('child_process').exec;
-    var parentPath = path.join(os.homedir(), `.vscode/extensions`);
-    exec(`ls ${parentPath}`, function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
+    const url = `https://github.com/redhat-developer/vscode-yaml/releases/download/0.4.0/redhat.vscode-yaml-0.4.0.vsix`;
+    downloadAndUnzipExtension(url, extensionPath, function() {
+        var exec = require('child_process').exec;
+        var parentPath = path.join(os.homedir(), `.vscode/extensions`);
+        exec(`ls ${parentPath}`, function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
     });
 }
 
