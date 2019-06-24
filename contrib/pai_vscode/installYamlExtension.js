875d3f4c4977b3aaf0a32ac6cd8b21c24ed55679
Yi Yi <yiyi@microsoft.com>
2019-06-25 03:44:54 +0800

    add install yaml extension for CI

---
+++ contrib/pai_vscode/installYamlExtension.js
@@ -0,0 +1,25 @@
+var glob = require('glob');
+var gulp = require('gulp');
+var os = require('os');
+var path = require('path');
+var decompress = require('gulp-decompress');
+var download = require('gulp-download');
+
+function installVscodeYamlExtension() {
+    const version = '0.4.0';
+    const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}`);
+    const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
+    if (existingExtensions.length === 0) {
+        return download(`https://github.com/redhat-developer/vscode-yaml/releases/download/0.4.0/redhat.vscode-yaml-0.4.0.vsix`)
+            .pipe(decompress({
+                filter: (file) => file.path.startsWith('extension/'),
+                map: (file) => {
+                    file.path = file.path.slice(10);
+                    return file;
+                }
+            }))
+            .pipe(gulp.dest(extensionPath));
+    }
+}
+
+installVscodeYamlExtension();
