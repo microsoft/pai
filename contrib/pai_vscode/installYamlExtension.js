f308eb93ab6789896a480ea68b0b192c14f7b186
Yi Yi <yiyi@microsoft.com>
2019-06-25 04:08:59 +0800

    udpate

--- contrib/pai_vscode/installYamlExtension.js
+++ contrib/pai_vscode/installYamlExtension.js
@@ -9,6 +9,7 @@ function installVscodeYamlExtension() {
     const version = '0.4.0';
     const extensionPath = path.join(os.homedir(), `.vscode/extensions/redhat.vscode-yaml-${version}`);
     const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
+    console.log(`extensionPath: ${extensionPath}`);
     if (existingExtensions.length === 0) {
         return download(`https://github.com/redhat-developer/vscode-yaml/releases/download/0.4.0/redhat.vscode-yaml-0.4.0.vsix`)
             .pipe(decompress({
