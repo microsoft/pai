9b98c20cc88a9f3e21f18c8883ea70f157c1dcd4
Yi Yi <yiyi@microsoft.com>
2019-06-25 18:11:01 +0800

    update

--- contrib/pai_vscode/installYamlExtension.js
+++ contrib/pai_vscode/installYamlExtension.js
@@ -1,3 +1,9 @@
+/**
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License in the project root for license information.
+ *  @author Microsoft
+ */
+
 var fs = require('fs');
 var getDirName = require('path').dirname;
 var os = require('os');
