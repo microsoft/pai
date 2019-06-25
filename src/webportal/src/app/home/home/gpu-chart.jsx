41389485606ac99796d92dfb25ca58e32614ce0a
Qinzheng Sun <qinsu@microsoft.com>
2019-06-25 16:38:59 +0800

    revert

--- src/webportal/src/app/home/home/gpu-chart.jsx
+++ src/webportal/src/app/home/home/gpu-chart.jsx
@@ -97,7 +97,6 @@ const GpuChart = ({style, gpuPerNode, virtualClusters}) => {
         tooltips: {
           enabled: true,
           mode: 'index',
-          yAlign: 'center',
           callbacks: {
             title: (item, data) => (
               `#GPU: ${item[0].label}`
