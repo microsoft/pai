c1add860f5ccb17943e1368036df084132db3317
Qinzheng Sun <qinsu@microsoft.com>
2019-06-28 16:29:09 +0800

    fix bug: clone job can not submit

--- src/webportal/src/app/job-submission/components/submission-section.jsx
+++ src/webportal/src/app/job-submission/components/submission-section.jsx
@@ -98,8 +98,8 @@ export const SubmissionSection = (props) => {
     const newValidationMessage = JobProtocol.validateFromObject(protocol);
     if (newValidationMessage !== validationMsg) {
       setValidationMsg(newValidationMessage);
-      setErrorMessage(VALIDATION_ERROR_MESSAGE_ID, newValidationMessage);
     }
+    setErrorMessage(VALIDATION_ERROR_MESSAGE_ID, validationMsg);
   };
 
   useEffect(() => {
