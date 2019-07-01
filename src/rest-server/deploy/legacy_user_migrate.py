ab4107dc0b92c9769bcec1acfd32931841d067bf
YundongYe <YundongYe@outlook.com>
2019-07-01 19:40:29 +0800

    Fix user migration issue (#3036)

--- src/rest-server/deploy/legacy_user_migrate.py
+++ src/rest-server/deploy/legacy_user_migrate.py
@@ -100,7 +100,7 @@ class TransferClient:
         meta_dict['name'] = user_info_item['metadata']['name']
         grouplist = []
         virtual_cluster = []
-        if base64.b64decode(user_info_item['data']['admin']) == 'true':
+        if base64.b64decode(user_info_item['data']['admin']).decode('utf-8')== 'true':
             grouplist.append(self.admin_group)
         for vc_name in base64.b64decode(user_info_item['data']['virtualCluster']).decode('utf-8').split(','):
             self.vc_set.add(vc_name)
