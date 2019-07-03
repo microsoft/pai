b2afa3c335482d4b996d2b10eb1b090cfb6d92de
Qinzheng Sun <qinsu@microsoft.com>
2019-07-03 21:00:29 +0800

    remove learn more link's tooltip

--- src/webportal/src/app/job-submission/components/job-information.jsx
+++ src/webportal/src/app/job-submission/components/job-information.jsx
@@ -24,7 +24,7 @@
  */
 
 import React, {useCallback} from 'react';
-import {Text, Stack, TooltipHost, FontWeights} from 'office-ui-fabric-react';
+import {Text, Stack, FontWeights} from 'office-ui-fabric-react';
 import PropTypes from 'prop-types';
 import {FormTextField} from './form-text-field';
 import {FormPage} from './form-page';
@@ -66,20 +66,12 @@ export const JobInformation = React.memo(({jobInformation, onChange, advanceFlag
           <Text variant='xLarge' styles={{root: {fontWeight: 'semibold'}}}>
             Job submission
           </Text>
-          <TooltipHost
-            calloutProps={{
-              isBeakVisible: false,
-              gapSpace: 8, // spacing.s1
-            }}
-            content='Click to open job help page'
-          >
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://github.com/microsoft/pai/blob/master/docs/pai-job-protocol.yaml'
-              style={{fontWeight: FontWeights.semibold}}
-            >{'Learn more >'}</a>
-          </TooltipHost>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://github.com/microsoft/pai/blob/master/docs/pai-job-protocol.yaml'
+            style={{fontWeight: FontWeights.semibold}}
+          >{'Learn more >'}</a>
         </Stack>
         <FormTextField
           sectionLabel={'Job name'}
