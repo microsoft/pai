d85b64abbb2f26e64be88d2a41009f862d4cf1da
Binyang2014 <binyli@microsoft.com>
2019-07-08 14:44:10 +0800

    fix bug: redict v2 job to default submission papge if plugin not installed (#3091)
    * fix bug: redict v2 job to default submission papge if plugin not installed
    
    * remove extras.submitForm if clone from plugin
    
    * fix review comments

--- src/webportal/src/app/job/job-view/fabric/job-detail/conn.js
+++ src/webportal/src/app/job/job-view/fabric/job-detail/conn.js
@@ -134,6 +134,12 @@ export async function cloneJob(rawJobConfig) {
   const plugins = window.PAI_PLUGINS;
   const pluginIndex = plugins.findIndex((x) => x.id === pluginId);
   if (pluginIndex === -1) {
+    // redirect v2 job to default submission page
+    if (isJobV2(rawJobConfig)) {
+      alert(`The job was submitted by ${pluginId}, but it is not installed. Will use default submission page instead`);
+      window.location.href = `/submit.html?${qs.stringify(query)}`;
+      return;
+    }
     alert(`Clone job failed. The job was submitted by ${pluginId}, but it is not installed.`);
     return;
   }
