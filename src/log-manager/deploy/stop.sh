260af2d62ab6d2281d931ff1e4e8059365ee8b10
WangDian <wangdian82@gmail.com>
2019-07-18 10:52:11 +0800

    Bug fix

--- src/log-manager/deploy/stop.sh
+++ src/log-manager/deploy/stop.sh
@@ -19,4 +19,4 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-kubectl delete --ignore-not-found --now "daemonset/log-manager"
+kubectl delete --ignore-not-found --now "daemonset/log-manager-ds"
