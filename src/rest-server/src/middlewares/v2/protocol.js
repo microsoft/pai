ee4b1e70e69304a3d763c33409597c8813183702
zimiao <mzmssg.love@163.com>
2019-07-25 14:54:50 +0800

    fix

--- src/rest-server/src/middlewares/v2/protocol.js
+++ src/rest-server/src/middlewares/v2/protocol.js
@@ -21,10 +21,8 @@ const yaml = require('js-yaml');
 const mustache = require('mustache');
 const createError = require('@pai/utils/error');
 const protocolSchema = require('@pai/config/v2/protocol');
-const hivedSchema = require('@pai/config/v2/hived');
-const vcConfig = require('@pai/config/vc');
 const launcherConfig = require('@pai/config/launcher');
-
+const hivedMiddle = require('@pai/middlewares/v2/hived');
 
 const mustacheWriter = new mustache.Writer();
 
@@ -61,149 +59,6 @@ const render = (template, dict, tags=['<%', '%>']) => {
     }
   }
   return result.trim();
-};
-
-const convertPriority = (priorityClass) => {
-  // TODO: make it a cluster-wise config
-  const priorityMap = {
-    prod: 1000,
-    test: 100,
-  };
-  return priorityClass in priorityMap ? priorityMap[priorityClass] : null;
-};
-
-const hivedValidate = (protocolObj) => {
-  if (!hivedSchema.validate(protocolObj)) {
-    throw createError('Bad Request', 'InvalidProtocolError', hivedSchema.validate.errors);
-  }
-  const resourceUnits = vcConfig.resourceUnits;
-  const minCpu = Math.min(...Array.from(Object.values(resourceUnits), (v)=>v.cpu));
-  const minMemoryMB = Math.min(...Array.from(Object.values(resourceUnits), (v)=>v.memoryMB));
-  let hivedConfig = null;
-  const affinityGroups = {};
-  if ('extras' in protocolObj && 'hivedScheduler' in protocolObj.extras) {
-    hivedConfig = protocolObj.extras.hivedScheduler;
-    for (let taskRole of Object.keys(hivedConfig.taskRoles)) {
-      // must be a valid taskRole
-      if (!(taskRole in protocolObj.taskRoles)) {
-        throw createError(
-          'Bad Request',
-          'InvalidProtocolError',
-          `Hived error: ${taskRole} does not exist.`
-        );
-      }
-      const instances = protocolObj.taskRoles[taskRole].instances;
-      const gpu = protocolObj.taskRoles[taskRole].resourcePerInstance.gpu || 0;
-
-      const taskRoleConfig = hivedConfig.taskRoles[taskRole];
-      // at most one of [reservationId, gpuType] allowed
-      if (taskRoleConfig.reservationId !== null && taskRoleConfig.gpuType !== null) {
-        throw createError(
-          'Bad Request',
-          'InvalidProtocolError',
-          `Hived error: ${taskRole} has both reservationId and gpuType, only one allowed.`
-        );
-      }
-
-      if (taskRoleConfig.gpuType !== null && !(taskRoleConfig.gpuType in resourceUnits)) {
-        throw createError(
-          'Bad Request',
-          'InvalidProtocolError',
-          `Hived error: ${taskRole} has unknown gpuType ${taskRoleConfig.gpuType}, allow ${Object.keys(resourceUnits)}.`
-        );
-      }
-
-      const affinityGroupName = taskRoleConfig.affinityGroupName;
-      // affinityGroup should have uniform reservationId and gpuType
-      if (affinityGroupName !== null) {
-        if (affinityGroupName in affinityGroups) {
-          if (taskRoleConfig.reservationId === null) {
-            taskRoleConfig.reservationId = affinityGroups[affinityGroupName].reservationId;
-          }
-          if (taskRoleConfig.gpuType === null) {
-            taskRoleConfig.gpuType = affinityGroups[affinityGroupName].gpuType;
-          }
-          if (taskRoleConfig.reservationId !== affinityGroups[affinityGroupName].reservationId ||
-            taskRoleConfig.gpuType !== affinityGroups[affinityGroupName].gpuType) {
-            throw createError(
-              'Bad Request',
-              'InvalidProtocolError',
-              `Hived error: affinityGroup: ${affinityGroupName} has inconsistent gpuType or reservationId.`
-            );
-          }
-        } else {
-          affinityGroups[affinityGroupName] = {
-            reservationId: taskRoleConfig.reservationId,
-            gpuType: taskRoleConfig.gpuType,
-            affinityTaskList: [],
-          };
-        }
-        const currentItem = {
-          podNumber: instances,
-          gpuNumber: gpu,
-        };
-        affinityGroups[affinityGroupName].affinityTaskList.push(currentItem);
-      }
-    }
-
-    for (let affinityGroupName of Object.keys(affinityGroups)) {
-      if (affinityGroups[affinityGroupName].gpuType !== null &&
-        affinityGroups[affinityGroupName].reservationId !== null) {
-        throw createError(
-          'Bad Request',
-          'InvalidProtocolError',
-          `Hived error: affinityGroup: ${affinityGroupName} has both reservationId and gpuType, only one allowed.`
-        );
-      }
-    }
-  }
-
-  // generate podSpec for every taskRole
-  for (let taskRole of Object.keys(protocolObj.taskRoles)) {
-    const gpu = protocolObj.taskRoles[taskRole].resourcePerInstance.gpu || 0;
-    const cpu = protocolObj.taskRoles[taskRole].resourcePerInstance.cpu;
-    const memoryMB = protocolObj.taskRoles[taskRole].resourcePerInstance.memoryMB;
-    let allowedCpu = minCpu * gpu;
-    let allowedMemoryMB = minMemoryMB * gpu;
-    const podSpec = {
-      virtualCluster: ('defaults' in protocolObj && protocolObj.defaults.virtualCluster != null) ?
-        protocolObj.defaults.virtualCluster : 'default',
-      priority: null,
-      gpuType: null,
-      reservationId: null,
-      gpuNumber: protocolObj.taskRoles[taskRole].resourcePerInstance.gpu,
-      affinityGroup: null,
-    };
-    if (hivedConfig !== null && taskRole in hivedConfig.taskRoles) {
-      podSpec.priority = convertPriority(hivedConfig.jobPriorityClass);
-      podSpec.gpuType = hivedConfig.taskRoles[taskRole].gpuType;
-      if (podSpec.gpuType !== null) {
-        allowedCpu = resourceUnits[podSpec.gpuType].cpu * gpu;
-        allowedMemoryMB = resourceUnits[podSpec.gpuType].memoryMB * gpu;
-      }
-      podSpec.reservationId = hivedConfig.taskRoles[taskRole].reservationId;
-
-      const affinityGroupName = hivedConfig.taskRoles[taskRole].affinityGroupName;
-      podSpec.affinityGroup = affinityGroupName ? {
-        name: protocolObj.name + '/' + affinityGroupName,
-        members: affinityGroups[affinityGroupName].affinityTaskList,
-      } : null;
-    }
-    // TODO: hardcode for demo, removed it!
-    if (podSpec.gpuType === null && podSpec.reservationId === null) {
-      podSpec.gpuType = 'K80';
-    }
-
-    if (cpu > allowedCpu || memoryMB > allowedMemoryMB) {
-      throw createError(
-        'Bad Request',
-        'InvalidProtocolError',
-        `Hived error: ${taskRole} requests (${cpu}cpu, ${memoryMB}memoryMB), allow (${allowedCpu}cpu, ${allowedMemoryMB}memoryMB) with ${gpu}gpu.`
-      );
-    }
-    protocolObj.taskRoles[taskRole].hivedPodSpec = podSpec;
-  }
-  return protocolObj;
 };
 
 const protocolValidate = (protocolYAML) => {
@@ -332,7 +187,7 @@ const protocolSubmitMiddleware = [
 if (launcherConfig.enabledHived) {
   protocolSubmitMiddleware.push(
     (req, res, next) => {
-      res.locals.protocol = hivedValidate(res.locals.protocol);
+      res.locals.protocol = hivedMiddle.validate(res.locals.protocol);
       next();
   });
 }
