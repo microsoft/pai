ea47697e96b93fece36f1a3a173b6c6084e26d11
Yuqi Wang <32826762+yqwang-ms@users.noreply.github.com>
2019-08-01 14:55:51 +0800

    Initial HivedScheduler (#3283)
    Authors:
    @zhypku Scheduling Algorithm Core
    @mzmssg Scheduling Algorithm Config Parser
    @yqwang-ms User Experience/Interface, Scheduling Framework/Architecture, K8S Integration, and others

---
+++ subprojects/hivedscheduler/pkg/common/utils.go
@@ -0,0 +1,257 @@
+// MIT License
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE
+
+package common
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"runtime/debug"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func Quote(s string) string {
+	return `"` + s + `"`
+}
+
+func ReferEnvVar(name string) string {
+	return "$(" + name + ")"
+}
+
+func PtrString(o string) *string {
+	return &o
+}
+
+func PtrInt32(o int32) *int32 {
+	return &o
+}
+
+func NilInt32() *int32 {
+	return nil
+}
+
+func PtrInt64(o int64) *int64 {
+	return &o
+}
+
+func PtrFloat64(o float64) *float64 {
+	return &o
+}
+
+func PtrBool(o bool) *bool {
+	return &o
+}
+
+func NilBool() *bool {
+	return nil
+}
+
+func PtrUID(o types.UID) *types.UID {
+	return &o
+}
+
+func PtrUIDStr(s string) *types.UID {
+	return PtrUID(types.UID(s))
+}
+
+func PtrNow() *meta.Time {
+	now := meta.Now()
+	return &now
+}
+
+func SecToDuration(sec *int64) time.Duration {
+	return time.Duration(*sec) * time.Second
+}
+
+func IsTimeout(leftDuration time.Duration) bool {
+	// Align with the AddAfter method of the workqueue
+	return leftDuration <= 0
+}
+
+func CurrentLeftDuration(startTime meta.Time, timeoutSec *int64) time.Duration {
+	currentDuration := time.Since(startTime.Time)
+	timeoutDuration := SecToDuration(timeoutSec)
+	leftDuration := timeoutDuration - currentDuration
+	return leftDuration
+}
+
+func InitAll() {
+	InitLogger()
+	InitRandSeed()
+}
+
+func InitLogger() {
+	// Defaulting
+	klog.InitFlags(flag.CommandLine)
+	flag.Set("v", "2")
+
+	// Configure klog from command line
+	flag.Parse()
+
+	// Only support stderr logging and not support file logging.
+	// To achieve file logging, user can redirect the stderr to file and rotate the
+	// file to avoid out of disk.
+	// This is because currently file logging in klog has some limitations:
+	// 1. klog will never remove old files even if the log_file_max_size is exceeded.
+	//    So, the total log data size cannot be limited.
+	// 2. We do not have the chance to flush log to file, if a panic is called in
+	//    an unmanaged goroutine and no one can recover it in the goroutine.
+	//    So, even if the log about the panic itself can be lost.
+	flag.Set("logtostderr", "true")
+	flag.Set("alsologtostderr", "true")
+	flag.Set("stderrthreshold", "INFO")
+	klog.SetOutput(ioutil.Discard)
+
+	// Redirect the default golang log to klog
+	log.SetOutput(KlogWriter{})
+	log.SetFlags(0)
+}
+
+func InitRandSeed() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func NewStopChannel() <-chan struct{} {
+	stopCh := make(chan struct{})
+
+	// Stop on shutdown signal:
+	// Must be a buffered channel, otherwise the shutdown signal may be lost.
+	shutdownCh := make(chan os.Signal, 1)
+	signal.Notify(shutdownCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		s := <-shutdownCh
+		klog.Warningf("Received shutdown signal: %v", s)
+		close(stopCh)
+	}()
+
+	return stopCh
+}
+
+// Rand in range [min, max]
+func RandInt64(min int64, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func ToYaml(obj interface{}) string {
+	yamlBytes, err := yaml.Marshal(obj)
+	if err != nil {
+		panic(fmt.Errorf("Failed to marshal Object %#v to YAML: %v", obj, err))
+	}
+	return string(yamlBytes)
+}
+
+func FromYaml(yamlStr string, objAddr interface{}) {
+	err := yaml.Unmarshal([]byte(yamlStr), objAddr)
+	if err != nil {
+		panic(fmt.Errorf("Failed to unmarshal YAML %#v to Object: %v", yamlStr, err))
+	}
+}
+
+func ToJson(obj interface{}) string {
+	return string(ToJsonBytes(obj))
+}
+
+func FromJson(jsonStr string, objAddr interface{}) {
+	FromJsonBytes([]byte(jsonStr), objAddr)
+}
+
+func ToJsonBytes(obj interface{}) []byte {
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		panic(fmt.Errorf("Failed to marshal Object %#v to JSON: %v", obj, err))
+	}
+	return jsonBytes
+}
+
+func FromJsonBytes(jsonBytes []byte, objAddr interface{}) {
+	err := json.Unmarshal(jsonBytes, objAddr)
+	if err != nil {
+		panic(fmt.Errorf("Failed to unmarshal JSON %#v to Object: %v", string(jsonBytes), err))
+	}
+}
+
+func GetPanicDetails(recoveredObj interface{}) string {
+	return fmt.Sprintf("\n  panic: %#v\n  %v", recoveredObj, GetCallStack())
+}
+
+func GetCallStack() string {
+	return strings.Replace(strings.Replace(string(debug.Stack()),
+		"\t", "  ", -1),
+		"\n", "\n    ", -1)
+}
+
+func ToIndicesString(indices []int32) string {
+	strIndices := []string{}
+
+	for _, i := range indices {
+		strIndices = append(strIndices, fmt.Sprint(i))
+	}
+	indicesStr := strings.Join(strIndices, ",")
+	return indicesStr
+}
+
+func StringsContains(s []string, e string) bool {
+	for _, te := range s {
+		if te == e {
+			return true
+		}
+	}
+	return false
+}
+
+func NextPowerOf2(i int32) int32 {
+	u := uint32(i)
+	u--
+	u |= u >> 1
+	u |= u >> 2
+	u |= u >> 4
+	u |= u >> 8
+	u |= u >> 16
+	u++
+	return int32(u)
+}
+
+func Int32ToString(i int32) string {
+	s := strconv.FormatInt(int64(i), 10)
+	return s
+}
+
+func StringToInt32(s string) int32 {
+	i, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		panic(fmt.Sprintf("invalid literal for int32: %v", err))
+	}
+	return int32(i)
+}
---
+++ subprojects/hivedscheduler/pkg/internal/types.go
@@ -0,0 +1,195 @@
+// MIT License
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE
+
+package internal
+
+import (
+	"fmt"
+	si "github.com/microsoft/hivedscheduler/pkg/api"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ei "k8s.io/kubernetes/pkg/scheduler/api"
+)
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Internal Common Types
+///////////////////////////////////////////////////////////////////////////////////////
+
+// WebServer Callbacks with K8S Default Scheduler
+// Notes:
+// 1. Error should be passed by panic
+// 2. Should not assume previous succeeded operation also has been successfully
+//    executed by K8S Default Scheduler.
+type ExtenderHandlers struct {
+	FilterHandler  func(args ei.ExtenderArgs) *ei.ExtenderFilterResult
+	BindHandler    func(args ei.ExtenderBindingArgs) *ei.ExtenderBindingResult
+	PreemptHandler func(args ei.ExtenderPreemptionArgs) *ei.ExtenderPreemptionResult
+}
+
+// SchedulerAlgorithm is used to make the pod schedule decision based on its whole
+// cluster scheduling view constructed from its Add/Update/Delete callbacks.
+// Notes:
+// 1. Error should be passed by panic
+// 2. Should take all the input parameters as readonly and return pod schedule
+//    decision by PodScheduleResult.
+// 3. {Schedule, AddAllocatedPod, DeleteAllocatedPod} will never be executed
+//    concurrently for all pods.
+// 4. [Schedule -> (AddAllocatedPod) -> Schedule -> ...] is executed sequentially
+//    for all pods.
+//    I.e. the constructed scheduling view is already lock protected.
+// 5. [START -> AddAllocatedPod -> DeleteAllocatedPod -> END] is executed
+//    sequentially for one specific Pod.
+//    I.e. once a specific Pod is allocated by AddAllocatedPod, its placement
+//    will never be changed to another one.
+type SchedulerAlgorithm interface {
+	// See details in PodScheduleResult.
+	Schedule(pod *core.Pod, suggestedNodes []string) PodScheduleResult
+
+	// Track all current Nodes in the whole cluster.
+	AddNode(node *core.Node)
+	UpdateNode(oldNode, newNode *core.Node)
+	DeleteNode(node *core.Node)
+
+	// Track all current allocated Pods in the whole cluster.
+	// Allocated Pod includes both PodBound and PodBinding Pods.
+	AddAllocatedPod(pod *core.Pod)
+	DeleteAllocatedPod(pod *core.Pod)
+}
+
+// Notes:
+// 1. If the SchedulerAlgorithm found sufficient free resource, only PodBindInfo
+//    should be set.
+//    If the SchedulerAlgorithm found sufficient preemptable resource, only
+//    PodPreemptInfo should be set.
+//    Otherwise, only PodWaitInfo can be optionally set.
+// 2. The selected node in PodBindInfo requires:
+//    1. Must be within candidateNodes:
+//       All existing nodes which can be constructed from AddNode/DeleteNode.
+//       Otherwise, the Pod after bound will be probably deleted by the
+//       GarbageCollectionController.
+//    2. Better to be within suggestedNodes:
+//       The input parameter of Schedule().
+//       Otherwise, the Pod after bound will not respect previous pod schedule
+//       decision from K8S Default Scheduler, i.e. this binding is incompatible
+//       with K8S Default Scheduler.
+type PodScheduleResult struct {
+	PodWaitInfo    *PodWaitInfo
+	PodPreemptInfo *PodPreemptInfo
+	PodBindInfo    *si.PodBindInfo
+}
+
+// PodUID -> PodScheduleStatus
+type PodScheduleStatuses map[types.UID]*PodScheduleStatus
+
+// Used to track the PodScheduleResult
+type PodScheduleStatus struct {
+	// The Pod which will be used in current PodState.
+	// For example, in PodBinding state, it should be a Pod used to bind,
+	// i.e. with all placements set in the Pod.
+	Pod      *core.Pod
+	PodState PodState
+	// The already tried Pod binding attempts, i.e. the scheduled times for a Pod
+	// in PodBinding state.
+	PodBindAttempts   int32
+	PodScheduleResult *PodScheduleResult
+}
+
+type PodState string
+
+// [VirtualState]: This state is not tracked in PodScheduleStatuses.
+//
+// Pod is unknown for the scheduler, such as the Pod does not exist, completed
+// or has not been informed to the scheduler.
+//
+// A completed Pod is the same as a deleted Pod from the scheduler view and they
+// will never be informed to the scheduler, thus they are rejected to be scheduled.
+// However, the not yet informed not completed Pod will be eventually informed to
+// the scheduler, then transition to not VirtualState, thus they are accepted
+// to be scheduled.
+//
+// This state can be transitioned from any state, but can only transition to:
+// -> PodWaiting
+// -> PodBound
+const PodUnknown PodState = "Unknown"
+
+// [AllocatedState]: The Pod is considered to be allocated from the scheduler view.
+const (
+	// Pod is waiting for preemptable or free resource to appear.
+	// [StartState]
+	// -> PodPreempting
+	// -> PodBinding
+	PodWaiting PodState = "Waiting"
+
+	// Pod is waiting for the appeared preemptable resource to be free by preemption.
+	// -> PodBinding
+	// -> PodWaiting
+	PodPreempting PodState = "Preempting"
+
+	// Pod is binding on the free resource.
+	// [AllocatedState]
+	// -> PodBound
+	PodBinding PodState = "Binding"
+
+	// Pod is bound on the free resource.
+	// [FinalState]
+	// [AllocatedState]
+	PodBound PodState = "Bound"
+)
+
+func IsAllocated(state PodState) bool {
+	return state == PodBinding || state == PodBound
+}
+
+// No need to use it recover scheduler waiting resource
+type PodWaitInfo struct {
+	// NodeName ->  The reason why the node cannot be used for the Pod to allocate.
+	FailedNodeReasons map[string]string
+}
+
+// No need to use it recover scheduler preempting resource
+type PodPreemptInfo struct {
+	// Including all victim Pods in the victim cell, although the default scheduler
+	// will only preempt victims on one node.
+	// Need to ensure the newly deleted victim Pods are eventually removed from here,
+	// otherwise, the default scheduler refuse to execute any preemption.
+	// Need to ensure the newly added victim Pods are eventually added to here,
+	// otherwise, the preemption will never complete.
+	VictimPods []*core.Pod
+}
+
+type PodKey struct {
+	Namespace string
+	Name      string
+	UID       types.UID
+}
+
+func NewPodKey(namespace string, name string, uid types.UID) *PodKey {
+	return &PodKey{
+		Namespace: namespace,
+		Name:      name,
+		UID:       uid,
+	}
+}
+
+func (pk *PodKey) String() string {
+	return fmt.Sprintf("%v(%v/%v)", pk.UID, pk.Namespace, pk.Name)
+}
