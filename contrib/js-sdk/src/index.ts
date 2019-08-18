9fc2f3fe21c99e53d57ff840a46188737bec5dba
Yi Yi <yiyi@microsoft.com>
2019-08-18 13:18:07 +0800

    add index.ts

---
+++ contrib/js-sdk/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License in the project root for license information.
+ * @author Microsoft
+ */
+
+import { JobClient } from './client/jobClient';
+import { IPAICluster } from './models/cluster';
+import { IJobConfig, IJobStatusV1 } from './models/job';
+
+export {
+    JobClient,
+    IPAICluster,
+    IJobConfig,
+    IJobStatusV1
+};
