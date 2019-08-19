c56fe48603457194c93fbfe409e8f4b84c2d11e0
Binyang Li <binyli@microsoft.com>
2019-08-19 20:19:09 +0800

    add alert panel

---
+++ src/webportal/src/app/layout/alerts.jsx
@@ -0,0 +1,78 @@
+import c from 'classnames';
+import {Panel, List, mergeStyleSets, getFocusStyle, getTheme, initializeIcons, PanelType} from 'office-ui-fabric-react';
+import React, {useCallback, useState, useEffect} from 'react';
+
+import {initTheme} from '../components/theme';
+
+import config from '../config/webportal.config';
+
+initTheme();
+initializeIcons();
+
+const theme = getTheme();
+const {palette, semanticColors} = theme;
+
+const classNames = mergeStyleSets({
+  itemCell: [
+    getFocusStyle(theme, {inset: -1}),
+    {
+      minHeight: 54,
+      padding: 10,
+      boxSizing: 'border-box',
+      borderBottom: `1px solid ${semanticColors.bodyDivider}`,
+      display: 'flex',
+      selectors: {
+        '&:hover': {background: palette.neutralLight},
+      },
+    },
+  ],
+});
+
+export const NotificationButton = () => {
+  const [panelOpened, setPanelOpened] = useState(false);
+  const [alertItems, setAlertItems] = useState([]);
+
+  useEffect(() => {
+    const alertsUrl = `${config.alertManagerUri}/alert-manager/api/v1/alerts?silenced=false&inhibited=false`;
+    fetch(alertsUrl).then((res) => {
+      if (!res.ok) {
+        throw Error('Failed to get alert infos');
+      }
+      res.json().then((data) => {
+        if (data.status !== 'success') {
+          throw Error('Failed to get alerts data');
+        }
+        setAlertItems(data.data);
+      }).catch(() => {
+        throw Error('Get alerts json failed');
+      });
+    }).catch(alert);
+  }, []);
+
+  const open = useCallback(() => {
+    setPanelOpened(true);
+  });
+  const close = useCallback(() => {
+    setPanelOpened(false);
+  }, [setAlertItems]);
+
+  return (
+    <React.Fragment>
+      <i className={c('fa fa-bell-o')} style={{fontSize: '16px'}} onClick={open}></i>
+      <Panel
+        isOpen={panelOpened}
+        isLightDismiss={true}
+        onDismiss={close}
+        type={PanelType.medium}
+        headerText={'Alerts'}>
+        <List items={alertItems} onRenderCell={(item) => {
+          return (
+             <div className={classNames.itemCell} data-is-focusable={true}>
+               {item.annotations.summary}
+            </div>
+          );
+        }}/>
+      </Panel>
+    </React.Fragment>
+  );
+};
--- src/webportal/src/app/layout/layout.component.js
+++ src/webportal/src/app/layout/layout.component.js
@@ -24,6 +24,11 @@ require('admin-lte/dist/css/AdminLTE.min.css');
 require('admin-lte/dist/css/skins/_all-skins.min.css');
 require('font-awesome/css/font-awesome.min.css');
 require('./layout.component.scss');
+
+const ReactDOM = require('react-dom');
+const React = require('react');
+const alerts = require('./alerts.jsx');
+
 const userAuthComponent = require('../user/user-auth/user-auth.component.js');
 const userLogoutComponent = require('../user/user-logout/user-logout.component.js');
 const userLoginNavComponent = require('../user/user-login/user-login-nav.component.ejs');
@@ -42,6 +47,8 @@ if (!userAuthComponent.checkAdmin()) {
   $('#sidebar-menu--cluster-view').hide();
 }
 
+const notificationButtonContainer = document.getElementById('notification-button');
+ReactDOM.render(<alerts.NotificationButton />, notificationButtonContainer);
 
 if (authnMethod === 'OIDC') {
   $('#sidebar-menu--cluster-view--user-management').hide();
