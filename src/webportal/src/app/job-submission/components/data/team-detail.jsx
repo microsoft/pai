69baad3ff8bbc40dd48faa19159ec0002bbe7dcf
fenghuajia <17610303377@163.com>
2019-08-30 17:53:52 +0800

    team-storage-detail layout adjustment in job-submission page.2

--- src/webportal/src/app/job-submission/components/data/team-detail.jsx
+++ src/webportal/src/app/job-submission/components/data/team-detail.jsx
@@ -16,7 +16,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import React from 'react';
-import {Dialog, DialogType, FontClassNames, FontSizes, getTheme, DetailsList, DetailsListLayoutMode, FontWeights, Link, SelectionMode} from 'office-ui-fabric-react';
+import {Dialog, DialogType, FontClassNames, FontSizes, DetailsList, DetailsListLayoutMode, FontWeights, Link, SelectionMode} from 'office-ui-fabric-react';
 import PropTypes from 'prop-types';
 import c from 'classnames';
 import t from '../../../components/tachyons.scss';
@@ -84,8 +84,6 @@ export default function TeamDetail({isOpen = false, config, servers=[], hide}) {
     },
   ];
 
-  const {spacing} = getTheme();
-
   return (
     <Dialog
       hidden={!isOpen}
@@ -110,20 +108,20 @@ export default function TeamDetail({isOpen = false, config, servers=[], hide}) {
         Team storage <span style={{fontWeight: FontWeights.semibold}}>{config.name}</span> is configured for group <span style={{fontWeight: FontWeights.semibold}}>{config.gpn}</span>. For more details, please contact the cluster admin.
       </div>
       <div>
-        <div className={c(t.mt5)} style={{fontSize: FontSizes.small, marginBottom: spacing.s3, fontWeight: FontWeights.bold}}>How to upload data ?</div>
+        <div className={c(t.mt5)} style={{fontSize: FontSizes.small, marginBottom: '12px', fontWeight: FontWeights.bold}}>How to upload data ?</div>
         <div className={c(FontClassNames.small)}>
           Please upload data to corresponding <span style={{fontWeight: FontWeights.semibold}}>server path</span> before use. Different server types require different upload methods.
         </div>
         <div>
         {usedServers.map((server) => {
             return (
-            <div key={server.spn} className={c(FontClassNames.small, t.ml4)} style={{marginTop: spacing.s3}}>
+            <div key={server.spn} className={c(FontClassNames.small, t.ml4)} style={{marginTop: '12px'}}>
                 {NAS_TIPS[server.type]}
             </div>
             );
         })}
         </div>
-        <div className={c(t.mt5)} style={{fontSize: FontSizes.small, fontWeight: FontWeights.bold, marginBottom: spacing.s3}}>How to use data ?</div>
+        <div className={c(t.mt5)} style={{fontSize: FontSizes.small, fontWeight: FontWeights.bold, marginBottom: '12px'}}>How to use data ?</div>
         <div className={c(FontClassNames.small)}>
             By selecting team storage, the <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> will be automatically mounted to <span style={{fontWeight: FontWeights.semibold}}>Path</span> when job runs.
             Please treat it as local folder.
@@ -152,80 +150,80 @@ TeamDetail.propTypes = {
 
 export const NAS_TIPS = {
     nfs: (
-        <div>
-            <span style={{fontWeight: FontWeights.semibold}}>NFS</span><br/>
-            Mount <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> to local then copy data. For example:<br/>
-            <div className={c(FontClassNames.small)}>
-            apt-get update && apt-get install -y nfs-common<br/>
-            mount -t nfs4 <span style={{fontWeight: FontWeights.semibold}}>[Server Path]</span> /mnt<br/>
-            cp <span style={{fontWeight: FontWeights.semibold}}>[local data]</span> /mnt/<br/>
-            umount -l /mnt<br/>
-            </div>
+      <div>
+        <div style={{fontWeight: FontWeights.semibold}}>NFS</div>
+        <div>Mount <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> to local then copy data. For example:</div>
+        <div className={c(FontClassNames.small)}>
+          <div>apt-get update && apt-get install -y nfs-common</div>
+          <div>mount -t nfs4 <span style={{fontWeight: FontWeights.semibold}}>[Server Path]</span> /mnt</div>
+          <div>cp <span style={{fontWeight: FontWeights.semibold}}>[local data]</span> /mnt/</div>
+          <div>umount -l /mnt</div>
         </div>
-        ),
+      </div>
+      ),
     samba: (
-        <div>
-            <span style={{fontWeight: FontWeights.semibold}}>Samba</span><br/>
-            Mount <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> to local then copy data. For example:<br/>
-            <div className={c(FontClassNames.small)}>
-            apt-get update && apt-get install -y cifs-utils<br/>
-            mount -t cifs //<span style={{fontWeight: FontWeights.semibold}}>[Server Path]</span> /mnt<br/>
-            cp <span style={{fontWeight: FontWeights.semibold}}>[local data]</span> /mnt/<br/>
-            umount -l /mnt<br/>
-            </div>
-            In windows, Samba server can be accessed directly through file explorer. Add prefix {'\\\\'} to <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> {'and convert all "/"s to "\\"'} then open in file explorer.
+      <div>
+        <div style={{fontWeight: FontWeights.semibold}}>Samba</div>
+        <div>Mount <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> to local then copy data. For example:</div>
+        <div className={c(FontClassNames.small)}>
+          <div>apt-get update && apt-get install -y cifs-utils</div>
+          <div>mount -t cifs //<span style={{fontWeight: FontWeights.semibold}}>[Server Path]</span> /mnt</div>
+          <div>cp <span style={{fontWeight: FontWeights.semibold}}>[local data]</span> /mnt/</div>
+          <div>umount -l /mnt</div>
         </div>
-        ),
-   azurefile: (
-        <div>
-            <span style={{fontWeight: FontWeights.semibold}}>AzureFile</span><br/>
-        {'Download '}
+        <div>In windows, Samba server can be accessed directly through file explorer. Add prefix {'\\\\'} to <span style={{fontWeight: FontWeights.semibold}}>Server Path</span> {'and convert all "/"s to "\\"'} then open in file explorer.</div>
+      </div>
+      ),
+    azurefile: (
+      <div>
+        <div style={{fontWeight: FontWeights.semibold}}>AzureFile</div>
+        <span>Download</span>
         <Link
-        href='https://azure.microsoft.com/en-us/features/storage-explorer/'
-        target='_blank'
-        style={{fontWeight: FontWeights.semibold}}
+          href='https://azure.microsoft.com/en-us/features/storage-explorer/'
+          target='_blank'
+          style={{fontWeight: FontWeights.semibold}}
         >
-            {'Microsoft Azure Storage Explorer '}
+          Microsoft Azure Storage Explorer
         </Link>
-        to upload data to AzureFile. Use [Server Root Path] and ask cluster admin for golden key to access storage.
-        </div>
-        ),
-   azureblob: (
-        <div>
-        <span style={{fontWeight: FontWeights.semibold}}>AzureBlob</span><br/>
-        {'Download '}
+        <span>to upload data to AzureFile. Use [Server Root Path] and ask cluster admin for golden key to access storage.</span>
+      </div>
+      ),
+    azureblob: (
+      <div>
+        <div style={{fontWeight: FontWeights.semibold}}>AzureBlob</div>
+        <span>Download</span>
         <Link
-        href='https://azure.microsoft.com/en-us/features/storage-explorer/'
-        target='_blank'
-        style={{fontWeight: FontWeights.semibold}}
+          href='https://azure.microsoft.com/en-us/features/storage-explorer/'
+          target='_blank'
+          style={{fontWeight: FontWeights.semibold}}
         >
-            {'Microsoft Azure Storage Explorer '}
+          Microsoft Azure Storage Explorer
         </Link>
-        to upload data to AzureBlob. Use [Server Root Path] and ask cluster admin for golden key to access storage.
-        </div>
-        ),
+        <span>to upload data to AzureBlob. Use [Server Root Path] and ask cluster admin for golden key to access storage.</span>
+      </div>
+      ),
     hdfs: (
-        <div>
-        <span style={{fontWeight: FontWeights.semibold}}>HDFS</span><br/>
-            {'Use '}
+      <div>
+        <div style={{fontWeight: FontWeights.semibold}}>HDFS</div>
+        <span>Use</span>
         <Link
-        href='https://github.com/microsoft/hdfs-mount'
-        target='_blank'
-        style={{fontWeight: FontWeights.semibold}}
+          href='https://github.com/microsoft/hdfs-mount'
+          target='_blank'
+          style={{fontWeight: FontWeights.semibold}}
         >
-            {'hdfs-mount '}
+          hdfs-mount
         </Link>
-            {'mount HDFS to local then upload. Or use '}
+        <span>mount HDFS to local then upload. Or use</span>
         <Link
-        href='https://hadoop.apache.org/docs/r1.0.4/webhdfs.html'
-        target='_blank'
-        style={{fontWeight: FontWeights.semibold}}
+          href='https://hadoop.apache.org/docs/r1.0.4/webhdfs.html'
+          target='_blank'
+          style={{fontWeight: FontWeights.semibold}}
         >
-            {'WebHDFS '}
+          WebHDFS
         </Link>
-            {'to upload data directly.'}
-        </div>
-        ),
+        <span>to upload data directly.</span>
+      </div>
+      ),
   };
 
 export const SERVER_PATH = {
