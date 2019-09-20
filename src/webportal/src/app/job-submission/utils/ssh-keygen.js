9430fc8ba5f0089f73384ea8020f223ef75ad650
WangDian <wangdian82@gmail.com>
2019-09-20 17:28:38 +0800

    Fix lint error

--- src/webportal/src/app/job-submission/utils/ssh-keygen.js
+++ src/webportal/src/app/job-submission/utils/ssh-keygen.js
@@ -15,8 +15,8 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import { default as NodeRSA } from 'node-rsa';
-import { default as SSHPk } from 'sshpk';
+import * as NodeRSA from 'node-rsa';
+import * as SSHPk from 'sshpk';
 
 export function generateSSHKeyPair(bits) {
   const key = new NodeRSA({ b: bits });
