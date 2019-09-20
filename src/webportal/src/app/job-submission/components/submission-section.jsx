5f8b5b350e1afe250f8abda7556702c77a9446a0
Shuguang Wei (FA Talent) <v-shuwei@microsoft.com>
2019-09-20 14:51:45 +0800

    Disable 'Back' button when cloning

--- src/webportal/src/app/job-submission/components/submission-section.jsx
+++ src/webportal/src/app/job-submission/components/submission-section.jsx
@@ -83,6 +83,7 @@ export const SubmissionSection = props => {
   const monaco = useRef(null);
 
   const { vcNames, errorMessages, setErrorMessage } = useContext(Context);
+  const params = new URLSearchParams(window.location.search);
 
   const _protocolAndErrorUpdate = protocol => {
     if (!isEqual(jobProtocol, protocol)) {
@@ -211,6 +212,7 @@ export const SubmissionSection = props => {
       <Stack horizontal horizontalAlign='space-between'>
         <DefaultButton
           text='Back'
+          disabled={params.has('op') && params.get('op') === 'resubmit'}
           onClick={() => {
             history.push('/');
           }}
