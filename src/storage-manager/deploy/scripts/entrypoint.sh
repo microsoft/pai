071a76f230e93233632d30fab5c2f3c3e2c8d62f
TongboZhang <t-tozh@microsoft.com>
2019-09-23 14:13:35 +0800

    Refactor code

--- src/storage-manager/deploy/scripts/entrypoint.sh
+++ src/storage-manager/deploy/scripts/entrypoint.sh
@@ -17,27 +17,52 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-user=$1
-domain=$2
-uname=$3
-paiuser=$4
-userspath=/share/pai/users
+# copy smb.conf
+echo "copy smb.conf ----------"
+cp /etc/pai-config/smb.conf /etc/samba/smb.conf
 
-umask 000
-if [ ! -d "$userspath" ];then
-  mkdir -p "$userspath"
-  chown "$paiuser":"$paiuser" "$userspath"
+# install krb5 and join domain
+if [[ $SECURETYPE == "ADS" ]]; then
+  echo "install krb5 ----------"
+  cp /etc/pai-config/krb5.conf /etc/krb5.conf
+  cp /etc/pai-config/nsswitch.conf /etc/nsswitch.conf
+  apt update && apt install -y \
+    winbind \
+    libpam-winbind \
+    libnss-winbind \
+    libpam-krb5 \
+    krb5-config \
+    krb5-user
+  echo "join domain ----------"
+  net ads join -U "$DOMAINUSER"%"$DOMAINPWD"
+  echo "domain info ----------"
+  net ads info
 fi
 
-umask 007
-userpath="$userspath"/"$user"
-if [ ! -d "$userpath" ];then
-  mkdir -p "$userpath"
-  if [ $user != $uname ]
-  then
-    chown "$domain\\$user":"$paiuser" $userpath
-  else
-    chown "$user":"$paiuser" $userpath
-  fi
-  setfacl -m u:"$paiuser":rwx $userpath
+# create folders
+echo "create folders ----------"
+mkdir -p /share/pai/data
+mkdir -p /share/pai/users
+
+# load nfs modules
+echo "load nfs modules ----------"
+modprobe nfs
+modprobe nfsd
+
+# restart services
+echo "restart services ----------"
+if [[ $SECURETYPE == "ADS" ]]; then
+  service winbind restart
 fi
+service smbd restart
+service rpcbind restart
+service nfs-kernel-server restart
+
+# add paismbuser
+echo "add paismbuser ----------"
+useradd $PAISMBUSER
+(echo $PAISMBPWD && echo $PAISMBPWD) | ./usr/bin/smbpasswd -a $PAISMBUSER
+
+# sleep
+echo "sleep infinity ----------"
+sleep infinity
