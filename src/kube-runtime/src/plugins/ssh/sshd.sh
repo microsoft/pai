c9f7b4433f3a15d586ea4bae2b11e5d85c6505a3
Yifan Xiong <yifan.xiong@microsoft.com>
2019-10-14 17:54:18 +0800

    Fix sshd issues in k8s runtime
    Fix sshd issues in k8s runtime.

--- src/kube-runtime/src/plugins/ssh/sshd.sh
+++ src/kube-runtime/src/plugins/ssh/sshd.sh
@@ -28,13 +28,13 @@ function prepare_ssh()
   mkdir -p ${SSH_DIR}
   chmod 700 ${SSH_DIR}
   touch ${SSH_DIR}/authorized_keys
-  chmod 644 ${SSH_DIR}/authorized_keys
+  chmod 600 ${SSH_DIR}/authorized_keys
 
   mkdir -p /var/run/sshd
 
 # Set sshd config
-  sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-  sed -i 's/Port.*/Port '$PAI_CONTAINER_SSH_PORT'/' /etc/ssh/sshd_config
+  sed -i 's/[# ]*PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+  sed -i 's/[# ]*Port.*/Port '$PAI_CONTAINER_SSH_PORT'/' /etc/ssh/sshd_config
   echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
 
   sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
