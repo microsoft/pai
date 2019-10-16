b6ca2dac3ed7f0ff3ded27cbcb3cfb0cc87d8757
Binyang2014 <binyli@microsoft.com>
2019-10-16 11:07:17 +0800

    Enable RBAC for watchdog (#3721)
    * Enable RBAC for watchdog
    
    * change log level and stop script

--- src/watchdog/deploy/stop.sh
+++ src/watchdog/deploy/stop.sh
@@ -19,6 +19,7 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-kubectl delete --ignore-not-found --now daemonset/watchdog
-kubectl delete --ignore-not-found --now deployment/watchdog
-kubectl delete --ignore-not-found --now configmap/watchdog
+kubectl delete --ignore-not-found --now deployment/watchdog || exit $?
+kubectl delete --ignore-not-found --now clusterrolebinding/watchdog-role-binding || exit $?
+kubectl delete --ignore-not-found --now clusterrolebinding/watchdog-role || exit $?
+kubectl delete --ignore-not-found --now serviceaccount/watchdog-account || exit $?
