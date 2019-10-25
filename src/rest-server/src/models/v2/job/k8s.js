85425cf95f8a5b3b365e8210d0df14c6c3b68bfc
Yifan Xiong <yifan.xiong@microsoft.com>
2019-10-25 16:37:13 +0800

    [Rest Server] Decompress large framework (#3771)
    * Decompress large framework
    
    Decompress possible fields for large framework.
    
    Reference: https://github.com/microsoft/frameworkcontroller/pull/44
    
    * Fix lint
    
    Fix lint.
    
    * Remove task status in framework summary
    
    Remove task status in framework summary.

--- src/rest-server/src/models/v2/job/k8s.js
+++ src/rest-server/src/models/v2/job/k8s.js
@@ -18,6 +18,7 @@
 
 // module dependencies
 const {Agent} = require('https');
+const zlib = require('zlib');
 const axios = require('axios');
 const yaml = require('js-yaml');
 const base32 = require('base32');
@@ -76,6 +77,14 @@ const decodeName = (name, labels) => {
   }
 };
 
+const decompressField = (val) => {
+  if (val == null) {
+    return null;
+  } else {
+    return JSON.parse(zlib.gunzipSync(Buffer.from(val, 'base64')).toString());
+  }
+};
+
 const convertState = (state, exitCode, retryDelaySec) => {
   switch (state) {
     case 'AttemptCreationPending':
@@ -131,9 +140,9 @@ const convertFrameworkSummary = (framework) => {
     appExitCode: completionStatus ? completionStatus.code : null,
     virtualCluster: framework.metadata.labels ? framework.metadata.labels.virtualCluster : 'unknown',
     totalGpuNumber: framework.metadata.annotations ? framework.metadata.annotations.totalGpuNumber : 0,
-    totalTaskNumber: framework.status.attemptStatus.taskRoleStatuses.reduce(
-      (num, statuses) => num + statuses.taskStatuses.length, 0),
-    totalTaskRoleNumber: framework.status.attemptStatus.taskRoleStatuses.length,
+    totalTaskNumber: framework.spec.taskRoles.reduce(
+      (num, spec) => num + spec.taskNumber, 0),
+    totalTaskRoleNumber: framework.spec.taskRoles.length,
   };
 };
 
@@ -184,6 +193,11 @@ const convertTaskDetail = async (taskStatus, ports, userName, jobName, taskRoleN
 };
 
 const convertFrameworkDetail = async (framework) => {
+  // check fields which may be compressed
+  if (framework.status.attemptStatus.taskRoleStatuses == null) {
+    framework.status.attemptStatus.taskRoleStatuses = decompressField(framework.status.attemptStatus.taskRoleStatusesCompressed);
+  }
+
   const completionStatus = framework.status.attemptStatus.completionStatus;
   const diagnostics = completionStatus ? completionStatus.diagnostics : null;
   const exitDiagnostics = generateExitDiagnostics(diagnostics);
