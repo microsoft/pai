c2228c8553b9fce2c74e6046be998dd5a981bdba
WangDian <wangdian82@gmail.com>
2019-11-07 18:31:21 +0800

    Separate service and daemonset

--- src/storage-manager/deploy/stop.sh
+++ src/storage-manager/deploy/stop.sh
@@ -17,6 +17,7 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+kubectl delete --ignore-not-found --now "service/storage-manager-service"
 kubectl delete --ignore-not-found --now "daemonset/storage-manager-ds"
 
 if kubectl get configmap | grep -q "storage-configuration"; then
