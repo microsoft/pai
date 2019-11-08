d9f6faca60dcc18eeec2a181a3e1cbd0ba36e9ef
hzy46 <362583303@qq.com>
2019-11-08 10:40:18 +0800

    add postgresql

---
+++ src/postgresql/README.md
@@ -0,0 +1,28 @@
+## Postgresql
+
+Postgresql is an internal service for structured information persistence. By default, the service uses the folder `/paiInternal` on the master node for data storage. The real data directory for the postgresql would be `/paiInternal/pgdata`. The default service configurations are as follows:
+
+```yaml
+postgresql:
+  user: root
+  passwd: rootpass
+  port: 5432
+  db: openpai
+``` 
+
+One can override these settings by editing `services-configuration.yaml` .
+
+### Table Initialization
+
+If it is the first time the service launches, it will execute `src/init_table.sql` to initialize the table structure. The initialization won't be fired if the service detects old data. If you want to re-trigger it, please remove `/paiInternal/pgdata` and restart the service manually.
+
+### How to Access the Database
+
+The database connection string is written to the cluster configuration object in `config/postgresql.py`. One can use `cluster_cfg['postgresql']['connectionStr']` to retrieve it in any template files.
+
+Particularly, the connection string is exposed as an environmental variable in `rest-server`:
+
+```bash
+# in rest-server pod
+SQL_CONNECTION_STR=postgresql://root:rootpass@<master-ip>:5432/openpai
+```
---
+++ src/postgresql/config/postgresql.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import copy
+import logging
+
+
+class Postgresql(object):
+    def __init__(self, cluster_conf, service_conf, default_service_conf):
+        self.cluster_conf = cluster_conf
+        self.service_conf = self.merge_service_configuration(default_service_conf, service_conf)
+        self.logger = logging.getLogger(__name__)
+
+    @staticmethod
+    def merge_service_configuration(overwrite_srv_cfg, default_srv_cfg):
+        if overwrite_srv_cfg is None:
+            return default_srv_cfg
+        srv_cfg = default_srv_cfg.copy()
+        for k in overwrite_srv_cfg:
+            srv_cfg[k] = overwrite_srv_cfg[k]
+        return srv_cfg
+
+    def validation_pre(self):
+        machine_list = self.cluster_conf['machine-list']
+        if len([host for host in machine_list if host.get('pai-master') == 'true']) != 1:
+            return False, '1 and only 1 "pai-master=true" machine is required to deploy the postgresql service'
+        return True, None
+
+    def run(self):
+        result = copy.deepcopy(self.service_conf)
+        machine_list = self.cluster_conf['machine-list']
+        master_ip = [host['hostip'] for host in machine_list if host.get('pai-master') == 'true'][0]
+        result['host'] = master_ip
+        result['connectionStr'] = 'postgresql://{}:{}@{}:{}/{}'.format(
+            result['user'], result['passwd'], result['host'], result['port'], result['db'])
+        result['configured'] = True
+        return result
+
+    def validation_post(self, conf):
+        return True, None
