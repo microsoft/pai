aec1896f1262be36f46813bcae18b531444886a0
WangDian <wangdian82@gmail.com>
2019-11-13 15:26:45 +0800

    Generate secret pair when deploy cluster-configuration

--- src/cluster-configuration/deploy/secret-create.sh
+++ src/cluster-configuration/deploy/secret-create.sh
@@ -17,5 +17,11 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# Can generate ssh key pair here
+# Create secret under ssh-configuration
+pushd $(dirname "$0") > /dev/null
+
+echo -e 'y\n' | ssh-keygen -t rsa -N "" -f ssh-configuration/id_rsa -q
 kubectl create secret generic job-ssh-secret --from-file=ssh-privatekey=ssh-configuration/id_rsa --from-file=ssh-publickey=ssh-configuration/id_rsa.pub --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
+rm ssh-configuration/*
+
+popd > /dev/null
