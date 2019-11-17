e0264576bdfe58de0e285c2a229256a5c019f695
Yifan Xiong <yifan.xiong@microsoft.com>
2019-11-17 17:20:08 +0800

    Add comments
    Add comments

--- src/rest-server/src/models/v2/job/k8s.js
+++ src/rest-server/src/models/v2/job/k8s.js
@@ -319,6 +319,8 @@ const generateTaskRole = (taskRole, labels, config) => {
         },
         spec: {
           privileged: false,
+          // workaround for pending pods due to no ready sandbox before the fix
+          // https://github.com/kubernetes/kubernetes/commit/3fac48f8
           restartPolicy: gangAllocation === 'true' ? 'Never' : 'OnFailure',
           serviceAccountName: 'frameworkbarrier-account',
           initContainers: [
