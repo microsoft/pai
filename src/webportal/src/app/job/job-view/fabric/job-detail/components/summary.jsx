acb1766f7a936f4b88dc4669b6d5f1c97f16ff42
Mingliang Tao <mintao@microsoft.com>
2019-11-28 14:38:57 +0800

    Revert url param from 'jobname' to 'jobName' (#3890)

--- src/webportal/src/app/job/job-view/fabric/job-detail/components/summary.jsx
+++ src/webportal/src/app/job/job-view/fabric/job-detail/components/summary.jsx
@@ -375,7 +375,7 @@ export default class Summary extends React.Component {
 
     const params = new URLSearchParams(window.location.search);
     const namespace = params.get('username');
-    const jobName = params.get('jobname');
+    const jobName = params.get('jobName');
 
     return (
       <div className={className}>
@@ -502,7 +502,7 @@ export default class Summary extends React.Component {
               <div className={c(t.gray, FontClassNames.medium)}>Retries</div>
               {this.checkRetryLink() ? (
                 <Link
-                  href={`job-retry.html?username=${namespace}&jobname=${jobName}`}
+                  href={`job-retry.html?username=${namespace}&jobName=${jobName}`}
                 >
                   <div className={c(t.mt3, FontClassNames.mediumPlus)}>
                     {jobInfo.jobStatus.retries}
@@ -574,7 +574,7 @@ export default class Summary extends React.Component {
               <div className={c(t.flex)}>
                 <Link
                   styles={{ root: [FontClassNames.mediumPlus] }}
-                  href={`job-retry.html?username=${namespace}&jobname=${jobName}`}
+                  href={`job-retry.html?username=${namespace}&jobName=${jobName}`}
                   disabled={!this.checkRetryLink()}
                   target='_blank'
                 >
