e2b38bf1ec467c19fb561f6b4bd21c6efd6ebf8c
Binyang Li <binyli@microsoft.com>
2019-12-02 17:10:59 +0800

    fix gpu order issue

--- src/job-exporter/src/nvidia.py
+++ src/job-exporter/src/nvidia.py
@@ -101,8 +101,11 @@ def parse_smi_xml_result(smi):
 
     result = {}
 
-    for gpu in gpus:
-        minor = gpu.getElementsByTagName("minor_number")[0].childNodes[0].data
+    for index, gpu in enumerate(gpus):
+        if os.getenv("LAUNCHER_TYPE") == "k8s":
+            minor = index
+        else:
+            minor = gpu.getElementsByTagName("minor_number")[0].childNodes[0].data
         utilization = gpu.getElementsByTagName("utilization")[0]
 
         gpu_util = utilization.getElementsByTagName("gpu_util")[0].childNodes[0].data.replace("%", "").strip()
