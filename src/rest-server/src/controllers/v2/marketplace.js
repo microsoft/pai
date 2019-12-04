4a22636cfde3a609394496eec292069e5bb79b4a
Shuguang Wei (FA Talent) <v-shuwei@microsoft.com>
2019-12-04 16:14:43 +0800

    Add validation to marketplace's rest-api

--- src/rest-server/src/controllers/v2/marketplace.js
+++ src/rest-server/src/controllers/v2/marketplace.js
@@ -1,295 +1,123 @@
-// postgresql
-const Pool = require('pg').Pool;
+const marketItemModel = require('@pai/models/v2/market-item');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'root',
-  port: 5432,
-});
-
-const getMarketList = (req, res) => {
-  pool.query(
-    `select * from marketplaceitem`,
-    function(error, results) {
-      if (error == null) {
-        return res.status(200).json(results.rows);
-      } else {
-        return res.status(404).json({
-          message: 'fetch failed',
-        });
-      }
+const getMarketList = async (req, res) => {
+  try {
+    const marketList = await marketItemModel.getMarketList();
+    if (marketList.length === 0) {
+      return res.status(404).json({message: 'Not Found'});
+    } else {
+      return res.status(200).json(marketList);
     }
-  );
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Service Internal Error',
+    });
+  }
 };
 
-const createMarketItem = (req, res) => {
-  const { id, name, author, createDate, updateDate, category, tags, introduction, description, jobConfig, submits, stars } = req.body;
+const createMarketItem = async (req, res) => {
+  const marketItem =[
+    req.body.id,
+    req.body.name,
+    req.body.author,
+    req.body.createDate,
+    req.body.updateDate,
+    req.body.category,
+    req.body.tags,
+    req.body.introduction,
+    req.body.description,
+    req.body.jobConfig,
+    req.body.submits,
+    req.body.stars,
+  ];
 
-  let data = [id, name, author, createDate, updateDate, category, tags, introduction, description, jobConfig, submits, stars];
-  let stmt_insert = 'insert into marketplaceitem(id, name, author, createDate, updateDate, category, tags, introduction, description, jobconfig, submits, stars) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';
-  pool.query(
-    stmt_insert,
-    data,
-    function(error, results) {
-      if (error == null) {
-        return res.status(200).json({
-          message: 'created successfully',
-        });
-      } else {
-        return res.status(500).json({
-          message: 'create failed',
-        });
-      }
-    }
-  );
+  try {
+    await marketItemModel.createMarketItem(marketItem);
+    return res.status(201).json({message: 'MerketItem is created successfully'});
+  } catch (error) {
+    return res.status(500).json({message: 'Service Internal Error'});
+  }
 };
 
-const getMarketItem = (req, res) => {
+const getMarketItem = async (req, res) => {
   var itemId = req.params.itemId;
-  pool.query(
-    `select * from marketplaceitem where id = $1`,
-    [itemId],
-    function(error, results) {
-      if (error == null){
-        return res.status(200).json(results.rows[0]);
-      } else {
-        return res.status(404).json({
-          message: 'fetch failed',
-        });
-      }
+  try {
+    const marketItem = await marketItemModel.getMarketItem(itemId);
+    if (marketItem === null) {
+      return res.status(404).json({message: 'Marketplace item not found'});
+    } else {
+      return res.status(200).json({
+        id: marketItem.id,
+        name: marketItem.name,
+        author: marketItem.author,
+        createDate: marketItem.createdate,
+        updateDate: marketItem.updatedate,
+        category: marketItem.category,
+        tags: marketItem.tags,
+        introduction: marketItem.introduction,
+        description: marketItem.description,
+        jobConfig: marketItem.jobconfig,
+        submits: marketItem.submits,
+        stars: marketItem.stars,
+      });
     }
-  );
+  } catch (error) {
+    return res.status(500).json({message: 'Service Internal Error'});
+  }
 };
 
-const updateMarketItem = (req, res) => {
+const updateMarketItem = async (req, res) => {
   const itemId = req.params.itemId;
-  const { name, updateDate, category, tags, introduction, description, submits, stars } = req.body;
-
-  let data = [name, updateDate, category, tags, introduction, description, submits, stars, itemId];
-  let stmt_update = `update marketplaceitem set name = $1, updatedate = $2, category = $3, tags = $4, introduction = $5, description = $6, submits = $7, stars = $8 where id = $9`;
-  pool.query(
-    stmt_update,
-    data,
-    function(error, results) {
-      // success
-      if (error == null) {
-        return res.status(200).json({
-          message: `updated successfully`,
-        });
-      } else {
-        // failure
-        return res.status(404).json({
-          message: 'update failed',
-        });
-      }
-    }
-  );
+  const data = [req.body.name, req.body.updateDate, req.body.category, req.body.tags, req.body.introduction, req.body.description, itemId];
+  try {
+    await marketItemModel.updateMarketItem(data);
+    return res.status(201).json({message: 'MarketItem updated successfully'});
+  } catch (error) {
+    return res.status(500).json({message: 'Service Internal Error'});
+  }
 };
 
-const deleteMarketItem = (req, res) => {
+const deleteMarketItem = async (req, res) => {
   const itemId = req.params.itemId;
-
-  let stmt_del = `delete from marketplaceitem where id = $1`;
-  db.run(
-    stmt_del,
-    [itemId],
-    function(error, results) {
-      //success
-      if (error == null) {
-        return res.status(200).json({
-          message: `deleted marketItem ${itemId} successfully!`,
-        });
-      } else {
-        // failure
-        return res.status(404).json({
-          message: 'delete failed',
-        });
-      }
-    }
-  );
+  try {
+    await marketItemModel.deleteMarketItem(itemId);
+    return res.status(200).json({message: 'MarketItem deleted successfully'});
+  } catch (error) {
+    return res.status(500).json({message: 'Service Internal Error'});
+  }
 };
 
-const getStarRelation = (req, res) => {
+const getStarRelation = async (req, res) => {
   const itemId = req.params.itemId;
   const userName = req.params.userName;
-  pool.query(
-    'select status from starrelation where marketitemid = $1 and userid = $2',
-    [itemId, userName],
-    function(error, results) {
-      if (error) {
-        return res.status(500).json({message: 'service internal error', });
-      } else {
-        if (results.rowCount === 1) {
-          return res.status(200).json({
-            message: results.rows[0].status.toString(),
-          });
-        } else {
-          return res.status(200).json({
-            message: 'false',
-          });
-        }
-      }
+  try {
+    const result = await marketItemModel.getStarRelation(itemId, userName);
+    if (result) {
+      return res.status(200).json({message: 'true'});
+    } else {
+      return res.status(200).json({message: 'false'});
     }
-  );
+  } catch (error) {
+    return res.status(500).json({message: 'service internal error'});
+  }
 };
 
-const updateStarRelation = (req, res) => {
+const updateStarRelation = async (req, res) => {
   const itemId = req.params.itemId;
   const userName = req.params.userName;
 
-  pool.query(
-    'select status from starrelation where marketitemid = $1 and userid = $2',
-    [itemId, userName],
-    function(error, results) {
-      if (error) {
-        return res.status(500).json({message: 'service internal error', });
-      } else {
-        if (results.rowCount === 1 && results.rows[0].status === true) {
-          deleteStarRelation(req, res);
-        } else {
-          addStarRelation(req, res);
-        }
-      }
+  try {
+    const result = await marketItemModel.getStarRelation(itemId, userName);
+    if (result) {
+      await marketItemModel.deleteStarRelation(itemId, userName);
+      return res.status(201).json({message: 'false'});
+    } else {
+      await marketItemModel.addStarRelation(itemId, userName);
+      return res.status(201).json({message: 'true'});
     }
-  );
-};
-
-const addStarRelation = (req, res) => {
-  const itemId = req.params.itemId;
-  const userName = req.params.userName;
-  // add star count
-  pool.query(
-    `select stars from marketplaceitem where id = $1`,
-    [itemId],
-    function(error, results) {
-      const row = results.rows[0];
-      if (error == null) {
-        var stars = row.stars + 1;
-
-        // count + 1 in table marketItem
-        const stmt_update = `update marketplaceitem set stars = $1 where id = $2`;
-        const data_update = [stars, itemId];
-        pool.query(
-          stmt_update,
-          data_update,
-          function(error, results) {
-            // fail
-            if (error) {
-              return res.status(500).json({message: 'service internal error', });
-            }
-          }
-        );
-      } else {
-        // fail
-        return res.status(500).json({message: 'service internal error', });
-      }
-    }
-  );
-
-  // update star-relation
-  pool.query(
-    `select * from starrelation where marketitemid = $1 and userid = $2`,
-    [itemId, userName],
-    function(error, results) {
-      if (error) {
-        return res.status(500).json({message: 'service internal error', });
-      } else {
-        if (results.rowCount === 0) {
-          // add relation in table StarRelation
-          const stmt_add = `insert into starrelation(marketitemid, userid, status) values($1, $2, $3)`;
-          const data_add = [itemId, userName, true];
-          pool.query(
-            stmt_add,
-            data_add,
-            function(error, results) {
-              // success
-              if (error == null) {
-                return res.status(200).json({
-                  message: 'true',
-                });
-              } else {
-                // failure
-                return res.status(500).json({message: 'service internal error', });
-              }
-            }
-          );
-        } else {
-          // change relation-status
-          const stmt_update = 'update starrelation set status = $1 where marketitemid = $2 and userid = $3';
-          const data_update = [true, itemId, userName];
-          pool.query(
-            stmt_update,
-            data_update,
-            function(error, results) {
-              if (error == null) {
-                return res.status(200).json({
-                  message: 'true',
-                });
-              } else {
-                return res.status(500).json({message: 'service internal error', });
-              }
-            }
-          );
-        }
-      }
-    }
-  );
-};
-
-const deleteStarRelation = (req, res) => {
-  const itemId = req.params.itemId;
-  const userName = req.params.userName;
-  // decrement star count
-  pool.query(
-    `select stars from marketplaceitem where id = $1`,
-    [itemId],
-    function(error, results) {
-      const row = results.rows[0];
-      if (error == null) {
-        stars = row.stars - 1;
-
-        // count - 1 in table marketItem
-        const stmt_update = `update marketplaceitem set stars = $1 where id = $2`;
-        const data_update = [stars, itemId];
-        pool.query(
-          stmt_update,
-          data_update,
-          function(error, results) {
-            // fail
-            if (error) {
-              return res.status(500).json({message: 'service internal error', });
-            }
-          }
-        );
-      } else {
-        // fail
-        return res.status(500).json({message: 'service internal error', });
-      }
-    }
-  );
-
-  // unstar relation in table StarRelation
-  const stmt_del = `update starrelation set status = $1 where marketitemid = $2 and userid = $3`;
-  const data_del = [false, itemId, userName];
-  pool.query(
-    stmt_del,
-    data_del,
-    function(error, results) {
-      // success
-      if (error == null) {
-        return res.status(200).json({
-          message: 'false',
-        });
-      } else {
-        // failure
-        return res.status(500).json({
-          message: 'service internal error',
-        });
-      }
-    }
-  );
+  } catch (error) {
+    return res.status(500).json({message: 'service internal error', });
+  }
 };
 
 module.exports = {
--- src/rest-server/src/routes/v2/marketplace.js
+++ src/rest-server/src/routes/v2/marketplace.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const marketItemController = require('@pai/controllers/v2/marketplace');
+const param = require('@pai/middlewares/parameter');
+const marketItemInputSchema = require('@pai/config/v2/marketItem');
 
 const router = new express.Router();
 
 router.route('/items')
     .get(marketItemController.getMarketList)
-    .post(marketItemController.createMarketItem);
+    .post(param.validate(marketItemInputSchema.marketItemCreateInputSchema), marketItemController.createMarketItem);
 
 router.route('/items/:itemId')
     .get(marketItemController.getMarketItem)
-    .put(marketItemController.updateMarketItem)
+    .put(param.validate(marketItemInputSchema.marketItemUpdateInputSchema), marketItemController.updateMarketItem)
     .delete(marketItemController.deleteMarketItem);
 
-    router.route('/items/:itemId/:userName')
+router.route('/items/:itemId/:userName')
     .get(marketItemController.getStarRelation)
     .put(marketItemController.updateStarRelation);
 
