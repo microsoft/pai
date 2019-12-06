4b0346bf3cadd73dc194afd0ff029a038ac4c5ee
Yifan Xiong <yifan.xiong@microsoft.com>
2019-12-06 11:16:01 +0800

    [Rest Server] Fix duplicate affinity group name issue (#3971)
    * Fix duplicate affinity group name issue
    
    Fix duplicate affinity group name issue.

--- src/rest-server/src/middlewares/v2/hived.js
+++ src/rest-server/src/middlewares/v2/hived.js
@@ -33,7 +33,7 @@ const convertPriority = (priorityClass='test') => {
   return priorityClass in priorityMap ? priorityMap[priorityClass] : null;
 };
 
-const hivedValidate = (protocolObj) => {
+const hivedValidate = (protocolObj, username) => {
   if (!hivedSchema.validate(protocolObj)) {
     throw createError('Bad Request', 'InvalidProtocolError', hivedSchema.validate.errors);
   }
@@ -162,14 +162,14 @@ const hivedValidate = (protocolObj) => {
 
       const affinityGroupName = hivedConfig.taskRoles[taskRole].affinityGroupName;
       podSpec.affinityGroup = affinityGroupName ? {
-        name: protocolObj.name + '/' + affinityGroupName,
+        name: `${username}~${protocolObj.name}/${affinityGroupName}`,
         members: affinityGroups[affinityGroupName].affinityTaskList,
       } : null;
     }
 
     if (defaultAffinityGroup != null) {
       podSpec.affinityGroup = {
-        name: `${protocolObj.name}/default`,
+        name: `${username}~${protocolObj.name}/default`,
         members: defaultAffinityGroup.affinityTaskList,
       };
     }
