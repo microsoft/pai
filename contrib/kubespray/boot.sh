a25483f2b1359660e2038b1ac475020873f2cf16
YundongYe <YundongYe@outlook.com>
2019-12-26 17:18:40 +0800

    Init commit

--- contrib/kubespray/boot.sh
+++ contrib/kubespray/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts "w:m:c:o:" opt; do
+while getopts "w:m:c:" opt; do
   case $opt in
     w)
       WORKER_LIST=$OPTARG
@@ -40,4 +40,33 @@ then
   exit 1
 fi
 
+echo "Create working folder in ${HOME}/pai-deploy"
+mkdir -p ~/pai-deploy/
+cd ~/pai-deploy
+
+echo "Clone kubespray source code from github"
+git clone https://github.com/kubernetes-sigs/kubespray.git
+
+echo "Checkout to the Release Branch"
+git checkout release-2.11
+
 echo "Install necessray packages"
+
+echo "Install Python3 and pip"
+sudo apt-get -y update
+sudo apt-get -y install software-properties-common python3 python3-dev
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+
+echo "Install paramiko"
+sudo pip3 install paramiko
+
+echo "Install kubespray"
+cd ${HOME}/pai-deploy/kubespray
+sudo pip3 install -r requirements.txt
+
+echo "Clone OpenPAI source code from github"
+git clone https://github.com/microsoft/pai.git
+
+echo "Checkout to the release branch "
+
