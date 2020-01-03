4d90a5d65933640c59d7e007c3b9b5071f2cec3e
YundongYe <YundongYe@outlook.com>
2020-01-03 12:30:52 +0800

    issue fix.

--- contrib/kubespray/boot.sh
+++ contrib/kubespray/boot.sh
@@ -72,6 +72,7 @@ sudo pip3 install -r requirements.txt
 echo "Clone OpenPAI source code from github"
 cd ${HOME}/pai-deploy
 git clone https://github.com/microsoft/pai.git
+cd pai
 git checkout yuye/quick-start-script
 
 cd ${HOME}/pai-deploy/pai/contrib/kubespray
