988d0df1b15a5fcb41ca359087a4a726c15538a9
Binyang Li <binyli@microsoft.com>
2020-01-14 14:41:53 +0800

    temp

--- src/kube-runtime/src/init.d/image_checker.py
+++ src/kube-runtime/src/init.d/image_checker.py
@@ -18,10 +18,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import argparse
+import base64
+import copy
 import http
 import logging
 import os
-import re
 import sys
 
 import requests
@@ -32,70 +33,174 @@ from common.utils import init_logger  #pylint: disable=wrong-import-position
 
 LOGGER = logging.getLogger(__name__)
 
+# The workflow:
+# 1. try v2 function, if not v2 support, ignore
+# 2. use WWW-Authenticate to get auth method if schema is Bearer
+# 3. get token by modify the request
+# 4. Try to get image manifest, use HEAD, don't need response body
 
-def _is_docker_hub_uri(uri):
-    if re.fullmatch(r"(?:[a-z\-_.0-9]+\/)?[a-z\-_.0-9]+(?::[a-z\-_.0-9]+)?",
-                    uri):
-        return True
-    return False
+BEARER_AUTH = "Bearer"
+BASIC_AUTH = "Basic"
 
 
-def _get_docker_repository_name(image_name):
+class ImageChecker():
+    def __init__(self, job_config, secret):
+        prerequisites = job_config["prerequisites"]
+        task_role_name = os.getenv("PAI_CURRENT_TASK_ROLE_NAME")
+        task_role = job_config["taskRoles"][task_role_name]
+        docker_image_name = task_role["dockerImage"]
+
+        docker_images = list(
+            filter(lambda pre: pre["name"] == docker_image_name,
+                   prerequisites))
+        assert len(docker_images) == 1
+        image_info = docker_images[0]
+
+        self.image_uri = image_info["uri"]
+        self.registry_uri = "https://index.docker.io/v2/"
+        self.basic_auth_headers = {}
+        self.bearer_auth_headers = {}
+        self.registry_auth_type = BASIC_AUTH
+
+        if "auth" in image_info:
+            auth = image_info["auth"]
+            self._init_auth_info(auth, secret, self.image_uri)
+
+    def _init_auth_info(self, auth, secret, image_uri) -> None:
+        if "registryuri" in auth:
+            self.registry_uri = self._get_registry_uri(auth["registryuri"],
+                                                       image_uri)
+        username = auth["username"] if "username" in auth else ""
+        password = secret[auth["password"]] if "password" in auth and auth[
+            "password"] in secret else ""
+        if username and password:
+            basic_auth_token = base64.b64decode("{}:{}".format(
+                username, password)).decode()
+            self.basic_auth_headers["Authorization"] = "{} {}".format(
+                BASIC_AUTH, basic_auth_token)
+
+    def _get_registry_uri(self, uri, image_uri) -> str:
+        ret_uri = uri.strip().rstrip("/")
+        chunks = image_uri.split("/")
+        if ret_uri.lstrip("http://") != chunks[0] and ret_uri.lstrip(
+                "http://") != chunks[0]:
+            LOGGER.info("Using default registry")
+            return self.registry_uri
+
+        if not ret_uri.startswith("http") and not ret_uri.startswith("https"):
+            ret_uri = "https://{}".format(ret_uri)
+        chunks = ret_uri.split('/')
+        api_version_str = chunks[-1]
+        if api_version_str == "v1" or api_version_str == "v2":
+            ret_uri = "/".join(chunks[:-1])
+        ret_uri = ret_uri.rstrip("/") + "/v2/"
+        return ret_uri
+
+    # Parse the challenge field, refer to: https://tools.ietf.org/html/rfc6750#section-3
+    def _parse_auth_challenge(self, challenge) -> dict:
+        if not challenge.strip().startswith(BEARER_AUTH):
+            LOGGER.info("Challenge not supported, ignore this")
+            return {}
+
+        chunks = challenge.strip().split(",")
+        challenge_dir = {}
+        for chunk in chunks:
+            pair = chunk.strip().split("=")
+            challenge_dir[pair[0]] = pair[1].strip("\"")
+        return challenge_dir
+
+    def _get_and_set_token(self, challenge):
+        if not challenge:
+            return
+        if "realm" not in challenge:
+            LOGGER.warning("realm not in challenge, use basic auth")
+            return
+        url = challenge["realm"]
+        paramters: dict = copy.deepcopy(challenge)
+        del paramters["realm"]
+        resp = requests.get(url,
+                            headers=self.basic_auth_headers,
+                            params=paramters)
+        if not resp.ok:
+            raise RuntimeError(
+                "Failed to get auth token, status code: {}".format(
+                    resp.status_code))
+        body = resp.json()
+        self.bearer_auth_headers = "{} {}".format(BEARER_AUTH, body["token"])
+        self.registry_auth_type = BEARER_AUTH
+
+    def _is_registry_v2_supportted(self) -> bool:
+        try:
+            resp = requests.head(self.registry_uri,
+                                 headers=self.basic_auth_headers,
+                                 timeout=10)
+            if resp.ok or resp.status_code == http.HTTPStatus.UNAUTHORIZED:
+                return True
+            return False
+        except (TimeoutError, ConnectionError):
+            return False
+
+    def _login_v2_registry(self) -> None:
+        if not self._is_registry_v2_supportted():
+            LOGGER.warning(
+                "Registry %s not support v2 api, ignore image check",
+                self.registry_uri)
+            return
+        resp = requests.head(self.registry_uri,
+                             headers=self.basic_auth_headers)
+        if not resp.ok:
+            LOGGER.error("Failed to login registry, resp code is %d",
+                         resp.status_code)
+            raise RuntimeError("Failed to login registry")
+        headers = resp.headers
+        if "Www-Authenticate" in headers:
+            challenge = self._parse_auth_challenge(headers["Www-Authenticate"])
+            self._get_and_set_token(challenge)
+
+    def is_docker_image_accessible(self):
+        if self.registry_auth_type == BEARER_AUTH:
+            resp = requests.head(self.registry_uri,
+                                 headers=self.bearer_auth_headers)
+        else:
+            resp = requests.head(self.registry_auth_type,
+                                 headers=self.basic_auth_headers)
+        if resp.ok:
+            LOGGER.info("image %s found in registry", self.image_uri)
+            return True
+        if resp.status_code == http.HTTPStatus.NOT_FOUND or resp.status_code == http.HTTPStatus.UNAUTHORIZED:
+            LOGGER.info(
+                "image %s not found or user unauthorized, registry is %s, resp code is %d",
+                self.image_uri, self.registry_uri, resp.status_code)
+            return False
+        LOGGER.warning("resp with code %d, ignore image check",
+                       resp.status_code)
+        raise RuntimeError("Unknown response from registry")
+
+
+def _get_docker_repository_name(image_name) -> str:
     paths = image_name.split("/")
     if len(paths) == 1:
         return "library/{}".format(paths[0])
     return image_name
 
 
-def _is_docker_image_valid(job_config):
-    prerequisites = job_config["prerequisites"]
-
-    task_role_name = os.getenv("PAI_CURRENT_TASK_ROLE_NAME")
-    task_role = job_config["taskRoles"][task_role_name]
-    docker_image_name = task_role["dockerImage"]
-
-    docker_images = list(
-        filter(lambda pre: pre["name"] == docker_image_name, prerequisites))
-    assert len(docker_images) == 1
-    image_info = docker_images[0]
-
-    if "auth" in image_info:
-        LOGGER.info("skip checking docker image with auth info")
-        return True
-
-    if not _is_docker_hub_uri(image_info["uri"]):
-        LOGGER.info("Not use docker hub as registry, ignore checking")
-        return True
-
-    arr = image_info["uri"].split(":")
-    if len(arr) == 1:
-        uri = "http://hub.docker.com/v2/repositories/{}".format(
-            _get_docker_repository_name(arr[0]))
-    elif len(arr) == 2:
-        uri = "http://hub.docker.com/v2/repositories/{}/tags/{}".format(
-            _get_docker_repository_name(arr[0]), arr[1])
-    else:
-        LOGGER.ERROR("Maybe docker uri is invalid")
-        return False
-    res = requests.get(uri)
-    if res.status_code != http.HTTPStatus.OK:
-        LOGGER.error(
-            "Failed to get docker image info from docker hub, resp: %s",
-            res.text)
-        return False
-    return True
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("job_config", help="job config yaml")
+    parser.add_argument("secret_file", help="secret file path")
     args = parser.parse_args()
 
     LOGGER.info("get job config from %s", args.job_config)
-    with open(args.job_config) as f:
-        job_config = yaml.safe_load(f)
-        if not _is_docker_image_valid(job_config):
-            sys.exit(1)
+    with open(args.job_config) as config, open(args.secret_file) as secret:
+        job_config = yaml.safe_load(config)
+        job_secret = yaml.safe_load(secret)
+
+        image_checker = ImageChecker(job_config, job_secret)
+        try:
+            if not image_checker.is_docker_image_accessible():
+                sys.exit(1)
+        except Exception:  #pylint: disable=broad-except
+            LOGGER.warning("Failed to check image", exc_info=True)
 
 
 if __name__ == "__main__":
--- src/kube-runtime/test/test_image_checker.py
+++ src/kube-runtime/test/test_image_checker.py
@@ -15,107 +15,104 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import os
-import functools
-import sys
-import unittest
+# import os
+# import functools
+# import sys
+# import unittest
 
-import yaml
+# import yaml
 
-# pylint: disable=wrong-import-position
-sys.path.append(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src"))
-sys.path.append(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src/init.d"))
-import image_checker
-from common.utils import init_logger
-# pylint: enable=wrong-import-position
+# # pylint: disable=wrong-import-position
+# sys.path.append(
+#     os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src"))
+# sys.path.append(
+#     os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src/init.d"))
+# import image_checker
+# from common.utils import init_logger
+# # pylint: enable=wrong-import-position
 
-PACKAGE_DIRECTORY_COM = os.path.dirname(os.path.abspath(__file__))
-init_logger()
+# PACKAGE_DIRECTORY_COM = os.path.dirname(os.path.abspath(__file__))
+# init_logger()
 
+# # pylint: disable=protected-access
+# def prepare_image_check(job_config_path):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(self, *args, **kwargs):
+#             os.environ["PAI_CURRENT_TASK_ROLE_NAME"] = "worker"
+#             if os.path.exists(job_config_path):
+#                 with open(job_config_path, 'r') as f:
+#                     self.config = yaml.load(f, Loader=yaml.FullLoader)
+#                 func(self, *args, **kwargs)
+#             del os.environ["PAI_CURRENT_TASK_ROLE_NAME"]
 
-# pylint: disable=protected-access
-def prepare_image_check(job_config_path):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(self, *args, **kwargs):
-            os.environ["PAI_CURRENT_TASK_ROLE_NAME"] = "worker"
-            if os.path.exists(job_config_path):
-                with open(job_config_path, 'r') as f:
-                    self.config = yaml.load(f, Loader=yaml.FullLoader)
-                func(self, *args, **kwargs)
-            del os.environ["PAI_CURRENT_TASK_ROLE_NAME"]
+#         return wrapper
 
-        return wrapper
+#     return decorator
 
-    return decorator
+# class TestImageChecker(unittest.TestCase):
+#     def setUp(self):
+#         try:
+#             os.chdir(PACKAGE_DIRECTORY_COM)
+#         except Exception:  #pylint: disable=broad-except
+#             pass
+#         self.config = {}
 
+# @prepare_image_check("docker_image_no_user_tag.yaml")
+# def test_image_without_username_tag(self):
+#     res = image_checker._is_docker_image_valid(self.config)
+#     self.assertEqual(res, True)
 
-class TestImageChecker(unittest.TestCase):
-    def setUp(self):
-        try:
-            os.chdir(PACKAGE_DIRECTORY_COM)
-        except Exception:  #pylint: disable=broad-except
-            pass
-        self.config = {}
+# @prepare_image_check("docker_image_no_tag.yaml")
+# def test_image_without_tag(self):
+#     res = image_checker._is_docker_image_valid(self.config)
+#     self.assertEqual(res, True)
 
-    @prepare_image_check("docker_image_no_user_tag.yaml")
-    def test_image_without_username_tag(self):
-        res = image_checker._is_docker_image_valid(self.config)
-        self.assertEqual(res, True)
+# @prepare_image_check("docker_image_no_exist.yaml")
+# def test_image_not_exist(self):
+#     res = image_checker._is_docker_image_valid(self.config)
+#     self.assertEqual(res, False)
 
-    @prepare_image_check("docker_image_no_tag.yaml")
-    def test_image_without_tag(self):
-        res = image_checker._is_docker_image_valid(self.config)
-        self.assertEqual(res, True)
+# @prepare_image_check("docker_image_local_registry.yaml")
+# def test_image_local(self):
+#     res = image_checker._is_docker_image_valid(self.config)
+#     self.assertEqual(res, True)
 
-    @prepare_image_check("docker_image_no_exist.yaml")
-    def test_image_not_exist(self):
-        res = image_checker._is_docker_image_valid(self.config)
-        self.assertEqual(res, False)
+# @prepare_image_check("docker_image_auth.yaml")
+# def test_image_auth(self):
+#     res = image_checker._is_docker_image_valid(self.config)
+#     self.assertEqual(res, True)
 
-    @prepare_image_check("docker_image_local_registry.yaml")
-    def test_image_local(self):
-        res = image_checker._is_docker_image_valid(self.config)
-        self.assertEqual(res, True)
+# def test_docker_hub_uri(self):
+#     valid_docker_hub_uris = [
+#         "user-name.domain/repo-0.domain:tag-0.domain", "username/repo:tag",
+#         "username/repo", "repo"
+#     ]
 
-    @prepare_image_check("docker_image_auth.yaml")
-    def test_image_auth(self):
-        res = image_checker._is_docker_image_valid(self.config)
-        self.assertEqual(res, True)
+#     for uri in valid_docker_hub_uris:
+#         self.assertTrue(image_checker._is_docker_hub_uri(uri))
 
-    def test_docker_hub_uri(self):
-        valid_docker_hub_uris = [
-            "user-name.domain/repo-0.domain:tag-0.domain", "username/repo:tag",
-            "username/repo", "repo"
-        ]
+#     invalid_docker_hub_uris = [
+#         "localhost:5000/repo", "localhost/username/repo:tag"
+#     ]
 
-        for uri in valid_docker_hub_uris:
-            self.assertTrue(image_checker._is_docker_hub_uri(uri))
+#     for uri in invalid_docker_hub_uris:
+#         self.assertFalse(image_checker._is_docker_hub_uri(uri))
 
-        invalid_docker_hub_uris = [
-            "localhost:5000/repo", "localhost/username/repo:tag"
-        ]
+# def test_get_docker_repository_name(self):
+#     official_image_names = [
+#         "ubuntu", "python:latest", "golang:1.12.6-alpine"
+#     ]
+#     third_party_image_names = [
+#         "tensorflow/tensorflow:devel-gpu",
+#         "docker pull pytorch/pytorch:1.3-cuda10.1-cudnn7-runtime"
+#     ]
+#     for name in official_image_names:
+#         self.assertEqual("library/{}".format(name),
+#                          image_checker._get_docker_repository_name(name))
+#     for name in third_party_image_names:
+#         self.assertEqual(name,
+#                          image_checker._get_docker_repository_name(name))
 
-        for uri in invalid_docker_hub_uris:
-            self.assertFalse(image_checker._is_docker_hub_uri(uri))
-
-    def test_get_docker_repository_name(self):
-        official_image_names = [
-            "ubuntu", "python:latest", "golang:1.12.6-alpine"
-        ]
-        third_party_image_names = [
-            "tensorflow/tensorflow:devel-gpu",
-            "docker pull pytorch/pytorch:1.3-cuda10.1-cudnn7-runtime"
-        ]
-        for name in official_image_names:
-            self.assertEqual("library/{}".format(name),
-                             image_checker._get_docker_repository_name(name))
-        for name in third_party_image_names:
-            self.assertEqual(name,
-                             image_checker._get_docker_repository_name(name))
-
-
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
