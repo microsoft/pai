0a04afb17374211184f6f648f5f7c25dab8af5d5
YundongYe <YundongYe@outlook.com>
2020-01-15 16:31:56 +0800

    issue fix

--- contrib/kubespray/script/openpai-generator.py
+++ contrib/kubespray/script/openpai-generator.py
@@ -159,8 +159,6 @@ def main():
     node_resource_dict = get_kubernetes_node_info_from_API()
     hived_config = hived_config_prepare(worker_dict, node_resource_dict)
 
-    print
-
     environment = {
         'master': master_list,
         'worker': worker_list,
