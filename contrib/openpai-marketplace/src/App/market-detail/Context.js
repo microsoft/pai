854961dde05ed141984e694dabf820a75d8330e1
Shuguang Wei (FA Talent) <v-shuwei@microsoft.com>
2020-01-16 17:27:17 +0800

    Delete marketplace under contrib and webportal

--- contrib/openpai-marketplace/src/App/market-detail/Context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Context = React.createContext({
-  marketItem: null,
-  api: null,
-  history: null,
-});
-
-export default Context;
--- src/webportal/src/app/marketplace/market-detail/Context.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Context = React.createContext({
-  marketItem: null,
-});
-
-export default Context;
