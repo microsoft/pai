5c7a526667ebb9eeb02cd322eb82eaabee1a8124
YundongYe <YundongYe@outlook.com>
2020-01-17 14:41:58 +0800

    Install nvidia device plugin for hived.

--- contrib/kubespray/script/service-boot.sh
+++ contrib/kubespray/script/service-boot.sh
@@ -52,6 +52,8 @@ echo "branch name: ${BRANCH_NAME}"
 git checkout ${BRANCH_NAME}
 git pull
 
+kubectl apply --overwrite=true -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta4/nvidia-device-plugin.yml || exit $?
+
 python3 /root/pai/contrib/kubespray/script/openpai-generator.py -m /quick-start-config/master.csv -w /quick-start-config/worker.csv -c /quick-start-config/config.yml -o /cluster-configuration || exit $?
 
 echo y | pip3 uninstall kubernetes==11.0.0b2
