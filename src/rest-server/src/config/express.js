71633cf44d929558c4f7666bacbce9aad9caaeea
ydye <yuye@microsoft.com>
2020-02-20 23:21:44 +0800

    issue fix

--- src/rest-server/src/config/express.js
+++ src/rest-server/src/config/express.js
@@ -29,6 +29,7 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('@pai/config');
 const logger = require('@pai/config/logger');
 const createError = require('@pai/utils/error');
+const querystring = require('querystring');
 const routers = {
   v1: require('@pai/routes/index'),
   v2: require('@pai/routes/v2/index'),
