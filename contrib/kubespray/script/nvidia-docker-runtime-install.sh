eb153e5de6a562431741ea4dc1c1cd3ff3f6606b
ydye <yuye@microsoft.com>
2020-02-26 16:51:28 +0800

    Refactor quick start based on ansible role.

--- contrib/kubespray/script/nvidia-docker-runtime-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ${HOME}/pai-deploy/pai/contrib/kubespray
-
-echo "Install nvidia docker runtime"
-ansible-playbook -i ${HOME}/pai-deploy/cluster-cfg/gpu-hosts.yml nvidia-docker.yml --become --become-user=root || exit $?
--- contrib/kubespray/script/nvidia-drivers-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ${HOME}/pai-deploy/pai/contrib/kubespray
-
-echo "Install nvidia drivers to the machine"
-ansible-playbook -i ${HOME}/pai-deploy/cluster-cfg/gpu-hosts.yml nvidia-drivers.yml --become --become-user=root || exit $?
-
-echo "Enable nvidia persistent mode"
-ansible-playbook -i ${HOME}/pai-deploy/cluster-cfg/gpu-hosts.yml nvidia-persistent-mode.yml --become --become-user=root || exit $?
