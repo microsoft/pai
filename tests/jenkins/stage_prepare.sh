5fdf7f25ae3bafee35b672e3bac14833bcccd2f6
Yifan Xiong <yifan.xiong@microsoft.com>
2020-03-05 14:59:06 +0800

    Fix Jenkins issue caused by downloading MNIST 403
    Fix Jenkins test job failed issue caused by
    upstream pytorch and torchvision.

--- tests/jenkins/stage_prepare.sh
+++ tests/jenkins/stage_prepare.sh
@@ -17,6 +17,8 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# remove unused packages especially linux headers
+sudo apt autoremove || true
 # stop all docker containers
 sudo docker stop $(sudo docker ps -q) || true
 # remove all docker containers
