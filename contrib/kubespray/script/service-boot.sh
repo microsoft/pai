a278fb5d42a2b55aebf6bb1afb53f996ff0a2e7e
YundongYe <8675883+ydye@users.noreply.github.com>
2020-03-07 14:07:39 +0800

    [quick-start] hived config generator's log update and support amd device (#4253)

--- contrib/kubespray/script/service-boot.sh
+++ contrib/kubespray/script/service-boot.sh
@@ -56,9 +56,19 @@ echo "branch name: ${OPENPAI_BRANCH_NAME}"
 git checkout ${OPENPAI_BRANCH_NAME}
 git pull
 
+echo "starting nvidia device plugin to detect nvidia gpu resource"
 kubectl apply --overwrite=true -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta4/nvidia-device-plugin.yml || exit $?
+sleep 5
+
+echo "starting AMD device plugin to detect AMD gpu resource"
+kubectl apply --overwrite=true -f https://raw.githubusercontent.com/RadeonOpenCompute/k8s-device-plugin/master/k8s-ds-amdgpu-dp.yaml || exit $?
+sleep5
 
 python3 /root/pai/contrib/kubespray/script/openpai-generator.py -m /quick-start-config/master.csv -w /quick-start-config/worker.csv -c /quick-start-config/config.yml -o /cluster-configuration || exit $?
+
+kubectl delete ds nvidia-device-plugin-daemonset -n kube-system || exit $?
+kubectl delete ds amdgpu-device-plugin-daemonset -n kube-system || exit $?
+sleep 5
 
 echo y | pip3 uninstall kubernetes==11.0.0b2
 pip3 install kubernetes
