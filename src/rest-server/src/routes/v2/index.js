ff13db6692a856b19811a54e326631ab083dc5d6
Yi Yi <yiyi@microsoft.com>
2020-04-09 02:11:47 +0800

    update

--- src/rest-server/src/routes/v2/index.js
+++ src/rest-server/src/routes/v2/index.js
@@ -1,20 +1,5 @@
-// Copyright (c) Microsoft Corporation
-// All rights reserved.
-//
-// MIT License
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 // module dependencies
 const express = require('express');
@@ -26,7 +11,7 @@ const controller = require('@pai/controllers/v2');
 const jobRouter = require('@pai/routes/v2/job');
 const virtualClusterRouter = require('@pai/routes/v2/virtual-cluster');
 const authnRouter = require('@pai/routes/authn');
-const infoController = require('@pai/controllers/v2/info')
+const infoController = require('@pai/controllers/v2/info');
 const tokenRouter = require('@pai/routes/token');
 const k8sRouter = require('@pai/routes/kubernetes');
 
