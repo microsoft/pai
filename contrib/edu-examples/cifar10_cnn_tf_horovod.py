386eebaf407360a4c1b77b36a739cf5829f39168
suiguoxin <suiguoxin@gmail.com>
2020-04-08 17:27:15 +0800

    fix horovod example issue

--- contrib/edu-examples/cifar10_cnn_tf_horovod.py
+++ contrib/edu-examples/cifar10_cnn_tf_horovod.py
@@ -1,5 +1,28 @@
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+"""
+example of cnn-cifar10 (horovod)
+"""
+
 import tensorflow as tf
 import horovod.tensorflow.keras as hvd
+import math
 
 from tensorflow.keras import datasets, layers, models
 
@@ -39,7 +62,8 @@ opt = hvd.DistributedOptimizer(opt)
 
 model.compile(optimizer=opt,
               loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=['accuracy'],
+              experimental_run_tf_function=False)
 
 callbacks = [
     # Horovod: broadcast initial variable states from rank 0 to all other processes.
@@ -58,7 +82,7 @@ verbose = 1 if hvd.rank() == 0 else 0
 
 # Horovod: adjust number of steps based on number of GPUs.
 model.fit(train_images, train_labels,
-            epochs=10, batch_size=32,callbacks=callbacks)
+            epochs=int(math.ceil(hvd.size())), batch_size=32,callbacks=callbacks)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
