4fb0fedbb908045283fd20a8270852938f23e5ae
yiyione <yiyi_one@foxmail.com>
2020-04-08 23:27:43 +0800

    add get /api/v2/info

---
+++ src/rest-server/src/controllers/v2/info.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// module dependencies
+const status = require('statuses');
+
+const get = (req, res) => {
+  return res.status(200).json({
+    name: 'PAI RESTful API',
+    version: paiConfig.version,
+    launcherType: launcherConfig.type,
+    authnMethod: authnConfig.authnMethod,
+  });
+};
+
+// module exports
+module.exports = {get};
