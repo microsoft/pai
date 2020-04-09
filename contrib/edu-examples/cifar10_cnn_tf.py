2423498eb1acfe2e42673f4b299ca4b912b04ca7
suiguoxin <suiguoxin@gmail.com>
2020-04-09 16:07:40 +0800

    test pre-trained vgg model

--- contrib/edu-examples/cifar10_cnn_tf.py
+++ contrib/edu-examples/cifar10_cnn_tf.py
@@ -26,34 +26,98 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dropout, Flatten, Dense
+from tensorflow.keras import callbacks
+from tensorflow.keras import Model
 
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1
 # train_images, test_images = train_images / 255.0, test_images / 255.0
 
-# normalize the input according to the methods used in the paper
-X_train = preprocess_input(train_images)
-X_test = preprocess_input(test_images)
-# one-hot-encode the labels for training
-y_train = to_categorical(train_labels)
-y_test = to_categorical(test_labels)
+# # normalize the input according to the methods used in the paper
+# X_train = preprocess_input(train_images)
+# X_test = preprocess_input(test_images)
+# # one-hot-encode the labels for training
+# y_train = to_categorical(train_labels)
+# y_test = to_categorical(test_labels)
 
-model = VGG16(
-    weights=None, 
-    include_top=True, 
-    classes=10,
-    input_shape=(32,32,3)
-)
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+epochs = 50
+nb_classes = 10
+
+(X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
+Y_train = to_categorical(y_train, nb_classes)
+Y_test = to_categorical(y_test, nb_classes)
+
+nb_train_samples = X_train.shape[0]
+nb_validation_samples = X_test.shape[0]
+
+# Extract the last layer from third block of vgg16 model
+last = base_model.get_layer('block3_pool').output
+# Add classification layers on top of it
+x = Flatten()(last)
+x = Dense(256, activation='relu')(x)
+x = Dropout(0.5)(x)
+pred = Dense(10, activation='sigmoid')(x)
+
+model = Model(base_model.input, pred)
+
+# set the base model's layers to non-trainable
+# uncomment next two lines if you don't want to
+# train the base model
+# for layer in base_model.layers:
+#     layer.trainable = False
+
+# compile the model with a SGD/momentum optimizer
+# and a very slow learning rate.
+model.compile(loss='binary_crossentropy',
+              optimizer=tf.optimizers.SGD(lr=1e-3, momentum=0.9),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=3, batch_size=32,
-                    validation_data=(test_images, test_labels))
+# prepare data augmentation configuration
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+train_datagen.fit(X_train)
+train_generator = train_datagen.flow(X_train, Y_train, batch_size=32)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+validation_generator = test_datagen.flow(X_test, Y_test, batch_size=32)
+
+# callback for tensorboard integration
+tb = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
+
+# fine-tune the model
+model.fit(
+    train_generator,
+    # samples_per_epoch=nb_train_samples,
+    epochs=epochs,
+    validation_data=validation_generator,
+    # nb_val_samples=nb_validation_samples,
+    callbacks=[tb])
+
+# model = VGG16(
+#     weights=None, 
+#     include_top=True, 
+#     classes=10,
+#     input_shape=(32,32,3)
+# )
+
+# model.compile(optimizer='sgd',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+
+# model.fit(train_images, train_labels, epochs=30, batch_size=256,
+#                     validation_data=(test_images, test_labels))
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
 print("The test accuracy is: ", test_acc)
