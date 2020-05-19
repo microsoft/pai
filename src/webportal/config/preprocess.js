a2acd371444a4c243d3e2765dd901f566cfc6220
Zhiyuan He <362583303@qq.com>
2020-05-19 10:23:14 +0800

    Remove previous docs (#4545)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * bulk delete
    
    * fix broken links
    
    * fix
    
    * fix
    
    * trigger
    
    * fix

--- src/webportal/config/preprocess.js
+++ src/webportal/config/preprocess.js
@@ -14,18 +14,3 @@
 // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// module dependencies
-const fse = require('fs-extra');
-const helpers = require('./helpers');
-
-// copy docs to app
-fse.copySync(
-  helpers.root('../../docs/user/job_submission.md'),
-  helpers.root('src/app/job/job-docs/job-docs.md'),
-);
-
-fse.copySync(
-  helpers.root('../../docs/user/imgs'),
-  helpers.root('src/app/job/job-docs/imgs'),
-);
