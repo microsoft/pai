3acdb425d9b35efbe8f891697d4a5b25aa60c25b
hzy46 <362583303@qq.com>
2020-05-26 17:22:37 +0800

    fix

--- contrib/kubespray/requirement.sh
+++ contrib/kubespray/requirement.sh
@@ -35,17 +35,8 @@ then
   echo "Pass: Cluster meets the requirements"
 else
   echo "Failed: There are unmet requirements in your cluster, the installation will be very likely to fail."
-  echo ""
-  echo "Please press ENTER to stop the script, check the log, and modify the cluster setting to meet the requirements."
-  echo "If you are very sure about the configuration, and still want to continue, you can type in \"continue\" to force the script to proceed."
   rm -rf ${HOME}/pai-pre-check/
-  read user_input
-  if [ "${user_input}"x = "continue"x ]; then
-    exit 0
-  else
-    exit $ret_code_check
-  fi
+  exit $ret_code_check
 fi
 
 rm -rf ${HOME}/pai-pre-check/
-
