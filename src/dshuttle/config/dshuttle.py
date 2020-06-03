631b485451f26f008092bbe3a58b105aa223351f
Binyang Li <binyli@microsoft.com>
2020-06-03 15:12:30 +0800

    update

--- src/dshuttle/config/dshuttle.py
+++ src/dshuttle/config/dshuttle.py
@@ -17,27 +17,23 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import copy
 import logging
 
 
 class Dshuttle(object):
     def __init__(self, cluster_conf, service_conf, default_service_conf):
         self.cluster_conf = cluster_conf
-        self.service_conf = dict(default_service_conf, **service_conf)
-        self.logger = logging.getLogger(__name__)
+        self.service_conf = service_conf
+        self.default_service_conf = default_service_conf
 
     def validation_pre(self):
         return True, None
 
     def run(self):
-        service_object_model = dict()
-        for k in [
-            'worker_rpc_port', 'worker_web_port', 'job_worker_rpc_port',
-            'job_worker_data_port', 'job_worker_web_port', 'master_rpc_port',
-            'master_web_port', 'master_job_rpc_port', 'master_job_web_port'
-        ]:
-            service_object_model[k] = self.service_conf[k]
-        return service_object_model
+        result = copy.deepcopy(self.default_service_conf)
+        result.update(self.service_conf)
+        return result
 
     def validation_post(self, conf):
         return True, None
