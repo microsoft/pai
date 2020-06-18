9632cc4038cc1d5bfbbb8f309adf61b6fa795c39
Yi Yi <yiyi@microsoft.com>
2020-06-18 17:19:03 +0800

    update

--- tests/jenkins/test_rest_server_js_sdk.sh
+++ tests/jenkins/test_rest_server_js_sdk.sh
@@ -13,7 +13,6 @@ case ${cluster_type} in
   "k8s")
     # install openpai-js-sdk
     sudo apt update
-    sudo apt -y upgrade
     sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
