13facb05170186a7aa5f7f874cd37734e746896f
YundongYe <YundongYe@outlook.com>
2020-06-28 13:52:47 +0800

    issue fix

--- contrib/api-stress-test/roles/virtual-kubelet/build/templates/build.sh
+++ contrib/api-stress-test/roles/virtual-kubelet/build/templates/build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run golang:1.14 -v {{{{ base_dir_repo }}}}:/repo -v {{ playbook_dir }}/.output:/output "cd /repo && make build && cp bin/virtual-kubelet /output"
+docker run golang:1.14 -v {{ base_dir_repo }}:/repo -v {{ playbook_dir }}/.output:/output "cd /repo && make build && cp bin/virtual-kubelet /output"
