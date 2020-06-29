b81e336743e4566a267610b44ba69af6d8b15d7e
Zhiyuan He <zhiyuhe@microsoft.com>
2020-06-29 16:58:53 +0800

    fix

--- src/database-controller/src/index.js
+++
@@ -1,9 +0,0 @@
-var { openpaidbsdk } = require('openpaidbsdk');
-
-
-function main() {
-  console.log('ok', openpaidbsdk, new Date());
-  setTimeout(main, 1000);
-}
-
-main()
---
+++ src/database-controller/src/poller_entry.js
@@ -0,0 +1,8 @@
+var DatabaseModel = require('openpaidbsdk')
+
+function main () {
+  console.log('ok', new Date())
+  setTimeout(main, 1000)
+}
+
+main()
