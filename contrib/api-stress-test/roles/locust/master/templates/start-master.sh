f3319ca127c64b88970a572bbdb562f5bbdb4c91
YundongYe <YundongYe@outlook.com>
2020-07-02 19:05:11 +0800

    Handle node label with parallel

--- contrib/api-stress-test/roles/locust/master/templates/start-master.sh
+++ contrib/api-stress-test/roles/locust/master/templates/start-master.sh
@@ -5,4 +5,4 @@ KUBECONFIG={{kube_config_path}}
 
 kubectl label nodes {{ hostvars[groups['kube-master'][0]].inventory_hostname }} locust-role=master
 
-kubectl create -f {{ locust_base_dir }}/master.yml
+kubectl apply -f {{ locust_base_dir }}/master.yml
