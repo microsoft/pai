7d2d17c4795c9092b1ce658bf57b34bec8193d2d
YundongYe <YundongYe@outlook.com>
2020-07-02 18:59:41 +0800

    Handle node label with parallel

--- contrib/api-stress-test/roles/locust/config/templates/start.sh
+++ contrib/api-stress-test/roles/locust/config/templates/start.sh
@@ -4,4 +4,4 @@ KUBECONFIG={{kube_config_path}}
 
 kubectl create configmap locust-script-configuration --from-file={{ locust_base_dir }}/configmap/ --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
 
-kubectl create -f {{ locust_base_dir }}/locust-sa.yml
+kubectl apply -f {{ locust_base_dir }}/locust-sa.yml
