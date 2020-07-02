9ba2ae69c9b901cd24ddcbb5956d8b5c8ce3eacb
YundongYe <YundongYe@outlook.com>
2020-07-02 17:41:45 +0800

    Handle node label with parallel

--- contrib/api-stress-test/roles/locust/worker/templates/start-worker.sh
+++ contrib/api-stress-test/roles/locust/worker/templates/start-worker.sh
@@ -4,6 +4,7 @@ KUBECONFIG={{kube_config_path}}
 
 {% for host in groups['kube-worker'] %}
 kubectl label nodes {{ hostvars[host].inventory_hostname }} locust-role=worker
+kubectl label nodes {{ hostvars[host].inventory_hostname }} no-jobexporter=true
 {% endfor %}
 
 kubectl create -f {{ locust_base_dir }}/worker.yml
