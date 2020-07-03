0de80f4fbc476ddecf7ffb04aeaa10292081aab2
YundongYe <8675883+ydye@users.noreply.github.com>
2020-07-03 16:10:22 +0800

    [contrib] locust stress test environment setup based on ansible (#4665)

---
+++ contrib/api-stress-test/roles/locust/config/templates/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+KUBECONFIG={{kube_config_path}}
+
+kubectl create configmap locust-script-configuration --from-file={{ locust_base_dir }}/configmap/ --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
+
+kubectl apply -f {{ locust_base_dir }}/locust-sa.yml
---
+++ contrib/api-stress-test/roles/locust/worker/templates/start-worker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+KUBECONFIG={{kube_config_path}}
+
+{% for host in groups['kube-worker'] %}
+kubectl label nodes {{ hostvars[host].inventory_hostname }} locust-role=worker
+kubectl label nodes {{ hostvars[host].inventory_hostname }} no-jobexporter=true
+{% endfor %}
+
+kubectl apply -f {{ locust_base_dir }}/worker.yml
