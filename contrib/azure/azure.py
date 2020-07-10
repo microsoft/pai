516d7b59952845e3ace89e67890b2bc705202e1d
ydye <yuye@microsoft.com>
2020-07-11 00:02:00 +0800

    az quick start config.

---
+++ contrib/azure/azure.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import logging
+import logging.config
+import argparse
+
+logger = logging.getLogger(__name__)
+
+TEMPORARY_DIR_NAME = ".azure_quick_start"
+
+
+def main():
+    None`
+
+if __name__ == "__main__":
+    main()
+
