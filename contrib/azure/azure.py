c1d7864a6425603594ba1382d6606f4c888eaa0f
ydye <yuye@microsoft.com>
2020-07-11 20:49:16 +0800

    aks-engine.sh.j2

--- contrib/azure/azure.py
+++ contrib/azure/azure.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import logging
+import os
+import datetime
+import yaml
 import logging.config
 import argparse
 
@@ -8,9 +10,33 @@ logger = logging.getLogger(__name__)
 
 TEMPORARY_DIR_NAME = ".azure_quick_start"
 
+def load_yaml_config(config_path):
+    with open(config_path, "r") as f:
+        config_data = yaml.load(f, yaml.SafeLoader)
+    return config_data
+
+def create_folder_if_not_exist(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def main():
-    None`
+    parser = argparse.ArgumentParser(description="OpenPAI at Azure quick start")
+
+    starttime = datetime.datetime.now()
+    logger.info("Start to deploy azure {0}".format(starttime))
+
+    parser.add_argument(
+        '-c', '--config',
+        type=bytes,
+        required=True,
+        help='The path of your configuration path.'
+    )
+
+    args = parser.parse_args()
+    config_path = args.config
+
+
+
 
 if __name__ == "__main__":
     main()
