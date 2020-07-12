c55b67dae97c5bb6710c7f4d0aab62a1964b6a41
ydye <yuye@microsoft.com>
2020-07-12 23:00:27 +0800

    update k8s.json.j2

--- contrib/azure/azure.py
+++ contrib/azure/azure.py
@@ -5,6 +5,8 @@ import datetime
 import yaml
 import logging.config
 import argparse
+import subprocess
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +20,13 @@ def load_yaml_config(config_path):
 def create_folder_if_not_exist(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def execute_shell(shell_cmd, error_msg):
+    try:
+        subprocess.check_call( shell_cmd, shell=True )
+    except subprocess.CalledProcessError:
+        logger.error(error_msg)
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="OpenPAI at Azure quick start")
@@ -34,6 +43,8 @@ def main():
 
     args = parser.parse_args()
     config_path = args.config
+
+    aks_engine_cfg = load_yaml_config(config_path)
 
 
 
