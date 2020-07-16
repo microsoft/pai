7cf2467dd90d81dc1b52a1bdb434547af0f9ad57
suiguoxin <suiguoxin@gmail.com>
2020-07-16 16:19:42 +0800

    create framework_history and pods table

--- src/postgresql/src/init_table.sql
+++ src/postgresql/src/init_table.sql
@@ -1,8 +1,21 @@
 /* This init script will be run every time when the database container is started. */
 
-CREATE TABLE IF NOT EXISTS fc_objectsnapshots (
-   tag Text,
-   time Timestamptz,
-   record JsonB
+CREATE TABLE IF NOT EXISTS framework_history (
+   insertedAt Timestamptz,
+   uid SERIAL,
+   frameworkName VARCHAR(64), 
+   attemptIndex INTEGER,
+   historyType VARCHAR(16),
+   snapshot TEXT
+);
+CREATE TABLE IF NOT EXISTS pods (
+   insertedAt Timestamptz,
+   updatedAt Timestamptz, uid VARCHAR(36),
+   frameworkName VARCHAR(64),
+   attemptIndex INTEGER,
+   taskroleName VARCHAR(256),
+   taskroleIndex INTEGER,
+   taskAttemptIndex INTEGER,
+   snapshot TEXT
 );
 CREATE INDEX IF NOT EXISTS uidindex ON fc_objectsnapshots USING gin ((record -> 'objectSnapshot' -> 'metadata' -> 'uid'));
