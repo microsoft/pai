ce8b3ce30832b791ce9894c6875cdaf230b33c41
zhangguowei <285009003@qq.com>
2020-08-26 17:07:35 +0800

    Add distributed training examples of PyTorch

--- examples/Distributed-example/cifar10-singal-mul-DDP-nccl-gloo.py
+++ examples/Distributed-example/cifar10-singal-mul-DDP-nccl-gloo.py
@@ -11,6 +11,7 @@ import torch.nn.functional as F
 import torch.distributed as dist
 from apex.parallel import DistributedDataParallel as DDP
 from apex import amp
+
 def main():
     print('run main')
     parser = argparse.ArgumentParser()
