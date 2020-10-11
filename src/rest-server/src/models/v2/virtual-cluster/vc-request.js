8ae0c7b48c70f7550eb3643f9fc830ba4b18b4ec
Yi Yi <yiyi@microsoft.com>
2020-10-11 17:17:44 +0800

    update vc-request model

--- src/rest-server/src/models/v2/virtual-cluster/vc-request.js
+++ src/rest-server/src/models/v2/virtual-cluster/vc-request.js
@@ -4,23 +4,105 @@
 // module dependencies
 const axios = require('axios');
 const yaml = require('js-yaml');
+const uuid = require('uuid');
 const createError = require('@pai/utils/error');
 const { resourceUnits } = require('@pai/config/vc');
 const { enabledHived, hivedWebserviceUri } = require('@pai/config/launcher');
 const kubernetes = require('@pai/models/kubernetes/kubernetes');
-const k8s = require('@pai/utils/k8sUtils');
+const k8sSecret = require('@pai/models/kubernetes/k8s-secret');
+const k8sModel = require('@pai/models/kubernetes/kubernetes');
 
-const listRequest = async (vcName) => {
-  const vcInfos = await getVcList();
-  if (!Object.prototype.hasOwnProperty.call(vcInfos, vcName)) {
+const namespace = process.env.PAI_VC_REQUEST_NAMESPACE || 'pai-vc-request';
+const requestState = {
+  NEW: 'new',
+  APPROVED: 'approved',
+  REJECTED: 'rejected',
+}
+
+const createRequest = async (vcName, username, message) => {
+  try {
+    const item = await k8sSecret.get(namespace, vcName, { encode: 'hex' });
+    const id = uuid();
+    const requestObject = {
+      id,
+      username,
+      message,
+      state: requestState.NEW,
+    };
+
+    if (item === null) {
+      await k8sSecret.create(
+        namespace,
+        vcName,
+        {
+          [id]: requestObject,
+        },
+        { encode: 'hex' },
+      );
+    } else {
+      item[id] = requestObject;
+      await k8sSecret.replace(namespace, vcName, result, { encode: 'hex' });
+    }
+  } catch (err) {
     throw createError(
       'Not Found',
       'NoVirtualClusterError',
       `Vc ${vcName} not found`,
     );
   }
-  return vcInfos[vcName];
+}
+
+const listRequest = async (vcName) => {
+  try {
+    const item = await k8sSecret.get(namespace, vcName, { encode: 'hex' });
+    if (item === null) {
+      return [];
+    }
+    return Object.values(item);
+  } catch (err) {
+    throw createError(
+      'Not Found',
+      'NoVirtualClusterError',
+      `Vc ${vcName} not found`,
+    );
+  }
 };
+
+const updateRequest = async (vcName, id, state) => {
+  try {
+    const item = await k8sSecret.get(namespace, vcName, { encode: 'hex' });
+    const requestObject = item[id];
+
+    if (item !== null) {
+      requestObject.state = state;
+      item[id] = requestObject;
+      await k8sSecret.replace(namespace, vcName, result, { encode: 'hex' });
+    }
+  } catch (err) {
+    throw createError(
+      'Not Found',
+      'NoVirtualClusterError',
+      `Vc ${vcName} not found`,
+    );
+  }
+}
+
+const deleteRequest = async (vcName, id) => {
+  try {
+    const item = await k8sSecret.get(namespace, vcName, { encode: 'hex' });
+
+    if (item !== null && item[id]) {
+      delete item[id];
+      await k8sSecret.replace(namespace, vcName, result, { encode: 'hex' });
+    }
+  } catch (err) {
+    throw createError(
+      'Not Found',
+      'NoVirtualClusterError',
+      `Vc ${vcName} not found`,
+    );
+  }
+}
 
 // module exports
 module.exports = {
