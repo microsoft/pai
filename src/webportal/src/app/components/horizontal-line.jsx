ed28b22d5664cb0d7ad7bedcd56ffba23b6037bb
debuggy <mintao@microsoft.com>
2020-10-19 15:16:57 +0800

    fix lint

--- src/webportal/src/app/components/horizontal-line.jsx
+++ src/webportal/src/app/components/horizontal-line.jsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-import { getTheme } from '@uifabric/styling';
 import styled from 'styled-components';
 
 const HorizontalLine = styled.hr`
