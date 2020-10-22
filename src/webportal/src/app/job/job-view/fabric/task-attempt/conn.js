0d25e8b77d132d3bda3e87f0b1cb5041b151d223
Mingliang Tao <mintao@microsoft.com>
2020-10-22 11:20:04 +0800

    Add task attempt info in job detail page (#4966)

--- src/webportal/src/app/job/job-view/fabric/task-attempt/conn.js
+++ src/webportal/src/app/job/job-view/fabric/task-attempt/conn.js
@@ -1,25 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { PAIV2 } from '@microsoft/openpai-js-sdk';
-import { isNil, get } from 'lodash';
-
 import { clearToken } from '../../../../user/user-logout/user-logout.component';
-import { checkToken } from '../../../../user/user-auth/user-auth.component';
 import config from '../../../../config/webportal.config';
-
-const params = new URLSearchParams(window.location.search);
-const userName = params.get('username');
-const jobName = params.get('jobName');
 const absoluteUrlRegExp = /^[a-z][a-z\d+.-]*:/;
-const token = cookies.get('token');
 
-const client = new PAIV2.OpenPAIClient({
-  rest_server_uri: new URL(config.restServerUri, window.location.href),
-  username: cookies.get('user'),
-  token: token,
-  https: window.location.protocol === 'https:',
-});
+const token = cookies.get('token');
 
 export class NotFoundError extends Error {
   constructor(msg) {
@@ -43,23 +29,13 @@ const wrapper = async func => {
   }
 };
 
-export async function fetchJobInfo(attemptIndex) {
-  return wrapper(async () => {
-    const restServerUri = new URL(config.restServerUri, window.location.href);
-    const url = isNil(attemptIndex)
-      ? `${restServerUri}/api/v2/jobs/${userName}~${jobName}`
-      : `${restServerUri}/api/v2/jobs/${userName}~${jobName}/attempts/${attemptIndex}`;
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const result = await res.json();
-    return result;
-  });
-}
-
-export async function fetchTaskStatus(attemptIndex, taskRoleName, taskIndex) {
+export async function fetchTaskStatus(
+  userName,
+  jobName,
+  attemptIndex,
+  taskRoleName,
+  taskIndex,
+) {
   return wrapper(async () => {
     const restServerUri = new URL(config.restServerUri, window.location.href);
     const url = `${restServerUri}/api/v2/jobs/${userName}~${jobName}/attempts/${attemptIndex}/taskRoles/${taskRoleName}/taskIndex/${taskIndex}/attempts`;
@@ -71,82 +47,6 @@ export async function fetchTaskStatus(attemptIndex, taskRoleName, taskIndex) {
     const result = await res.json();
     return result;
   });
-}
-
-export async function fetchRawJobConfig() {
-  return wrapper(() => client.job.getJobConfig(userName, jobName));
-}
-
-export async function fetchJobConfig() {
-  return wrapper(() => client.job.getJobConfig(userName, jobName));
-}
-
-export async function fetchSshInfo() {
-  const url = userName
-    ? `${config.restServerUri}/api/v2/jobs/${userName}~${jobName}/ssh`
-    : `${config.restServerUri}/api/v2/jobs/${jobName}/ssh`;
-  const token = checkToken();
-  const res = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const json = await res.json();
-  if (res.ok) {
-    return json;
-  } else {
-    if (json.code === 'NoJobSshInfoError') {
-      throw new NotFoundError(json.message);
-    } else {
-      throw new Error(json.message);
-    }
-  }
-}
-
-export function getTensorBoardUrl(jobInfo, rawJobConfig) {
-  let port = null;
-  let ip = null;
-  const tensorBoardPlugin = get(
-    rawJobConfig,
-    ['extras', 'com.microsoft.pai.runtimeplugin'],
-    [],
-  ).find(plugin => plugin.plugin === 'tensorboard');
-  if (isNil(tensorBoardPlugin)) {
-    return null;
-  }
-
-  const taskRoles = jobInfo.taskRoles;
-  const firstTaskRoleName = Object.keys(taskRoles)[0];
-  const taskStatuses = taskRoles[firstTaskRoleName].taskStatuses[0];
-  if (taskStatuses.taskState === 'RUNNING') {
-    port = get(tensorBoardPlugin, 'parameters.port');
-    ip = taskStatuses.containerIp;
-  }
-
-  if (isNil(port) || isNil(ip)) {
-    return null;
-  }
-  return `http://${ip}:${port}`;
-}
-
-export function getJobMetricsUrl(jobInfo) {
-  const from = jobInfo.jobStatus.createdTime;
-  let to = '';
-  const { state } = jobInfo.jobStatus;
-  if (state === 'RUNNING') {
-    to = Date.now();
-  } else {
-    to = jobInfo.jobStatus.completedTime;
-  }
-  return `${config.grafanaUri}/dashboard/db/joblevelmetrics?var-job=${
-    userName ? `${userName}~${jobName}` : jobName
-  }&from=${from}&to=${to}`;
-}
-
-export async function stopJob() {
-  return wrapper(() =>
-    client.job.updateJobExecutionType(userName, jobName, 'STOP'),
-  );
 }
 
 export async function getContainerLog(logUrl) {
