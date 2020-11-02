1f784c5647bd797e9d0d32bace37309197322895
hzy46 <362583303@qq.com>
2020-11-02 13:29:22 +0800

    fix

--- src/webportal/src/app/job/job-view/fabric/job-transfer/conn.js
+++ src/webportal/src/app/job/job-view/fabric/job-transfer/conn.js
@@ -47,3 +47,54 @@ export async function fetchJobConfig(userName, jobName) {
     return result;
   });
 }
+
+const boundedClusterWrapper = async (alias, func) => {
+  try {
+    return await func();
+  } catch (err) {
+      const errorMessage = _.pick(err, 'data.message', _.pick(err, 'message'))
+      throw new Error(`There is an error during an api call of the bounded cluster ${alias}. Details: ${errorMessage}"`);
+  }
+}
+
+
+// confirm the VC is available to the user
+async function confirmVC(clusterConfig, jobConfig) {
+  const vcName = _.pick(jobConfig, 'defaults.virtualCluster');
+  // get all available VCs
+  await boundedClusterWrapper(clusterConfig.alias, async () => {
+    const url = new URL(`/api/v2/virtual-clusters`, clusterConfig.uri)
+
+  });
+}
+
+// confirm the sku is available
+async function confirmSKU(clusterConfig, jobConfig) {
+
+}
+
+// confirm the job name is not duplicate
+async function confirmJobName(clusterConfig, jobConfig) {
+
+}
+
+
+async function addTagToJob(userName, jobName, tagName) {
+  return wrapper(async () => {
+    const restServerUri = new URL(config.restServerUri, window.location.href);
+    const url = `${restServerUri}/api/v2/jobs/${userName}~${jobName}/tag`
+    const res = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await res.json();
+    return result;
+  });
+}
+
+async function transfer(userName, jobName, clusterConfig, jobConfig) {
+
+
+}
