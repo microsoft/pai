49c8b0f02604200c5406f97e89aa3a77e87bed39
Guoxin <suiguoxin@gmail.com>
2020-11-12 13:15:35 +0800

    deployment process refine (#5077)
    - move requirement check before configuration setting;
    - move quick-start-config folder setting to quick-start-services.sh; 
    - fix installation document branch checkout issue; change suggested version to v1.3.y;
    - refine uninstallation doc
    - fix git clone issue: clone to an existing folder may raise error, so the code pai/kubespray may be unsuccessfully cloned.

--- contrib/kubespray/script/configuration-services.sh
+++ contrib/kubespray/script/configuration-services.sh
@@ -18,13 +18,6 @@ while getopts "w:m:c:" opt; do
   esac
 done
 
-cd ${HOME}/pai-deploy/pai/contrib/kubespray
-mkdir -p ${HOME}/pai-deploy/cluster-cfg
-python3 ${HOME}/pai-deploy/pai/contrib/kubespray/script/k8s-generator.py -m ${MASTER_LIST} -w ${WORKER_LIST} -c ${CLUSTER_CONFIG} -o ${HOME}/pai-deploy/cluster-cfg || exit $?
-
-cp ${HOME}/pai-deploy/cluster-cfg/openpai.yml ${HOME}/pai-deploy/kubespray/inventory/pai/
-cp ${HOME}/pai-deploy/cluster-cfg/hosts.yml ${HOME}/pai-deploy/kubespray/inventory/pai/
-
 mkdir -p ${HOME}/pai-deploy/quick-start-config/
 cp ${WORKER_LIST} ${HOME}/pai-deploy/quick-start-config/worker.csv
 cp ${MASTER_LIST} ${HOME}/pai-deploy/quick-start-config/master.csv
