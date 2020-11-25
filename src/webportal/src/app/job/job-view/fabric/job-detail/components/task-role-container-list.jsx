a17388308021ee310e21e02a43347eb8e6fd9e95
AmberMsy <46340789+AmberMsy@users.noreply.github.com>
2020-11-25 16:11:34 +0800

    Update task-role-container-list.jsx

--- src/webportal/src/app/job/job-view/fabric/job-detail/components/task-role-container-list.jsx
+++ src/webportal/src/app/job/job-view/fabric/job-detail/components/task-role-container-list.jsx
@@ -232,8 +232,8 @@ export default class TaskRoleContainerList extends React.Component {
       monacoProps: null,
       monacoTitle: '',
       monacoFooterButton: null,
-      fullLogUrls: null,
-      tailLogUrls: null,
+      fullLogUrls: {},
+      tailLogUrls: {},
     });
   }
 
@@ -532,7 +532,7 @@ export default class TaskRoleContainerList extends React.Component {
           <Stack gap='m'>
             <Text variant='xLarge'>All Logs:</Text>
             <LogDialogContent
-              urlLists={!isNil(fullLogUrls) ? fullLogUrls.locations : null}
+              urlLists={!isNil(fullLogUrls) ? fullLogUrls.locations : []}
             />
           </Stack>
           <DialogFooter>
