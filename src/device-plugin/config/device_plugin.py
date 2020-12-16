27ddacabb21c7b8c3a49afdd470138dbf0eb14b1
Zhiyuan He <362583303@qq.com>
2020-12-16 11:06:10 +0800

    [Multiple Hardwares] [Device Plugin] Read computing devices from layout.yaml (#5168)
    * fix
    
    * fix
    
    * fix

--- src/device-plugin/config/device_plugin.py
+++ src/device-plugin/config/device_plugin.py
@@ -22,12 +22,31 @@ class DevicePlugin:
         self.service_conf = service_conf
         self.default_service_conf = default_service_conf
 
+    # This function is used to find the all valid computing device types from `layout.yaml`
+    def find_computing_device_types(self):
+      computing_device_types = set()
+      machine_sku_to_device_type = {
+          sku_name: sku_attrs['computing-device']['type']
+          for sku_name, sku_attrs in self.cluster_conf['machine-sku'].items()
+          if 'computing-device' in sku_attrs
+      }
+      workers = list(filter(lambda elem: 'pai-worker' in elem and elem["pai-worker"] == 'true', self.cluster_conf['machine-list']))
+      for worker in workers:
+        if worker['machine-type'] in machine_sku_to_device_type:
+          computing_device_type = machine_sku_to_device_type[worker['machine-type']]
+          computing_device_types.add(computing_device_type)
+      return list(computing_device_types)
+
     def validation_pre(self):
         if 'devices' not in self.service_conf:
             self.service_conf['devices'] = self.default_service_conf['devices']
         return True, None
 
     def run(self):
+        # add computing device from `layout.yaml`
+        for computing_device in self.find_computing_device_types():
+            if computing_device not in self.service_conf['devices']:
+                self.service_conf['devices'].append(computing_device)
         return self.service_conf
 
     def validation_post(self, conf):
