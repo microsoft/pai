99d7fc8e19651b7102ba33d60e526958c4f3f589
Guoxin <suiguoxin@gmail.com>
2020-12-24 16:25:54 +0800

    copy config/layout files to pai-deploy folder after the generation of k8s & service configuration files (#5200)

--- contrib/kubespray/quick-start-service.sh
+++ contrib/kubespray/quick-start-service.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # assume pwd is pai/contrib/kubespray
+LAYOUT="$PWD/config/layout.yaml"
 CLUSTER_CONFIG="$PWD/config/config.yaml"
 
+echo "layout config file path: ${LAYOUT}"
 echo "cluster config file path: ${CLUSTER_CONFIG}"
 
 echo "Generating services configurations..."
-sudo python3 script/openpai_generator.py -l ${HOME}/pai-deploy/cluster-cfg/layout.yaml -c ${HOME}/pai-deploy/cluster-cfg/config.yaml -o ${HOME}/pai-deploy/cluster-cfg
+sudo python3 script/openpai_generator.py -l ${LAYOUT} -c ${CLUSTER_CONFIG} -o ${HOME}/pai-deploy/cluster-cfg
+
+echo "Saving layout.yaml & config.yaml to ${HOME}/pai-deploy/cluster-cfg"
+cp ${LAYOUT} ${HOME}/pai-deploy/cluster-cfg
+cp ${CLUSTER_CONFIG} ${HOME}/pai-deploy/cluster-cfg
 
 /bin/bash script/service-boot.sh -c ${CLUSTER_CONFIG}
