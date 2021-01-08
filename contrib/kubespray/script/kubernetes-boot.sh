42bcfb985d0baf05313190a5ac8a237a35133d73
Guoxin <suiguoxin@gmail.com>
2021-01-08 12:20:55 +0800

    remove pai-deploy/pai (#5232)

--- contrib/kubespray/script/kubernetes-boot.sh
+++ contrib/kubespray/script/kubernetes-boot.sh
@@ -8,4 +8,4 @@ sudo mkdir -p ${HOME}/pai-deploy/kube || exit $?
 sudo cp -rf ${HOME}/pai-deploy/kubespray/inventory/pai/artifacts/admin.conf ${HOME}/pai-deploy/kube/config || exit $?
 
 echo "You can run the following commands to setup kubectl on you local host:"
-echo "ansible-playbook -i ${HOME}/pai-deploy/kubespray/inventory/pai/hosts.yml ${HOME}/pai-deploy/pai/contrib/kubespray/set-kubectl.yml --ask-become-pass"
+echo "ansible-playbook -i ${HOME}/pai-deploy/kubespray/inventory/pai/hosts.yml set-kubectl.yml --ask-become-pass"
