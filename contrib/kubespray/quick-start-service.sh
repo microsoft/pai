6c5b54d64fdf81058bd0df988002442b34201082
suiguoxin <suiguoxin@gmail.com>
2021-01-08 16:33:30 +0800

    fix kubeconfig issue

--- contrib/kubespray/quick-start-service.sh
+++ contrib/kubespray/quick-start-service.sh
@@ -8,9 +8,6 @@ CLUSTER_CONFIG="$PWD/config/config.yaml"
 echo "layout config file path: ${LAYOUT}"
 echo "cluster config file path: ${CLUSTER_CONFIG}"
 
-echo "Generating services configurations..."
-sudo python3 script/openpai_generator.py -l ${LAYOUT} -c ${CLUSTER_CONFIG} -o ${HOME}/pai-deploy/cluster-cfg
-
 echo "Saving layout.yaml & config.yaml to ${HOME}/pai-deploy/cluster-cfg"
 cp ${LAYOUT} ${HOME}/pai-deploy/cluster-cfg
 cp ${CLUSTER_CONFIG} ${HOME}/pai-deploy/cluster-cfg
