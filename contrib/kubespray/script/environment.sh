236bc597e871afca0fe8f56a32e51a5b5baa6044
suiguoxin <suiguoxin@gmail.com>
2021-01-12 17:20:05 +0800

    change ansible stdout callback plugin

--- contrib/kubespray/script/environment.sh
+++ contrib/kubespray/script/environment.sh
@@ -19,6 +19,9 @@ echo "Clone kubespray source code from github to ${HOME}/pai-deploy"
 sudo rm -rf ${HOME}/pai-deploy/kubespray
 git clone -b release-2.11 https://github.com/kubernetes-sigs/kubespray.git ${HOME}/pai-deploy/kubespray
 
+echo "Copy customized ansible config to kubespray folder..."
+sudo cp -f ./ansible.cfg ${HOME}/pai-deploy/kubespray
+
 echo "Copy inventory folder, and save it "
 cp -rfp ${HOME}/pai-deploy/kubespray/inventory/sample ${HOME}/pai-deploy/kubespray/inventory/pai
 
