784a373d74a66118967ae4e529388a604cdc05c6
Xiang Long <mindsculptor@yeah.net>
2021-01-19 17:59:18 +0800

    Fix stop.sh file not find error and docker-cache-service namespace error

--- src/docker-cache/deploy/stop.sh
+++ src/docker-cache/deploy/stop.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-kubectl delete --ignore-not-found --now -f docker-cache-service.yaml
-kubectl delete --ignore-not-found --now -f docker-cache.yaml
-kubectl delete --ignore-not-found --now -f docker-cache-config.yaml
-kubectl delete --ignore-not-found --now -f docker-cache-secret.yaml
+kubectl delete --ignore-not-found --now service docker-registry-cache-service
+kubectl delete --ignore-not-found --now pod docker-registry-cache
+kubectl delete --ignore-not-found --now configmap registry-config
+kubectl delete --ignore-not-found --now secret registry-htpasswd
