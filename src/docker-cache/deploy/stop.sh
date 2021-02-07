615791004bab4028a80cc3026ce79946b224a4d7
Xiang Long <mindsculptor@yeah.net>
2021-02-07 13:39:35 +0800

    Fix deployment delete error

--- src/docker-cache/deploy/stop.sh
+++ src/docker-cache/deploy/stop.sh
@@ -4,6 +4,6 @@
 # Licensed under the MIT License.
 
 kubectl delete --ignore-not-found --now service docker-registry-cache-service
-kubectl delete --ignore-not-found --now pod docker-registry-cache
+kubectl delete --ignore-not-found --now deployment.apps/docker-registry-cache
 kubectl delete --ignore-not-found --now configmap registry-config
 kubectl delete --ignore-not-found --now secret registry-htpasswd
