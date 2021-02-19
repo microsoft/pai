9d9f8785481f6045afcd9c45df530c7fd9e3828a
Xiang Long <mindsculptor@yeah.net>
2021-02-19 17:08:50 +0800

    Add parameter to docker-cache role

--- contrib/kubespray/roles/docker-cache/install/files/add_docker_cache_config.py
+++ contrib/kubespray/roles/docker-cache/install/files/add_docker_cache_config.py
@@ -1,31 +1,46 @@
 from pathlib import Path
+import argparse
 import json
 
 
-folder_path = Path("/etc/docker")
-target_path = Path("/etc/docker/daemon.json")
-backup_path = Path("/etc/docker/daemon.json.bk")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", default="localhost", help="host addr of docker cache registry")
+    return parser.parse_args()
 
-folder_path.mkdir(parents=True, exist_ok=True)
-target_path.touch(mode=0o666)
-backup_path.touch(mode=0o666)
 
-with open(str(target_path)) as f:
-    current_config = json.load(f);
+def main():
+    args = parse_args()
 
-with open(str(backup_path), 'w') as f:
-    json.dump(current_config, f)
+    folder_path = Path("/etc/docker")
+    target_path = Path("/etc/docker/daemon.json")
+    backup_path = Path("/etc/docker/daemon.json.bk")
 
-if "registry-mirrors" in current_config:
-    current_config["registry-mirrors"].append("http://localhost:30500")
-else:
-    current_config["registry-mirrors"] = ["http://localhost:30500"]
+    folder_path.mkdir(parents=True, exist_ok=True)
+    target_path.touch(mode=0o666)
+    backup_path.touch(mode=0o666)
 
-if "insecure-registries" in current_config:
-    current_config["insecure-registries"].append("http://localhost:30500")
-else:
-    current_config["insecure-registries"] = ["http://localhost:30500"]
+    with open(str(target_path)) as f:
+        current_config = json.load(f);
 
-with open(str(target_path), 'w') as f:
-    json.dump(current_config, f)
+    with open(str(backup_path), 'w') as f:
+        json.dump(current_config, f)
 
+    docker_cache_mirror = "http://{}:30500".format(args.host)
+    if "registry-mirrors" in current_config:
+        if docker_cache_mirror not in current_config["registry-mirrors"]:
+            current_config["registry-mirrors"].append(docker_cache_mirror)
+    else:
+        current_config["registry-mirrors"] = [docker_cache_mirror]
+
+    if "insecure-registries" in current_config:
+        if docker_cache_mirror not in current_config["insecure-registries"]:
+            current_config["insecure-registries"].append(docker_cache_mirror)
+    else:
+        current_config["insecure-registries"] = [docker_cache_mirror]
+
+    with open(str(target_path), 'w') as f:
+        json.dump(current_config, f)
+
+if __name__ == "__main__":
+    main()
