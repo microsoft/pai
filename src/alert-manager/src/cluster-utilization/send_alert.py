0bce39e0c7f1ea55b51bf302e21663e459b0752c
Guoxin <suiguoxin@gmail.com>
2021-02-22 15:27:32 +0800

    [alert-manager] add gpu*hours info in cluster-utilization cronjob (#5294)
    * add gpu hours field to job gpu usage and sort the list
    
    * add severity label
    
    * add resources occupied to user usage
    
    * send all alerts in one http connect
    
    * get resources occupied from prometheus
    
    * get prometheus scrape interval from cluster cfg

--- src/alert-manager/src/cluster-utilization/send_alert.py
+++ src/alert-manager/src/cluster-utilization/send_alert.py
@@ -1,11 +1,11 @@
 from datetime import timezone, datetime
 import logging
 import os
-import urllib
 import requests
 
 CLUSTER_QUERY_STRING = "avg(avg_over_time(nvidiasmi_utilization_gpu[7d]))"
-JOB_QUERY_STRING = 'avg by (job_name) (avg_over_time(task_gpu_percent{username!="noves"}[7d]))'
+JOB_GPU_PERCENT = 'avg by (job_name) (avg_over_time(task_gpu_percent[7d]))'
+JOB_GPU_HOURS = 'sum by (job_name) (count_over_time(task_gpu_percent[7d]))'
 # user used gpu hours / total gpu hours
 USER_QUERY_STRING = \
     "(sum by (username) (sum_over_time(task_gpu_percent[7d]))) / (sum by (username) (count_over_time(task_gpu_percent[7d])*100)) * 100"
@@ -15,6 +15,7 @@ ALERT_PREFIX = "/alert-manager/api/v1/alerts"
 REST_JOB_API_PREFIX = "/rest-server/api/v2/jobs"
 
 TOKEN = os.environ.get('PAI_BEARER_TOKEN')
+PROMETHEUS_SCRAPE_INTERVAL = os.environ.get('PROMETHEUS_SCRAPE_INTERVAL')
 
 def enable_request_debug_log(func):
     def wrapper(*args, **kwargs):
@@ -32,87 +33,127 @@ def enable_request_debug_log(func):
     return wrapper
 
 
+def datetime_to_hours(dt):
+    """Converts datetime.timedelta to hours
+
+    Parameters:
+    -----------
+    dt: datetime.timedelta
+
+    Returns:
+    --------
+    float
+    """
+    return dt.days * 24 + dt.seconds / 3600
+
+
 @enable_request_debug_log
-def get_job_usage_info(job_usage_result, rest_url):
+def get_usage_info(job_gpu_percent, job_gpu_hours, user_usage_result, rest_url):
     job_infos = {}
-    job_usage = []
-    job_list = []
+    user_infos = {}
     # get all jobs
     headers = {'Authorization': "Bearer {}".format(TOKEN)}
     resp = requests.get(rest_url, headers=headers)
     resp.raise_for_status()
     job_list = resp.json()
 
-    for v in job_usage_result["data"]["result"]:
-        job_infos[v["metric"]["job_name"]] = {"usage": v["value"][1][:6] + "%"}
-    for job_name, job_info in job_infos.items():
-        url = urllib.parse.urljoin(rest_url + "/", job_name)
-        resp = requests.get(url, headers=headers)
-        if not resp.ok:
-            logging.warning("request failed %s", resp.text)
-            continue
-        resp_json = resp.json()
-        # get job duration
-        if not resp_json["jobStatus"]["appLaunchedTime"]:
-            logging.warning("job not start, ignore it")
-            continue
-        job_info["start_time"] = datetime.fromtimestamp(
-            int(resp_json["jobStatus"]["appLaunchedTime"]) / 1000,
-            timezone.utc)
-        # job has not finished
-        if not resp_json["jobStatus"]["appCompletedTime"]:
-            job_info["duration"] = datetime.now(
-                timezone.utc) - job_info["start_time"]
-        # job has finished
-        else:
-            job_info["duration"] = datetime.fromtimestamp(
-                int(resp_json["jobStatus"]["appCompletedTime"]) / 1000,
-                timezone.utc) - job_info["start_time"]
-        job_info["status"] = resp_json["jobStatus"]["state"]
+    for v in user_usage_result["data"]["result"]:
+        user_infos[v["metric"]["username"]] = {
+            "username": v["metric"]["username"],
+            "usage": v["value"][1][:6] + "%", "resources_occupied": 0
+        }
+    for v in job_gpu_percent["data"]["result"]:
+        job_name = v["metric"]["job_name"]
         matched_job = list(
             filter(lambda job: "{}~{}".format(job["username"], job["name"]) == job_name,
             job_list))
-        if matched_job:
-            job_info["gpu_number"] = matched_job[0]["totalGpuNumber"]
-        else:
-            job_info["gpu_number"] = 0
-    for job_name, job_info in job_infos.items():
-        if "start_time" not in job_info:
+        # ingore unfounded jobs
+        if not matched_job:
+            logging.warning("Job %s not found.", job_name)
             continue
-        job_usage.append(
-            (job_name, job_info["usage"], str(job_info["duration"]),
-             job_info["start_time"].strftime("%y-%m-%d %H:%M:%S"), job_info["status"],
-             str(job_info["gpu_number"])))
-    return job_usage
+        job_info = matched_job[0]
+        # ignore jobs not started
+        if not job_info["launchedTime"]:
+            logging.warning("job not start, ignore it")
+            continue
+
+        job_infos[job_name] = {
+            "job_name": job_name,
+            "usage": v["value"][1],
+            "gpu_number": job_info["totalGpuNumber"]
+        }
+
+        # get job duration
+        job_infos[job_name]["start_time"] = datetime.fromtimestamp(
+            int(job_info["launchedTime"]) / 1000,
+            timezone.utc)
+        # job has not finished
+        if not job_info["completedTime"]:
+            job_infos[job_name]["duration"] = datetime.now(timezone.utc) - job_infos[job_name]["start_time"]
+        # job has finished
+        else:
+            job_infos[job_name]["duration"] = datetime.fromtimestamp(
+                int(job_info["completedTime"]) / 1000,
+                timezone.utc) - job_infos[job_name]["start_time"]
+        job_infos[job_name]["status"] = job_info["state"]
+
+        # get matched job gpu hours info
+        gpu_hours_info = list(
+            filter(lambda job: job["metric"]["job_name"] == job_name,
+            job_gpu_hours["data"]["result"]))
+        job_infos[job_name]["resources_occupied"] = float(gpu_hours_info[0]["value"][1]) * PROMETHEUS_SCRAPE_INTERVAL / 3600 # GPU * hours
+
+        # gpu hours by user
+        username = job_info["username"]
+        user_infos[username]["resources_occupied"] += job_infos[job_name]["resources_occupied"]
+
+    # format
+    for job_name, job_info in job_infos.items():
+        job_infos[job_name]["usage"] = job_info["usage"][:6] + "%"
+        job_infos[job_name]["gpu_number"] = str(job_info["gpu_number"])
+        job_infos[job_name]["duration"] = str(job_info["duration"])
+        job_infos[job_name]["start_time"] = job_info["start_time"].strftime("%y-%m-%d %H:%M:%S")
+        job_infos[job_name]["resources_occupied"] = "{:.2f}".format(job_info["resources_occupied"])
+    for username, user_info in user_infos.items():
+        user_infos[username]["resources_occupied"] = "{:.2f}".format(user_info["resources_occupied"])
+
+    # sort usage info by resources occupied
+    job_usage = sorted(job_infos.values(), key=lambda x: float(x["resources_occupied"]), reverse=True)
+    user_usage = sorted(user_infos.values(), key=lambda x: float(x["resources_occupied"]), reverse=True)
+
+    return job_usage[:10], user_usage
 
 
 @enable_request_debug_log
 def collect_metrics(url):
-    logging.info("Start to collect usage info")
     query_url = url.rstrip("/") + QUERY_PREFIX
     rest_url = url.rstrip("/") + REST_JOB_API_PREFIX
 
     # cluster info
+    logging.info("Collecting cluster usage info...")
     resp = requests.get(query_url, params={"query": CLUSTER_QUERY_STRING})
     resp.raise_for_status()
     result = resp.json()
     cluster_usage = result["data"]["result"][0]["value"][1][:6] + "%"
 
     # user info
-    logging.info("Start to getting user average usage")
+    logging.info("Collecting user usage info...")
     resp = requests.get(query_url, params={"query": USER_QUERY_STRING})
     resp.raise_for_status()
-    result = resp.json()
-    user_usage = []
-    for v in result["data"]["result"]:
-        user_usage.append((v["metric"]["username"], v["value"][1][:6] + "%"))
+    user_usage_result = resp.json()
 
     # job info
-    logging.info("Start to getting job usage")
-    resp = requests.get(query_url, params={"query": JOB_QUERY_STRING})
+    logging.info("Collecting job usage info...")
+    # job gpu percent
+    resp = requests.get(query_url, params={"query": JOB_GPU_PERCENT})
     resp.raise_for_status()
-    result = resp.json()
-    job_usage = get_job_usage_info(result, rest_url)
+    job_gpu_percent = resp.json()
+    # job gpu hours
+    resp = requests.get(query_url, params={"query": JOB_GPU_HOURS})
+    resp.raise_for_status()
+    job_gpu_hours = resp.json()
+
+    job_usage, user_usage = get_usage_info(job_gpu_percent, job_gpu_hours, user_usage_result, rest_url)
 
     return cluster_usage, job_usage, user_usage
 
@@ -120,55 +161,61 @@ def collect_metrics(url):
 @enable_request_debug_log
 def send_alert(pai_url: str, cluster_usage, job_usage, user_usage):
     trigger_time = str(datetime.now(timezone.utc).date())
-    logging.info("Starting to send alerts")
     post_url = pai_url.rstrip("/") + ALERT_PREFIX
+    alerts = []
     # for cluster
-    payload = [{
+    alert = {
         "labels": {
             "alertname": "usage",
             "report_type": "cluster-usage",
+            "severity": "info",
             "cluster_usage": cluster_usage,
             "trigger_time": trigger_time,
         },
         "generatorURL": "alert/script"
-    }]
-    resp = requests.post(post_url, json=payload)
-    resp.raise_for_status()
+    }
+    alerts.append(alert)
 
     # for job
     for job in job_usage:
-        payload = [{
+        alert = {
             "labels": {
                 "alertname": "usage",
                 "report_type": "cluster-usage",
-                "job_name": job[0],
-                "job_usage": job[1],
-                "job_duration": job[2],
-                "job_start_time": job[3],
-                "job_status": job[4],
-                "job_gpu_number": job[5],
+                "severity": "info",
+                "job_name": job["job_name"],
+                "resources_occupied": job["resources_occupied"],
+                "gpu_number": job["gpu_number"],
+                "usage": job["usage"],
+                "duration": job["duration"],
+                "start_time": job["start_time"],
+                "status": job["status"],
                 "trigger_time": trigger_time,
             },
             "generatorURL": "alert/script"
-        }]
-        resp = requests.post(post_url, json=payload)
-        resp.raise_for_status()
+        }
+        alerts.append(alert)
 
     # for user
     for user in user_usage:
-        payload = [{
+        alert = {
             "labels": {
                 "alertname": "usage",
                 "report_type": "cluster-usage",
-                "user_name": user[0],
-                "user_usage": user[1],
+                "severity": "info",
+                "username": user["username"],
+                "resources_occupied": user["resources_occupied"],
+                "usage": user["usage"],
                 "trigger_time": trigger_time,
             },
             "generatorURL": "alert/script"
-        }]
-        resp = requests.post(post_url, json=payload)
-        resp.raise_for_status()
-    logging.info("Finished sending alerts")
+        }
+        alerts.append(alert)
+
+    logging.info("Sending alerts to alert-manager...")
+    resp = requests.post(post_url, json=alerts)
+    resp.raise_for_status()
+    logging.info("Alerts sent to alert-manager.")
 
 
 def main():
