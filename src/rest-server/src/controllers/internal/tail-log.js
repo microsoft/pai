8e712e9905f9940c44674a927107939ffed7cc8b
Binyang Li <binyli@microsoft.com>
2021-03-24 16:44:35 +0800

    fix lint

--- src/rest-server/src/controllers/internal/tail-log.js
+++ src/rest-server/src/controllers/internal/tail-log.js
@@ -16,24 +16,42 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // module dependencies
-const url = require('url')
+const url = require('url');
 const asyncHandler = require('@pai/middlewares/v2/asyncHandler');
-const {
-    ContainerClient,
-  } = require('@azure/storage-blob');
+const { ContainerClient } = require('@azure/storage-blob');
 
-const getTailLog = asyncHandler (async (req, res) => {
+const logger = require('@pai/config/logger');
+const createError = require('@pai/utils/error');
+
+const getTailLog = asyncHandler(async (req, res) => {
   const tailLogSize = 16 * 1024 * 1024; // 16 KB
   const logName = req.params.logName;
-  const queryString = url.parse(req.url).query
-
+  const queryString = url.parse(req.url).query;
   const account = process.env.LOG_AZURE_STORAGE_ACCOUNT;
-  const containerClient = new ContainerClient(`https://${account}.blob.core.windows.net/pai-log?${queryString}`);
-  const blobClient = containerClient.getBlobClient(logName);
-  const properies = await blobClient.getProperties();
-  const offset = properies.contentLength - tailLogSize < 0 ? 0 : properies.contentLength - tailLogSize;
-  const buffer = await blobClient.downloadToBuffer(offset, properies.contentLength - offset);
-  res.status(206).send(buffer);
+
+  try {
+    const containerClient = new ContainerClient(
+      `https://${account}.blob.core.windows.net/pai-log?${queryString}`,
+    );
+    const blobClient = containerClient.getBlobClient(logName);
+    const properties = await blobClient.getProperties();
+    const offset =
+      properties.contentLength - tailLogSize < 0
+        ? 0
+        : properties.contentLength - tailLogSize;
+    const buffer = await blobClient.downloadToBuffer(
+      offset,
+      properties.contentLength - offset,
+    );
+    res.status(206).send(buffer);
+  } catch (error) {
+    logger.error(`Got error when retrieving tail log, error: ${error}`);
+    throw createError(
+      'Internal Server Error',
+      'UnknownError',
+      'Failed to get tail log',
+    );
+  }
 });
 
 // module exports
