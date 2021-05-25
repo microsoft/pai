f17094f68d2a721de513ee20a84210f429131049
Binyang2014 <binyli@microsoft.com>
2021-05-25 17:21:57 +0800

    Support customize ssl port (#5495)
    Support customize ssl port

--- src/pylon/config/pylon.py
+++ src/pylon/config/pylon.py
@@ -36,11 +36,15 @@ class Pylon:
         machine_list = self.cluster_configuration['machine-list']
         master_ip = [host['hostip'] for host in machine_list if host.get('pai-master') == 'true'][0]
         port = self.service_configuration['port']
+        sslConfig = {'port': 443}
+        if 'ssl' in self.service_configuration:
+            sslConfig.update(self.service_configuration['ssl'])
+        sslPort = sslConfig['port']
         uri = 'http://{0}:{1}'.format(master_ip, port)
-        uriHttps = 'https://{0}'.format(master_ip)
+        uriHttps = 'https://{0}:{1}'.format(master_ip, sslPort)
         if 'domain' in self.service_configuration:
             uri = 'http://{0}:{1}'.format(self.service_configuration['domain'], port)
-            uriHttps = 'https://{0}'.format(self.service_configuration['domain'])
+            uriHttps = 'https://{0}:{1}'.format(self.service_configuration['domain'], sslPort)
 
         webhdfs_legacy_port = self.service_configuration['webhdfs-legacy-port']
         ret = {
@@ -49,8 +53,7 @@ class Pylon:
             'uri-https': uriHttps,
             'webhdfs-legacy-port': webhdfs_legacy_port,
         }
-        if 'ssl' in self.service_configuration:
-            ret['ssl'] = self.service_configuration['ssl']
+        ret['ssl'] = sslConfig if sslConfig else None
 
         return ret
 
